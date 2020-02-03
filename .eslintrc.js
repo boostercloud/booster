b4efa0bc1de26b1bc82a71d04ade9eeaa3f90153
alvaroloes <alvaro@booster.cloud>
2020-02-03 16:50:57 +0000

    Fix linter configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: 'tsconfig.eslint.json',
   },
   rules: {
     indent: ['error', 2, { SwitchCase: 1 }],
