8dbd7912d0b368a5326cc94bea6ef09016b6601d
Nick Tchayka <nick@booster.cloud>
2020-02-04 16:17:56 +0000

    PATCH: Ensure that the project name is properly formatted (#5)
    * Add assertion about name length
    
    * Add assertion about spaces
    
    * Add check to ensure project name is all lowercase

--- packages/cli/src/services/provider-service.ts
+++ packages/cli/src/services/provider-service.ts
@@ -2,7 +2,29 @@ import { BoosterConfig } from '@boostercloud/framework-types'
 import { Observable } from 'rxjs'
 import { Providers } from '@boostercloud/framework-core'
 
+export function assertNameIsCorrect(name: string): void {
+  // It is 55 because cloudformations max length is 63.
+  // Booster creates an S3 bucket ended in '-toolkit'
+  // which is 8 chars long. 63 - 8 = 55
+  const maxProjectNameLength = 55
+  if (name.length > maxProjectNameLength)
+    throw new Error(`Project name cannot be longer than ${maxProjectNameLength} chars long:
+
+    Found: '${name}'`)
+
+  if (name.includes(' '))
+    throw new Error(`Project name cannot contain spaces:
+
+    Found: '${name}'`)
+
+  if (name.toLowerCase() != name)
+    throw new Error(`Project name cannot contain uppercase letters:
+
+    Found: '${name}'`)
+}
+
 export const deployToCloudProvider = (configuration: BoosterConfig): Observable<string> => {
+  assertNameIsCorrect(configuration.appName)
   return Providers.getInfrastructure(configuration).deploy(configuration)
 }
 export const nukeCloudProviderResources = (configuration: BoosterConfig): Observable<string> => {
