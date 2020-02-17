27ebdcc8ad1cb87e35153ea0c702bbdd3cf944f4
Javier Toledo <javier@booster.cloud>
2020-02-17 20:46:30 +0000

    Added a dependency between the infrastructure and runtime aws modules to avoid duplicating the `constants.ts file`

--- packages/framework-provider-aws/src/constants.ts
+++ packages/framework-provider-aws/src/constants.ts
@@ -1,3 +1,2 @@
-// TODO: Find a way to generalize this into configuration. This file is duplicated in packages `framework-provider-aws` and `framework-provider-aws-infrastructure`
 export const eventStorePartitionKeyAttributeName = 'entityTypeName_entityID_kind'
 export const eventStoreSortKeyAttributeName = 'createdAt'
