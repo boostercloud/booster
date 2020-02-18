c7dd2e810a26582f45f4b8f70c9fd54bdfaf6e77
Nikita Tchayka <nick@booster.cloud>
2020-02-18 16:28:21 +0000

    Add docs and error handler

--- packages/framework-provider-local-infrastructure/src/index.ts
+++ packages/framework-provider-local-infrastructure/src/index.ts
@@ -1,9 +1,36 @@
 import * as express from 'express'
 
-export function run(port: number): void {
+/**
+ * `deploy` serves as the entry point for the local provider. Even though
+ * it doesn't deploy anything, it does start the required infrastructure
+ * locally, which is running an `express` server.
+ *
+ * @param storage Instance of a `Storage` object, in order to store the data
+ * @param port Port on which the express server will listen
+ */
+export function run(storage: RuntimeStorage, port: number): void {
   const expressServer = express()
   const router = express.Router()
+  expressServer.use(defaultErrorHandler)
   expressServer.use(express.json())
   expressServer.use(router)
   expressServer.listen(port)
+}
+
+/**
+ * Default error handling middleware. Instead of performing a try/catch in all endpoints
+ * express will check if contents were sent, and if it failed, it will send a 500 with the
+ * error attached.
+ */
+function defaultErrorHandler(
+  err: Error,
+  _req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): void {
+  if (res.headersSent) {
+    return next(err)
+  }
+  console.error(err.message)
+  res.status(500).json(err)
 }
