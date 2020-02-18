e60ee17a81e56ee8592c0c7265ea876e935b369f
Nikita Tchayka <nick@booster.cloud>
2020-02-18 18:46:58 +0000

    Allow running the preSignUpChecker

--- packages/framework-provider-local-infrastructure/src/controllers/auth.ts
+++ packages/framework-provider-local-infrastructure/src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import * as express from 'express'
 import { RuntimeStorage } from '../runtime-storage'
 import { UserEnvelope, BoosterConfig } from '@boostercloud/framework-types'
+import { boosterPreSignUpChecker } from '@boostercloud/framework-core'
 
 export class AuthController {
   public router: express.Router = express.Router()
@@ -11,7 +12,7 @@ export class AuthController {
 
   private async signUp(req: express.Request, res: express.Response): Promise<void> {
     const user: UserEnvelope = req.body
-    // await this.config.provider.preSignUpHandlerReference(req.body) // TODO
+    await boosterPreSignUpChecker(user)
     this.storage.registeredUsers[user.email] = user
     res.status(200).json()
   }
