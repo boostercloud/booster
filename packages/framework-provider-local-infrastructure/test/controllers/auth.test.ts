a450ac38198972a874e1463373aef2f3590789e4
Nikita Tchayka <nick@booster.cloud>
2020-02-19 16:59:13 +0000

    ✅ Stub signup checker

--- packages/framework-provider-local-infrastructure/test/controllers/auth.test.ts
+++ packages/framework-provider-local-infrastructure/test/controllers/auth.test.ts
@@ -3,13 +3,13 @@ import { RuntimeStorage } from '../../src/runtime-storage'
 import { BoosterConfig, ProviderLibrary } from '@boostercloud/framework-types'
 import { expect } from 'chai'
 import * as faker from 'faker'
-import path = require('path')
+import { stub } from 'sinon'
 
 describe('the authorization controller', () => {
   it('should sign up users', async () => {
     const storage = new RuntimeStorage()
     const provider = {} as ProviderLibrary
-    const userProject = require(path.normalize(path.join(process.cwd(), '../framework-example/src/index')))
+    const userProject = { boosterPreSignUpChecker: stub() }
     const config = new BoosterConfig()
     config.provider = provider
     const controller = new AuthController(storage, config, userProject)
