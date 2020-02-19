2397590dc94b4620598a89b156371b0ce61c1635
Nikita Tchayka <nick@booster.cloud>
2020-02-19 18:22:44 +0000

    Fix tests

--- packages/framework-provider-local-infrastructure/test/controllers/auth.test.ts
+++ packages/framework-provider-local-infrastructure/test/controllers/auth.test.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { AuthController } from '../../src/controllers/auth'
 import { RuntimeStorage } from '../../src/runtime-storage'
-import { BoosterConfig, ProviderLibrary } from '@boostercloud/framework-types'
+import { BoosterConfig, ProviderLibrary, UserApp } from '@boostercloud/framework-types'
 import { expect } from 'chai'
 import * as faker from 'faker'
 import { stub } from 'sinon'
@@ -9,7 +10,7 @@ describe('the authorization controller', () => {
   it('should sign up users', async () => {
     const storage = new RuntimeStorage()
     const provider = {} as ProviderLibrary
-    const userProject = { boosterPreSignUpChecker: stub() }
+    const userProject = { boosterPreSignUpChecker: stub() as any } as UserApp
     const config = new BoosterConfig()
     config.provider = provider
     const controller = new AuthController(storage, config, userProject)
