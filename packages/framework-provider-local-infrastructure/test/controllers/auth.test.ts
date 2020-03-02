a341dda7bd662382b114e69b5caa98dae0be73be
Nikita Tchayka <nick@booster.cloud>
2020-03-02 13:35:11 +0000

    Refactor tests

--- packages/framework-provider-local-infrastructure/test/controllers/auth.test.ts
+++ packages/framework-provider-local-infrastructure/test/controllers/auth.test.ts
@@ -1,23 +1,45 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { AuthController } from '../../src/controllers/auth'
 import { RuntimeStorage } from '../../src/runtime-storage'
-import { BoosterConfig, ProviderLibrary, UserApp, UserEnvelope } from '@boostercloud/framework-types'
+import {
+  BoosterConfig,
+  ProviderLibrary,
+  UserApp,
+  UserEnvelope,
+  NotAuthorizedError,
+} from '@boostercloud/framework-types'
 import { expect } from 'chai'
 import * as faker from 'faker'
 import { stub } from 'sinon'
+import * as chai from 'chai'
+import * as chaiAsPromised from 'chai-as-promised'
+chai.use(chaiAsPromised)
 
 describe('the authorization controller', () => {
+  let registeredUsers: Record<string, UserEnvelope> = {}
+  let authenticatedUsers: Record<string, UserEnvelope> = {}
+  const storage = {
+    registerUser: (user: UserEnvelope) => {
+      registeredUsers[user.email] = user
+    },
+    authenticateUser: (token: string, user: UserEnvelope) => {
+      authenticatedUsers[token] = user
+    },
+    getRegisteredUsersByEmail: (email: string) => {
+      const registered = registeredUsers[email]
+      return Promise.resolve(registered ? [registered] : [])
+    },
+  } as RuntimeStorage
+  const provider = {} as ProviderLibrary
+  const userProject = { boosterPreSignUpChecker: stub() as any } as UserApp
+  const config = new BoosterConfig()
+  config.provider = provider
+
+  beforeEach(() => {
+    registeredUsers = {}
+    authenticatedUsers = {}
+  })
   it('should sign up users', async () => {
-    const registeredUsers: Record<string, UserEnvelope> = {}
-    const storage = {
-      registerUser: (user: UserEnvelope) => {
-        registeredUsers[user.email] = user
-      },
-    } as RuntimeStorage
-    const provider = {} as ProviderLibrary
-    const userProject = { boosterPreSignUpChecker: stub() as any } as UserApp
-    const config = new BoosterConfig()
-    config.provider = provider
     const controller = new AuthController(storage, config, userProject)
     const userEmail = faker.internet.email()
     const user = {
@@ -29,20 +51,6 @@ describe('the authorization controller', () => {
   })
 
   it('should sign in users', async () => {
-    const registeredUsers: Record<string, UserEnvelope> = {}
-    const authenticatedUsers: Record<string, UserEnvelope> = {}
-    const storage = {
-      registerUser: (user: UserEnvelope) => {
-        registeredUsers[user.email] = user
-      },
-      authenticateUser: (token: string, user: UserEnvelope) => {
-        authenticatedUsers[token] = user
-      },
-    } as RuntimeStorage
-    const provider = {} as ProviderLibrary
-    const userProject = { boosterPreSignUpChecker: stub() as any } as UserApp
-    const config = new BoosterConfig()
-    config.provider = provider
     const controller = new AuthController(storage, config, userProject)
     const userEmail = faker.internet.email()
     const user = {
