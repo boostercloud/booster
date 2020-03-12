4fc4105d9472c58993ae91add140f6f5ae374ebe
Nikita Tchayka <nick@booster.cloud>
2020-03-12 13:54:45 +0000

    Update templates

--- packages/framework-example/src/config/config.ts
+++ packages/framework-example/src/config/config.ts
@@ -2,7 +2,7 @@ import { Booster } from '@boostercloud/framework-core'
 import { BoosterConfig } from '@boostercloud/framework-types'
 import { Provider } from '@boostercloud/framework-provider-aws'
 
-Booster.environment('production', (config: BoosterConfig): void => {
+Booster.configure('production', (config: BoosterConfig): void => {
   config.appName = 'my-store'
   config.provider = Provider
 })
