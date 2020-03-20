4be7bbd6716a475b4e6d99563334e7393494523d
Nikita Tchayka <nick@booster.cloud>
2020-03-20 19:05:03 +0000

    ci: Fix

--- scripts/generate-docs.sh
+++ scripts/generate-docs.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/boostercloud/docs-site .docs
 cp -r ./docs/documentation/* ./.docs/source/includes/
-cd ./docs
+cd ./.docs
 bundle install
 bundle exec middleman build --clean
 cd ..
