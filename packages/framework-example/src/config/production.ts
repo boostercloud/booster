3e790dbf08f28ac03b2b71df1354f34cb7ba67c4
Nikita Tchayka <nick@booster.cloud>
2020-03-24 17:04:01 +0000

    Split config

--- packages/framework-example/src/config/production.ts
+++ packages/framework-example/src/config/production.ts
@@ -1,14 +1,8 @@
 import { Booster } from '@boostercloud/framework-core'
 import { BoosterConfig } from '@boostercloud/framework-types'
 import { Provider } from '@boostercloud/framework-provider-aws'
-import * as Local from '@boostercloud/framework-provider-local'
 
 Booster.configure('production', (config: BoosterConfig): void => {
   config.appName = 'my-store'
   config.provider = Provider
-})
-
-Booster.configure('development', (config: BoosterConfig): void => {
-  config.appName = 'my-store'
-  config.provider = Local.Provider
 })
