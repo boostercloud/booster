0d7765cbdbd201e33ae593fad2226605a053d010
Nikita Tchayka <nick@booster.cloud>
2020-04-27 16:40:08 +0100

    Remove unused function

--- packages/framework-provider-aws/src/library/api-gateway-io.ts
+++ packages/framework-provider-aws/src/library/api-gateway-io.ts
@@ -16,23 +16,6 @@ export async function requestFailed(error: Error): Promise<APIGatewayProxyResult
   }
 }
 
-export async function graphQLRequestFailed(error: Error): Promise<APIGatewayProxyResult> {
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    statusCode: 200,
-    body: JSON.stringify({
-      errors: [
-        {
-          message: error.message,
-          locations: [], // TODO: Add locations on GraphQL errors
-        },
-      ],
-    }),
-  }
-}
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function requestSucceeded(body?: any): Promise<APIGatewayProxyResult> {
   return {
