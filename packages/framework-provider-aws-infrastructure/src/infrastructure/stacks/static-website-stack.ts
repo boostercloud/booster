5adc4528fee0c9fe5cac6a816eafa4ea514248fe
juanjoman <j2rgez@gmail.com>
2020-05-13 15:48:59 +0100

    Testing

--- packages/framework-provider-aws-infrastructure/src/infrastructure/stacks/static-website-stack.ts
+++ packages/framework-provider-aws-infrastructure/src/infrastructure/stacks/static-website-stack.ts
@@ -3,16 +3,23 @@ import { Bucket } from '@aws-cdk/aws-s3'
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment'
 import { CloudFrontWebDistribution } from '@aws-cdk/aws-cloudfront'
 import { BoosterConfig } from '@boostercloud/framework-types'
+import { Level } from '@boostercloud/framework-types'
+import {buildLogger} from "../../../../framework-core/dist/booster-logger";
 
 const publicDistPath = './public/dist'
 const indexHTML = 'index.html'
 
 export default class StaticWebsiteStack {
   public static build(config: BoosterConfig, stack: Stack): void {
+    const logger = buildLogger(Level.info)
     const staticSiteBucket = new Bucket(stack, 'staticWebsiteBucket', {
       websiteIndexDocument: indexHTML,
       bucketName: config.resourceNames.staticWebsite,
     })
+
+    logger.debug("////////////////BUCKET/////////////////")
+    logger.debug(staticSiteBucket)
+    logger.debug("/////////////////////////////////")
 
     const cloudFrontDistribution = new CloudFrontWebDistribution(stack, 'staticWebsiteDistribution', {
       originConfigs: [
@@ -24,6 +31,14 @@ export default class StaticWebsiteStack {
         },
       ],
     })
+
+    logger.debug("///////////////CLOUDFRONT DISTRIBUTION//////////////////")
+    logger.debug(cloudFrontDistribution)
+    logger.debug("/////////////////////////////////")
+
+    logger.debug("////////////////ASSET S3/////////////////")
+    logger.debug(Source.asset(publicDistPath))
+    logger.debug("/////////////////////////////////")
 
     new BucketDeployment(stack, 'staticWebsiteDeployment', {
       sources: [Source.asset(publicDistPath)],
