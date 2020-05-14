0d1d27738adb65a782e396d807813bc0ee3b5db9
Tai Nguyen Bui <tai@theagilemonkeys.com>
2020-05-14 12:33:27 +0100

    Update packages/framework-integration-tests/integration/helper/fileHelper.ts
    Co-authored-by: Javier Toledo <javier@booster.cloud>

--- packages/framework-integration-tests/integration/helper/fileHelper.ts
+++ packages/framework-integration-tests/integration/helper/fileHelper.ts
@@ -4,6 +4,4 @@ export const readFileContent = async (filePath: string): Promise<string> => {
   return fs.readFileSync(filePath, 'utf-8')
 }
 
-export const removeFile = async (filePath: string): Promise<void> => {
-  return fs.unlinkSync(filePath)
-}
+export const removeFile = fs.unlinkSync
