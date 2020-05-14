51e2c0b79467f578882906807b0c817cbfa33fe8
juanjoman <j2rgez@gmail.com>
2020-05-14 13:16:20 +0100

    BOOST-385 Add static website URL on cloudfront outputs

--- packages/framework-provider-aws-infrastructure/src/infrastructure/stacks/static-website-stack.ts
+++ packages/framework-provider-aws-infrastructure/src/infrastructure/stacks/static-website-stack.ts
@@ -1,25 +1,18 @@
-import { Stack } from '@aws-cdk/core'
+import { CfnOutput, Stack } from '@aws-cdk/core'
 import { Bucket } from '@aws-cdk/aws-s3'
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment'
 import { CloudFrontWebDistribution } from '@aws-cdk/aws-cloudfront'
 import { BoosterConfig } from '@boostercloud/framework-types'
-import { Level } from '@boostercloud/framework-types'
-import {buildLogger} from "../../../../framework-core/dist/booster-logger";
 
 const publicDistPath = './public/dist'
 const indexHTML = 'index.html'
 
 export default class StaticWebsiteStack {
   public static build(config: BoosterConfig, stack: Stack): void {
-    const logger = buildLogger(Level.info)
     const staticSiteBucket = new Bucket(stack, 'staticWebsiteBucket', {
       websiteIndexDocument: indexHTML,
       bucketName: config.resourceNames.staticWebsite,
     })
-
-    logger.debug("////////////////BUCKET/////////////////")
-    logger.debug(staticSiteBucket)
-    logger.debug("/////////////////////////////////")
 
     const cloudFrontDistribution = new CloudFrontWebDistribution(stack, 'staticWebsiteDistribution', {
       originConfigs: [
@@ -32,18 +25,15 @@ export default class StaticWebsiteStack {
       ],
     })
 
-    logger.debug("///////////////CLOUDFRONT DISTRIBUTION//////////////////")
-    logger.debug(cloudFrontDistribution)
-    logger.debug("/////////////////////////////////")
-
-    logger.debug("////////////////ASSET S3/////////////////")
-    logger.debug(Source.asset(publicDistPath))
-    logger.debug("/////////////////////////////////")
-
     new BucketDeployment(stack, 'staticWebsiteDeployment', {
       sources: [Source.asset(publicDistPath)],
       destinationBucket: staticSiteBucket,
       distribution: cloudFrontDistribution,
+    })
+
+    new CfnOutput(stack, 'static-website-URL', {
+      value: cloudFrontDistribution.domainName,
+      description: `The URL for the static website generated from ${publicDistPath} directory`,
     })
   }
 }
