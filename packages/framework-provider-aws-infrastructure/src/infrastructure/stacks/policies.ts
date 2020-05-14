ff2659010a5355056c9373093992a6b872a93af0
Tai Nguyen Bui <tai@theagilemonkeys.com>
2020-05-14 15:20:21 +0100

    BOOST-268 test: adds tests for policies

--- packages/framework-provider-aws-infrastructure/src/infrastructure/stacks/policies.ts
+++ packages/framework-provider-aws-infrastructure/src/infrastructure/stacks/policies.ts
@@ -1,8 +1,13 @@
-import { PolicyStatement } from '@aws-cdk/aws-iam'
+import { Effect, PolicyStatement } from '@aws-cdk/aws-iam'
 
-export const createPolicyStatement = (resources?: string[], actions?: string[]) => {
+export const createPolicyStatement = (
+  resources?: string[],
+  actions?: string[],
+  effect: Effect = Effect.ALLOW
+): PolicyStatement => {
   return new PolicyStatement({
     resources,
     actions,
+    effect,
   })
 }
