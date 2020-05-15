2af4e6571b0d38afc939ba62e1fb2d1a029f781f
juanjoman <j2rgez@gmail.com>
2020-05-15 15:47:59 +0100

    BOOST-385 Check if directory exists

--- packages/framework-provider-aws-infrastructure/src/infrastructure/stacks/static-website-stack.ts
+++ packages/framework-provider-aws-infrastructure/src/infrastructure/stacks/static-website-stack.ts
@@ -3,38 +3,41 @@ import { Bucket } from '@aws-cdk/aws-s3'
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment'
 import { CloudFrontWebDistribution } from '@aws-cdk/aws-cloudfront'
 import { BoosterConfig } from '@boostercloud/framework-types'
+import { existsSync } from "fs";
 
 const publicDistPath = './public/dist'
 const indexHTML = 'index.html'
 
 export default class StaticWebsiteStack {
   public static build(config: BoosterConfig, stack: Stack): void {
-    const staticSiteBucket = new Bucket(stack, 'staticWebsiteBucket', {
-      websiteIndexDocument: indexHTML,
-      bucketName: config.resourceNames.staticWebsite,
-      publicReadAccess: true
-    })
+    if (existsSync(publicDistPath)) {
+      const staticSiteBucket = new Bucket(stack, 'staticWebsiteBucket', {
+        websiteIndexDocument: indexHTML,
+        bucketName: config.resourceNames.staticWebsite,
+        publicReadAccess: true
+      })
 
-    const cloudFrontDistribution = new CloudFrontWebDistribution(stack, 'staticWebsiteDistribution', {
-      originConfigs: [
-        {
-          s3OriginSource: {
-            s3BucketSource: staticSiteBucket,
+      const cloudFrontDistribution = new CloudFrontWebDistribution(stack, 'staticWebsiteDistribution', {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: staticSiteBucket,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
           },
-          behaviors: [{ isDefaultBehavior: true }],
-        },
-      ],
-    })
+        ],
+      })
 
-    new BucketDeployment(stack, 'staticWebsiteDeployment', {
-      sources: [Source.asset(publicDistPath)],
-      destinationBucket: staticSiteBucket,
-      distribution: cloudFrontDistribution,
-    })
+      new BucketDeployment(stack, 'staticWebsiteDeployment', {
+        sources: [Source.asset(publicDistPath)],
+        destinationBucket: staticSiteBucket,
+        distribution: cloudFrontDistribution,
+      })
 
-    new CfnOutput(stack, 'static-website-URL', {
-      value: `https://${cloudFrontDistribution.domainName}`,
-      description: `The URL for the static website generated from ${publicDistPath} directory`,
-    })
+      new CfnOutput(stack, 'static-website-URL', {
+        value: `https://${cloudFrontDistribution.domainName}`,
+        description: `The URL for the static website generated from ${publicDistPath} directory`,
+      })
+    }
   }
 }
