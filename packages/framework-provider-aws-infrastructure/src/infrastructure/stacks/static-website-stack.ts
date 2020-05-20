aa5e0004ad0b74dfa5fb4c5c5145a06b83b7552c
juanjoman <j2rgez@gmail.com>
2020-05-20 11:27:39 +0100

    BOOST-385 Requested changes + modify tests

--- packages/framework-provider-aws-infrastructure/src/infrastructure/stacks/static-website-stack.ts
+++ packages/framework-provider-aws-infrastructure/src/infrastructure/stacks/static-website-stack.ts
@@ -1,7 +1,7 @@
 import { CfnOutput, Stack } from '@aws-cdk/core'
-import { Bucket } from '@aws-cdk/aws-s3'
+import { Bucket, BlockPublicAccess } from '@aws-cdk/aws-s3'
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment'
-import { CloudFrontWebDistribution, ViewerCertificate, ViewerProtocolPolicy } from '@aws-cdk/aws-cloudfront'
+import { CloudFrontWebDistribution, ViewerCertificate, ViewerProtocolPolicy, OriginAccessIdentity } from '@aws-cdk/aws-cloudfront'
 import { BoosterConfig } from '@boostercloud/framework-types'
 import { existsSync } from "fs";
 
@@ -11,11 +11,17 @@ const indexHTML = 'index.html'
 export default class StaticWebsiteStack {
   public static build(config: BoosterConfig, stack: Stack): void {
     if (existsSync(publicDistPath)) {
+      const staticWebsiteOIA = new OriginAccessIdentity(stack, 'staticWebsiteOIA', {
+        comment: "Allows static site to be reached only via CloudFront"
+      })
       const staticSiteBucket = new Bucket(stack, 'staticWebsiteBucket', {
         websiteIndexDocument: indexHTML,
         websiteErrorDocument: indexHTML,
+        blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
         bucketName: config.resourceNames.staticWebsite
       })
+
+      staticSiteBucket.grantRead(staticWebsiteOIA)
 
       const cloudFrontDistribution = new CloudFrontWebDistribution(stack, 'staticWebsiteDistribution', {
         defaultRootObject: indexHTML,
@@ -25,6 +31,7 @@ export default class StaticWebsiteStack {
           {
             s3OriginSource: {
               s3BucketSource: staticSiteBucket,
+              originAccessIdentity: staticWebsiteOIA
             },
             behaviors: [{ isDefaultBehavior: true }],
           },
