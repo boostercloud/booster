9d4eb2d2cc0fbe0bf7a7ac5440dd579cc73dd7ac
Tai Nguyen Bui <tai@theagilemonkeys.com>
2020-05-22 13:36:35 +0100

    Update packages/framework-integration-tests/integration/aws/commands-api.integration.ts
    Co-authored-by: Álvaro López Espinosa <alvaro@booster.cloud>

--- packages/framework-integration-tests/integration/aws/commands-api.integration.ts
+++ packages/framework-integration-tests/integration/aws/commands-api.integration.ts
@@ -53,13 +53,18 @@ describe('the commands API', async () => {
     // Verify latest event
     const latestEvent = await queryEvents('Cart-demo-cart-id-event')
     expect(latestEvent).not.to.be.null
-    expect(latestEvent[0].entityTypeName_entityID_kind).to.be.equal('Cart-demo-cart-id-event')
-    expect(latestEvent[0].value.productId).to.be.equal('demo-product-id')
-    expect(latestEvent[0].value.cartId).to.be.equal('demo-cart-id')
-    expect(latestEvent[0].value.quantity).to.be.equal(2)
-    expect(latestEvent[0].kind).to.be.equal('event')
-    expect(latestEvent[0].entityTypeName).to.be.equal('Cart')
-    expect(latestEvent[0].typeName).to.be.equal('ChangedCartItem')
+    const expectedEvent: EventEnvelope = {
+       entityTypeName_entityID_kind: 'Cart-demo-cart-id-event'
+       value: {
+         productId: 'demo-product-id'
+         cartId: 'demo-cart-id'
+         quantity: 2
+       }
+       kind: 'event'
+       entityTypeName: 'Cart'
+       typeName: 'ChangedCartItem'
+    })
+    expect(latestEvent[0]).to.be.deep.equal(expectedEvent)
   })
 
   it('should generate a snapshot after 5 events', async () => {
