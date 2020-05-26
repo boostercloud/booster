51a7e80242c5b05a8dfa7147a2238ee11469122b
Tai Nguyen Bui <tai@theagilemonkeys.com>
2020-05-26 08:56:10 +0100

    BOOST-598 simplify functions

--- packages/framework-integration-tests/integration/helper/fileHelper.ts
+++ packages/framework-integration-tests/integration/helper/fileHelper.ts
@@ -1,11 +1,7 @@
 import * as fs from 'fs'
 
-export const readFileContent = async (filePath: string): Promise<string> => {
-  return fs.readFileSync(filePath, 'utf-8')
-}
+export const readFileContent = async (filePath: string): Promise<string> => fs.readFileSync(filePath, 'utf-8')
 
-export const writeFileContent = async (filePath: string, data: any): Promise<void> => {
-  return fs.writeFileSync(filePath, data)
-}
+export const writeFileContent = async (filePath: string, data: any): Promise<void> => fs.writeFileSync(filePath, data)
 
 export const removeFile = fs.unlinkSync
