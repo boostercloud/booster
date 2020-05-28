101ba45d91e222c54934246a4a546392ac6b5413
Tai Nguyen Bui <tai@theagilemonkeys.com>
2020-05-28 13:51:38 +0100

    BOOST-700 refactors setup code to avoid repetitions

--- packages/framework-integration-tests/integration/cli/setup.ts
+++ packages/framework-integration-tests/integration/cli/setup.ts
@@ -6,6 +6,7 @@ import { CLI_ENTITY_INTEGRATION_TEST_FILES } from './cli.entity.integration'
 const exec = util.promisify(require('child_process').exec)
 
 const testFiles: Array<string> = [...CLI_ENTITY_INTEGRATION_TEST_FILES]
+const removeFiles = testFiles.map((file: string) => removeFile(path.join(path.dirname(__dirname), '..', file)))
 
 before(async () => {
   const integrationTestsPackageRoot = path.dirname(__dirname)
@@ -18,7 +19,7 @@ before(async () => {
   process.chdir('..')
 
   try {
-    await Promise.all(testFiles.map((file: string) => removeFile(path.join(path.dirname(__dirname), '..', file))))
+    await Promise.all(removeFiles)
   } catch (e) {
     // error whilst deleting files
   }
@@ -28,6 +29,6 @@ after(async () => {
   try {
     await exec('lerna run compile')
   } finally {
-    await Promise.all(testFiles.map((file: string) => removeFile(path.join(path.dirname(__dirname), '..', file))))
+    await Promise.all(removeFiles)
   }
 })
