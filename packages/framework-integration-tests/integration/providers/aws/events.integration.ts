1232ec56657bf2bf9802c77f6ea1473ecee383ba
Tai Nguyen Bui <tai@theagilemonkeys.com>
2020-06-02 09:54:46 +0100

    BOOST-577 create first cart end-to-end test

--- packages/framework-integration-tests/integration/providers/aws/events.integration.ts
+++ packages/framework-integration-tests/integration/providers/aws/events.integration.ts
@@ -5,29 +5,11 @@ import { ApolloClient } from 'apollo-client'
 import { NormalizedCacheObject } from 'apollo-cache-inmemory'
 import { random } from 'faker'
 
-describe('the commands API', async () => {
+describe('events', async () => {
   let client: ApolloClient<NormalizedCacheObject>
 
   before(async () => {
     client = await graphQLClient()
-  })
-
-  it('accepts a command successfully', async () => {
-    const response = await client.mutate({
-      variables: {
-        cartId: random.uuid(),
-        productId: random.uuid(),
-        quantity: random.number({ min: 1 }),
-      },
-      mutation: gql`
-        mutation ChangeCartItem($cartId: ID!, $productId: ID!, $quantity: Float) {
-          ChangeCartItem(input: { cartId: $cartId, productId: $productId, quantity: $quantity })
-        }
-      `,
-    })
-
-    expect(response).not.to.be.null
-    expect(response?.data?.ChangeCartItem).to.be.true
   })
 
   it('should create an event in the event store', async () => {
