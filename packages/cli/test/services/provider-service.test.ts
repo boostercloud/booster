7f8bee9f74ff3cf99b30d7f3db1329f85954f788
myf95 <myf1995@gmail.com>
2020-06-03 16:27:26 +0100

    BOOST-535: add assertNameIsCorrect tests

--- packages/cli/test/services/provider-service.test.ts
+++ packages/cli/test/services/provider-service.test.ts
@@ -1,6 +1,43 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { describe } from 'mocha'
+import { afterEach, describe } from 'mocha'
+import * as providerService from '../../src/services/provider-service'
+import { restore } from 'sinon'
+import { expect } from '../expect'
+import { random } from 'faker'
 
 describe('providerService', () => {
+  afterEach(() => {
+    restore()
+  })
+
+  describe('assertNameIsCorrect', () => {
+    it('should throw an error on surpassing project name max length', () => {
+      const inputString = random.alphaNumeric(38)
+      const errorString = `Project name cannot be longer than 37 chars long:\n\n    Found: '${inputString}'`
+
+      expect(() => providerService.assertNameIsCorrect(inputString)).to.throw(errorString)
+    })
+
+    it('should throw an error if project name includes a space', () => {
+      const inputString = random.words(3)
+      const errorString = `Project name cannot contain spaces:\n\n    Found: '${inputString}'`
+
+      expect(() => providerService.assertNameIsCorrect(inputString)).to.throw(errorString)
+    })
+
+    it('should throw an error if project name includes an uppercase letter', () => {
+      const inputString = random.alphaNumeric(37).toUpperCase()
+      const errorString = `Project name cannot contain uppercase letters:\n\n    Found: '${inputString}'`
+
+      expect(() => providerService.assertNameIsCorrect(inputString)).to.throw(errorString)
+    })
+
+    it('should not throw an error if project name is correct', () => {
+      const inputString = random.alphaNumeric(37)
+
+      expect(() => providerService.assertNameIsCorrect(inputString)).to.not.throw()
+    })
+  })
+
   describe.skip('deployToCloudProvider', () => {})
 })
