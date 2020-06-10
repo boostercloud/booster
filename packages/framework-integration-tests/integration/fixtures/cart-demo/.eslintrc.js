72952abf33c91adb0b40a7191b6d8d26d6714e8c
Guillermo Lammers <glammersc@gmail.com>
2020-06-10 22:48:19 +0100

    test: add fixtures for new:project command

---
+++ packages/framework-integration-tests/integration/fixtures/cart-demo/.eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: 'tsconfig.eslint.json',
+  },
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: false }],
+    semi: ['error', 'never'],
+    '@typescript-eslint/generic-type-naming': ['error', '^T[A-Z][a-zA-Z]+$'],
+    'no-extra-parens': 'off',
+    '@typescript-eslint/no-extra-parens': ['error'],
+    'no-magic-numbers': 'off',
+    '@typescript-eslint/no-parameter-properties': 0,
+    '@typescript-eslint/no-floating-promises': ['error'],
+    '@typescript-eslint/array-type': [0, 'generic'],
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/ban-ts-ignore': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      {
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true,
+      },
+    ],
+  },
+}
