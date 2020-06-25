76acf4895bdc9a3386afa096aad7b8f51e7703b4
Tai Nguyen Bui <tai@theagilemonkeys.com>
2020-06-25 12:46:22 +0100

    BOOST-472 add required env variable for testing

--- packages/framework-provider-local/src/services/graphql-service.ts
+++ packages/framework-provider-local/src/services/graphql-service.ts
@@ -1,8 +1,7 @@
-import { boosterServeGraphQL } from '@boostercloud/framework-core/dist'
+import { boosterServeGraphQL } from '@boostercloud/framework-core'
 
 export class GraphQLService {
-  public constructor() {
-  }
+  public constructor() {}
 
   public async query(request: any): Promise<any> {
     return await boosterServeGraphQL(request)
