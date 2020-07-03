f9b5859c9191709c22f90b8912dd8a0e239d970d
alvaroloes <alvaro@booster.cloud>
2020-07-03 13:53:50 +0100

    Add missing tests for partition-keys.ts

--- packages/framework-provider-aws/test/library/partition-keys.test.ts
+++ packages/framework-provider-aws/test/library/partition-keys.test.ts
@@ -1,16 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { expect } from '../expect'
-import { partitionKeyForEvent } from '../../src/library/partition-keys'
+import { partitionKeyForEvent, sortKeyForSubscription } from '../../src/library/partition-keys'
 import { EventEnvelope } from '@boostercloud/framework-types'
+import { lorem, random } from 'faker'
 
 describe('"partitionKeyForEvent" function', () => {
-  it('returns a correctly formated partition key', () => {
-    const entityName = 'name'
-    const entityID = 'id'
+  it('returns a correctly formatted partition key', () => {
+    const entityName = lorem.word()
+    const entityID = random.uuid()
     const kind: EventEnvelope['kind'] = 'snapshot'
 
     const expected = `${entityName}-${entityID}-${kind}`
     const got = partitionKeyForEvent(entityName, entityID, kind)
+
+    expect(got).to.equal(expected)
+  })
+})
+
+describe('"sortKeyForSubscription" function', () => {
+  it('returns a correctly formatted sortKey key', () => {
+    const connectionID = random.uuid()
+    const subscriptionID = random.uuid()
+
+    const expected = `${connectionID}-${subscriptionID}`
+    const got = sortKeyForSubscription(connectionID, subscriptionID)
 
     expect(got).to.equal(expected)
   })
