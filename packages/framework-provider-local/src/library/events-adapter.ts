fc2acb6c20233113e6a46768f13b693a5bd5b866
Tai Nguyen Bui <tai@theagilemonkeys.com>
2020-07-03 13:40:44 +0100

    BOOST-473 test: adds tests to make sure that we store an event in the database when we fire a command

--- packages/framework-provider-local/src/library/events-adapter.ts
+++ packages/framework-provider-local/src/library/events-adapter.ts
@@ -1,6 +1,10 @@
 import { Logger, BoosterConfig, EventEnvelope } from '@boostercloud/framework-types'
 import { EventRegistry } from '..'
 
+export function rawEventsToEnvelopes(rawEvents: Array<any>): Array<EventEnvelope> {
+  return rawEvents.map((event) => event as EventEnvelope)
+}
+
 export async function storeEvents(
   eventsStream: EventRegistry,
   eventEnvelopes: Array<EventEnvelope>,
