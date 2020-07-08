66d3d8f572e98e71679df24dceda3d9ca1c03ef3
Tai Nguyen Bui <tai@theagilemonkeys.com>
2020-07-08 12:47:34 +0100

    BOOST-473 fixes missing comma

--- packages/framework-integration-tests/integration/providers/local/commands.integration.ts
+++ packages/framework-integration-tests/integration/providers/local/commands.integration.ts
@@ -63,7 +63,7 @@ describe('commands', () => {
 
       // Wait until event is stored in database
       await waitForIt(
-        async () => events.loadDatabase()
+        async () => events.loadDatabase(),
         (_) => events.getAllData().some((value) => value.entityID === mockCartId)
       )
 
