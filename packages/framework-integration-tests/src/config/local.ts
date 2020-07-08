89113b0bdffc8ae7a38e33a3b9ec9b28c9753fdc
Tai Nguyen Bui <tai@theagilemonkeys.com>
2020-07-08 17:28:32 +0100

    BOOST-750 refactor: define environment configurations to match booster project initializer

--- packages/framework-integration-tests/src/config/local.ts
+++
@@ -1,8 +0,0 @@
-import { Booster } from '@boostercloud/framework-core'
-import { BoosterConfig } from '@boostercloud/framework-types'
-import { Provider } from '@boostercloud/framework-provider-local'
-
-Booster.configure('local', (config: BoosterConfig): void => {
-  config.appName = 'my-store'
-  config.provider = Provider
-})
