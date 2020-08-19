ba4116dd9096968f2b776dfd2cc0def7f960f91c
Mario Castro Squella <mario@theagilemonkeys.com>
2020-08-19 10:20:32 -0400

    Refactored API adapter tests

--- packages/framework-provider-azure/test/library/api-adapter.test.ts
+++ packages/framework-provider-azure/test/library/api-adapter.test.ts
@@ -17,45 +17,44 @@ describe('API adapter', () => {
       }
     }
 
-    it('Returns a proper body with several errors', async () => {
-      const testCases: Array<TestCase> = [
-        {
-          input: new InvalidParameterError('error message'),
-          expectedOutput: {
-            status: 400,
-            title: 'Invalid Parameter Error',
-          },
+    const testCases: Array<TestCase> = [
+      {
+        input: new InvalidParameterError('error message'),
+        expectedOutput: {
+          status: 400,
+          title: 'Invalid Parameter Error',
         },
-        {
-          input: new NotAuthorizedError('error message'),
-          expectedOutput: {
-            status: 401,
-            title: 'Not Authorized Error',
-          },
+      },
+      {
+        input: new NotAuthorizedError('error message'),
+        expectedOutput: {
+          status: 401,
+          title: 'Not Authorized Error',
         },
-        {
-          input: new NotFoundError('error message'),
-          expectedOutput: {
-            status: 404,
-            title: 'Not Found Error',
-          },
+      },
+      {
+        input: new NotFoundError('error message'),
+        expectedOutput: {
+          status: 404,
+          title: 'Not Found Error',
         },
-        {
-          input: new InvalidVersionError('error message'),
-          expectedOutput: {
-            status: 422,
-            title: 'Invalid Version Error',
-          },
+      },
+      {
+        input: new InvalidVersionError('error message'),
+        expectedOutput: {
+          status: 422,
+          title: 'Invalid Version Error',
         },
-      ]
+      },
+    ]
 
-      for (const testCase of testCases) {
-        const testDescription = `In test case '${testCase.input.constructor.name}'`
+    for (const testCase of testCases) {
+      it(`returns the proper body for error '${testCase.input.constructor.name}'`, async () => {
         const got = await requestFailed(testCase.input)
-        expect(got.status).to.be.equal(testCase.expectedOutput.status, testDescription)
+        expect(got.status).to.be.equal(testCase.expectedOutput.status)
         const body = JSON.parse(got.body)
-        expect(body.title).to.be.equal(testCase.expectedOutput.title, testDescription)
-      }
-    })
+        expect(body.title).to.be.equal(testCase.expectedOutput.title)
+      })
+    }
   })
 })
