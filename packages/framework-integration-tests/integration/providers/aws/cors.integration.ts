c195b541612f18767118273d7b441ee7becec936
Johan Sebastian Cortes Montenegro <johan@theagilemonkeys.com>
2020-08-27 19:44:48 +0100

    add support for the preflight request to /auth/sign-in and /auth/sign-out

--- packages/framework-integration-tests/integration/providers/aws/cors.integration.ts
+++ packages/framework-integration-tests/integration/providers/aws/cors.integration.ts
@@ -1,21 +1,24 @@
-import {authClientID, createPassword, signUpURL} from './utils'
-import {internet} from 'faker'
+import { authClientID, createPassword, signInURL, signOutURL, signUpURL } from './utils'
+import { internet } from 'faker'
 import fetch from 'cross-fetch'
-import {expect} from '@boostercloud/framework-provider-aws/test/expect'
+import { expect } from '@boostercloud/framework-provider-aws/test/expect'
 
 describe('The Authentication API', () => {
   let clientId: string
+  const methodsToCheck = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
+  const preflightOptions = generatePreflightOptionsList(methodsToCheck)
+
   before(async () => {
     clientId = await authClientID()
-  });
+  })
 
   context('/auth/sign-up', () => {
     let signUpUrl: string
-    let validSignUpBody: string;
+    let validSignUpBody: string
     const invalidSignUpBody = JSON.stringify({})
 
     before(async () => {
-      signUpUrl = await signUpURL();
+      signUpUrl = await signUpURL()
       validSignUpBody = JSON.stringify({
         clientId: clientId,
         username: internet.email(),
@@ -23,36 +26,14 @@ describe('The Authentication API', () => {
         userAttributes: {
           role: 'UserWithEmail',
         },
-      });
-    });
+      })
+    })
 
     context('OPTIONS', () => {
-      function methodToPreflightOptions(methodToPreflight: string): RequestInit {
-        return {
-          method: 'OPTIONS',
-          headers: {
-            'Access-Control-Request-Method': methodToPreflight,
-            'Access-Control-Request-Headers': 'X-any-header',
-            Origin: internet.url(),
-          },
-        }
-      }
-
-      const httpMethodsToPreflight = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
-
       it('should allow all the headers and methods regardless the requests values', async () => {
-        const optionsList = httpMethodsToPreflight.map(methodToPreflightOptions)
+        const responses = await Promise.all(preflightOptions.map(performPreflightRequest(signUpUrl)))
 
-        const responses = await Promise.all(optionsList.map((options) => fetch(signUpUrl, options)))
-
-        responses.forEach((response) => {
-          expect(response.status).to.be.eq(204)
-          expect(response.headers.get('Access-Control-Allow-Origin')).to.be.eq('*')
-          expect(response.headers.get('Access-Control-Allow-Headers')).to.be.eq('*')
-          expect(response.headers.get('Access-Control-Allow-Methods'))
-            .to.include('OPTIONS')
-            .and.to.include('POST')
-        })
+        responses.forEach(assertResponseContainsPreflightHeaders)
       })
     })
 
@@ -61,12 +42,12 @@ describe('The Authentication API', () => {
         const response = await fetch(signUpUrl, {
           method: 'POST',
           headers: {
-            'Content-Type': 'Application/json'
+            'Content-Type': 'Application/json',
           },
           body: validSignUpBody,
         })
 
-        expect(response.status).to.be.eq(200, `Response body was: ${JSON.stringify((await response.json()))}`)
+        expect(response.status).to.be.eq(200, `Response body was: ${JSON.stringify(await response.json())}`)
         expect(response.headers.get('Access-Control-Allow-Origin')).to.be.eq('*')
       })
 
@@ -74,7 +55,7 @@ describe('The Authentication API', () => {
         const response = await fetch(signUpUrl, {
           method: 'POST',
           headers: {
-            'Content-Type': 'Application/json'
+            'Content-Type': 'Application/json',
           },
           body: invalidSignUpBody,
         })
@@ -85,4 +66,63 @@ describe('The Authentication API', () => {
       it('should return the Access-Control-Allow-Origin header for 500 responses')
     })
   })
+
+  context('/auth/sign-in', () => {
+    let signInUrl: string
+
+    before(async () => {
+      signInUrl = await signInURL()
+    })
+
+    context('OPTIONS', () => {
+      it('should allow all the headers and methods regardless the requests values', async () => {
+        const responses = await Promise.all(preflightOptions.map(performPreflightRequest(signInUrl)))
+
+        responses.forEach(assertResponseContainsPreflightHeaders)
+      })
+    })
+  })
+
+  context('/auth/sign-out', () => {
+    let signOutUrl: string
+
+    before(async () => {
+      signOutUrl = await signOutURL()
+    })
+
+    context('OPTIONS', () => {
+      it('should allow all the headers and methods regardless the requests values', async () => {
+        const responses = await Promise.all(preflightOptions.map(performPreflightRequest(signOutUrl)))
+
+        responses.forEach(assertResponseContainsPreflightHeaders)
+      })
+    })
+  })
+
+  function generatePreflightOptionsList(desiredHttpMethods: string[]): RequestInit[] {
+    // For more info about preflight requests see: https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+    return desiredHttpMethods.map(
+      (method: string): RequestInit => ({
+        method: 'OPTIONS',
+        headers: {
+          'Access-Control-Request-Method': method,
+          'Access-Control-Request-Headers': 'X-any-header',
+          Origin: internet.url(),
+        },
+      })
+    )
+  }
+
+  function performPreflightRequest(url: string) {
+    return (options: RequestInit) => fetch(url, options)
+  }
+
+  function assertResponseContainsPreflightHeaders(response: Response) {
+    expect(response.status).to.be.eq(204)
+    expect(response.headers.get('Access-Control-Allow-Origin')).to.be.eq('*')
+    expect(response.headers.get('Access-Control-Allow-Headers')).to.be.eq('*')
+    expect(response.headers.get('Access-Control-Allow-Methods'))
+      .to.include('OPTIONS')
+      .and.to.include('POST')
+  }
 })
