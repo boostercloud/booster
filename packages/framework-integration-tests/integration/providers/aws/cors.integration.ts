e03f8d4435191f9a1b642f5f349127f87e9e2895
Johan Sebastian Cortes Montenegro <johan@theagilemonkeys.com>
2020-08-27 20:38:17 +0100

    test post requests to /auth/sign-in and /auth/sign-out return the allow origin

--- packages/framework-integration-tests/integration/providers/aws/cors.integration.ts
+++ packages/framework-integration-tests/integration/providers/aws/cors.integration.ts
@@ -3,28 +3,30 @@ import { internet } from 'faker'
 import fetch from 'cross-fetch'
 import { expect } from '@boostercloud/framework-provider-aws/test/expect'
 
-describe('The Authentication API', () => {
+describe('Given the Authentication API', () => {
   let clientId: string
-  const methodsToCheck = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
-  const preflightOptions = generatePreflightOptionsList(methodsToCheck)
-
+  const username = internet.email()
+  const password = createPassword()
+  const role = 'SuperUserNoConfirmation'
   before(async () => {
     clientId = await authClientID()
   })
 
-  context('/auth/sign-up', () => {
+  context('When /auth/sign-up', () => {
     let signUpUrl: string
-    let validSignUpBody: string
-    const invalidSignUpBody = JSON.stringify({})
+    let validAuthBody: string
+    const invalidAuthBody = JSON.stringify({})
+    const methodsToCheck = ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH']
+    const preflightOptions = generatePreflightOptionsList(methodsToCheck)
 
     before(async () => {
       signUpUrl = await signUpURL()
-      validSignUpBody = JSON.stringify({
-        clientId: clientId,
-        username: internet.email(),
-        password: createPassword(),
+      validAuthBody = JSON.stringify({
+        clientId,
+        username,
+        password,
         userAttributes: {
-          role: 'UserWithEmail',
+          role,
         },
       })
     })
@@ -44,11 +46,10 @@ describe('The Authentication API', () => {
           headers: {
             'Content-Type': 'Application/json',
           },
-          body: validSignUpBody,
+          body: validAuthBody,
         })
 
-        expect(response.status).to.be.eq(200, `Response body was: ${JSON.stringify(await response.json())}`)
-        expect(response.headers.get('Access-Control-Allow-Origin')).to.be.eq('*')
+        await verifyResponseAndAllowedOriginHeader(response, 200, '*', await response.json())
       })
 
       it('should return the Access-Control-Allow-Origin header for 400 responses', async () => {
@@ -57,72 +58,127 @@ describe('The Authentication API', () => {
           headers: {
             'Content-Type': 'Application/json',
           },
-          body: invalidSignUpBody,
+          body: invalidAuthBody,
         })
 
-        expect(response.status).to.be.eq(400)
-        expect(response.headers.get('Access-Control-Allow-Origin')).to.be.eq('*')
+        await verifyResponseAndAllowedOriginHeader(response, 400, '*', await response.json())
       })
+
       it('should return the Access-Control-Allow-Origin header for 500 responses')
     })
-  })
 
-  context('/auth/sign-in', () => {
-    let signInUrl: string
+    context('And then /auth/sign-in', () => {
+      let signInUrl: string
+      let accessToken: string
 
-    before(async () => {
-      signInUrl = await signInURL()
-    })
+      before(async () => {
+        signInUrl = await signInURL()
+      })
 
-    context('OPTIONS', () => {
-      it('should allow all the headers and methods regardless the requests values', async () => {
-        const responses = await Promise.all(preflightOptions.map(performPreflightRequest(signInUrl)))
+      context('OPTIONS', () => {
+        it('should allow all the headers and methods regardless the requests values', async () => {
+          const responses = await Promise.all(preflightOptions.map(performPreflightRequest(signInUrl)))
 
-        responses.forEach(assertResponseContainsPreflightHeaders)
+          responses.forEach(assertResponseContainsPreflightHeaders)
+        })
+      })
+
+      context('POST', () => {
+
+        it('should return the Access-Control-Allow-Origin header for 200 responses', async () => {
+          const response = await fetch(signInUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'Application/json' },
+            body: validAuthBody,
+          })
+          const jsonBody = await response.json()
+          accessToken = jsonBody['accessToken']
+
+          await verifyResponseAndAllowedOriginHeader(response, 200, '*', jsonBody)
+        })
+        it('should return the Access-Control-Allow-Origin header for 400 responses', async () => {
+          const response = await fetch(signInUrl, {
+            method: 'POST',
+            headers: { 'Content-Type': 'Application/json' },
+            body: invalidAuthBody,
+          })
+
+          await verifyResponseAndAllowedOriginHeader(response, 400, '*', await response.json())
+        })
+        it('should return the Access-Control-Allow-Origin header for 500 responses')
+      })
+
+      context('And then /auth/sign-out', () => {
+        let signOutUrl: string
+
+        before(async () => {
+          signOutUrl = await signOutURL()
+        })
+
+        context('OPTIONS', () => {
+          it('should allow all the headers and methods regardless the requests values', async () => {
+            const responses = await Promise.all(preflightOptions.map(performPreflightRequest(signOutUrl)))
+
+            responses.forEach(assertResponseContainsPreflightHeaders)
+          })
+        })
+
+        context('POST', () => {
+          it('should return the Access-Control-Allow-Origin header for 200 responses', async () => {
+            const response = await fetch(signOutUrl, {
+              method: 'POST',
+              headers: { 'Content-Type': 'Application/json' },
+              body: JSON.stringify({
+                accessToken: accessToken,
+              }),
+            })
+
+            await verifyResponseAndAllowedOriginHeader(response, 200, '*', await response.json())
+          })
+          it('should return the Access-Control-Allow-Origin header for 400 responses', async () => {
+            const response = await fetch(signOutUrl, {
+              method: 'POST',
+              headers: { 'Content-Type': 'Application/json' },
+              body: invalidAuthBody,
+            })
+
+            await verifyResponseAndAllowedOriginHeader(response, 400, '*', await response.json())
+          })
+          it('should return the Access-Control-Allow-Origin header for 500 responses')
+        })
       })
     })
+
+    function generatePreflightOptionsList(desiredHttpMethods: string[]): RequestInit[] {
+      // For more info about preflight requests see: https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
+      return desiredHttpMethods.map(
+        (method: string): RequestInit => ({
+          method: 'OPTIONS',
+          headers: {
+            'Access-Control-Request-Method': method,
+            'Access-Control-Request-Headers': 'X-any-header',
+            Origin: internet.url(),
+          },
+        }),
+      )
+    }
+
+    function performPreflightRequest(url: string) {
+      return (options: RequestInit) => fetch(url, options)
+    }
+
+    function assertResponseContainsPreflightHeaders(response: Response) {
+      expect(response.status).to.be.eq(204)
+      expect(response.headers.get('Access-Control-Allow-Origin')).to.be.eq('*')
+      expect(response.headers.get('Access-Control-Allow-Headers')).to.be.eq('*')
+      expect(response.headers.get('Access-Control-Allow-Methods'))
+        .to.include('OPTIONS')
+        .and.to.include('POST')
+    }
+
+    async function verifyResponseAndAllowedOriginHeader(response: Response, expectedHttpStatus: number, expectedAllowedOrigin: string, jsonBody: any) {
+      expect(response.status).to.be.eq(expectedHttpStatus, `Response body was: ${JSON.stringify(jsonBody)}`)
+      expect(response.headers.get('Access-Control-Allow-Origin')).to.be.eq(expectedAllowedOrigin)
+    }
   })
-
-  context('/auth/sign-out', () => {
-    let signOutUrl: string
-
-    before(async () => {
-      signOutUrl = await signOutURL()
-    })
-
-    context('OPTIONS', () => {
-      it('should allow all the headers and methods regardless the requests values', async () => {
-        const responses = await Promise.all(preflightOptions.map(performPreflightRequest(signOutUrl)))
-
-        responses.forEach(assertResponseContainsPreflightHeaders)
-      })
-    })
-  })
-
-  function generatePreflightOptionsList(desiredHttpMethods: string[]): RequestInit[] {
-    // For more info about preflight requests see: https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
-    return desiredHttpMethods.map(
-      (method: string): RequestInit => ({
-        method: 'OPTIONS',
-        headers: {
-          'Access-Control-Request-Method': method,
-          'Access-Control-Request-Headers': 'X-any-header',
-          Origin: internet.url(),
-        },
-      })
-    )
-  }
-
-  function performPreflightRequest(url: string) {
-    return (options: RequestInit) => fetch(url, options)
-  }
-
-  function assertResponseContainsPreflightHeaders(response: Response) {
-    expect(response.status).to.be.eq(204)
-    expect(response.headers.get('Access-Control-Allow-Origin')).to.be.eq('*')
-    expect(response.headers.get('Access-Control-Allow-Headers')).to.be.eq('*')
-    expect(response.headers.get('Access-Control-Allow-Methods'))
-      .to.include('OPTIONS')
-      .and.to.include('POST')
-  }
 })
