8f8fbf7371d257955ea7405b67b9bc29d10bfa92
charlietfe <carlos@theagilemonkeys.com>
2020-09-02 14:28:58 +0100

    moved destroy method to its own file

--- packages/framework-core/src/entity-destroyer.ts
+++ packages/framework-core/src/entity-destroyer.ts
@@ -1,13 +1,12 @@
 import { UUID, BoosterConfig, EntityInterface, Logger, Class } from '@boostercloud/framework-types'
 import { EventStore } from './services/event-store'
 
-export async function fetchEntitySnapshot<TEntity extends EntityInterface>(
+export async function destroyEntity<TEntity extends EntityInterface>(
   config: BoosterConfig,
   logger: Logger,
   entityClass: Class<TEntity>,
   entityID: UUID
-): Promise<TEntity | undefined> {
+): Promise<void> {
   const eventStore = new EventStore(config, logger)
-  const entitySnapshotEnvelope = await eventStore.fetchEntitySnapshot(entityClass.name, entityID)
-  return entitySnapshotEnvelope?.value as TEntity
+  return await eventStore.destroyEntity(entityClass.name, entityID)
 }
