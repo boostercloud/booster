4499fb1150c7621601e626a7587272210abb0b94
juanjoman <j2rgez@gmail.com>
2020-09-10 10:50:41 +0200

    Finish refactoring tests

--- packages/cli/test/commands/new.test.ts
+++ packages/cli/test/commands/new.test.ts
@@ -1,12 +1,10 @@
 import { ProjectInitializerConfig } from '../../src/services/project-initializer'
 import * as fs from 'fs'
 import { restore, replace, fake, spy } from 'sinon'
-import ErrnoException = NodeJS.ErrnoException
 import * as ProjectInitializer from '../../src/services/project-initializer'
 import * as Project from '../../src/commands/new/project'
 import { IConfig } from '@oclif/config'
 import { expect } from '../expect'
-import sinon = require('sinon')
 
 describe('new', (): void => {
   describe('project', () => {
@@ -28,25 +26,26 @@ describe('new', (): void => {
         ],
         src: ['commands', 'events', 'event-handlers', 'entities', 'read-models', 'config', 'common', 'index.ts'],
       }
-      const projectInitializerConfig = {
+      const defaultProjectInitializerConfig = {
         projectName: projectName,
-        description: '0.1.0',
+        description: '',
         version: '0.1.0',
-        author: 'superAuthor',
+        author: '',
         homepage: '',
         license: 'MIT',
         repository: '',
-        providerPackageName: '@boostercloud/framework-provider-aws',
+        providerPackageName: defaultProvider,
         boosterVersion: '0.5.1',
+        default: true,
       } as ProjectInitializerConfig
 
       afterEach(() => {
-        fs.rmdir(projectDirectory, { recursive: true }, (e: ErrnoException | null) => console.error(e))
+        fs.rmdirSync(projectDirectory, { recursive: true })
         restore()
       })
 
       it('generates all required files and folders', async () => {
-        replace(Project, 'parseConfig', fake.returns(projectInitializerConfig))
+        replace(Project, 'parseConfig', fake.returns(defaultProjectInitializerConfig))
         replace(ProjectInitializer, 'installDependencies', fake.returns({}))
         expect(fs.existsSync(projectDirectory)).to.be.false
 
@@ -58,33 +57,20 @@ describe('new', (): void => {
       })
 
       it('generates project with default parameters when using --default flag', async () => {
+        const parseConfigSpy = spy(Project, 'parseConfig')
         replace(ProjectInitializer, 'installDependencies', fake.returns({}))
         expect(fs.existsSync(projectDirectory)).to.be.false
-        const parseConfigSpy = spy(Project, 'parseConfig')
 
-        await new Project.default([projectName, '--default'], {} as IConfig).run()
-        // TODO: check spy (not checking returned value properly) and booster version issue
+        await new Project.default([projectName, '--default'], { version: '0.5.1' } as IConfig).run()
+
         expect(fs.existsSync(projectDirectory)).to.be.true
-        expect(parseConfigSpy).to.have.returned(
-          sinon.match.same(
-            Promise.resolve({
-              projectName: projectName,
-              providerPackageName: defaultProvider,
-              description: '',
-              version: '0.1.0',
-              author: '',
-              homepage: '',
-              license: 'MIT',
-              repository: '',
-              boosterVersion: '0.5.1',
-              default: true,
-            })
-          )
-        ).and.to.have.been.calledOnce
+        expect(parseConfigSpy).to.have.been.calledOnce
+        expect(await parseConfigSpy.firstCall.returnValue).to.be.deep.equal(
+          defaultProjectInitializerConfig as ProjectInitializerConfig
+        )
 
         const packageJson = JSON.parse(fs.readFileSync(`${projectDirectory}/package.json`).toString())
         expect(packageJson.name).to.be.equal(projectName)
-        expect(packageJson.dependencies[defaultProvider]).to.not.be.undefined
         expect(packageJson.dependencies[defaultProvider]).to.be.equal('*')
         expect(packageJson.description).to.be.equal('')
         expect(packageJson.version).to.be.equal('0.1.0')
@@ -92,7 +78,6 @@ describe('new', (): void => {
         expect(packageJson.homepage).to.be.equal('')
         expect(packageJson.license).to.be.equal('MIT')
         expect(packageJson.repository).to.be.equal('')
-        expect(packageJson.boosterVersion).to.be.equal('0.5.1')
       })
     })
   })
