ba2fa22c699e1b40df3be637813b94f2094365af
adriangmweb <adriangonzalezmesa@gmail.com>
2020-09-15 17:21:50 +0100

    add CheckCartCount to integration tests

---
+++ packages/framework-integration-tests/src/scheduled-commands/CheckCartCount.ts
@@ -0,0 +1,11 @@
+import { ScheduledCommand } from '@boostercloud/framework-core'
+
+@ScheduledCommand({
+  minute: '0/5',
+})
+export class CheckCartCount {
+  public static async handle(): Promise<void> {
+    console.log('This is an ScheduledCommand running')
+    await Promise.resolve('this is fine!')
+  }
+}
