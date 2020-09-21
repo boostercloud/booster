0f14fd982fd74139ada5833ae116ea637d7e79ab
Javier Toledo <javier@booster.cloud>
2020-09-21 16:30:00 +0100

    Ooops

--- packages/framework-provider-aws-infrastructure/src/index.ts
+++ packages/framework-provider-aws-infrastructure/src/index.ts
@@ -1,7 +1,7 @@
 import { ProviderInfrastructure, PluginDescriptor, BoosterConfig } from '@boostercloud/framework-types'
 import { loadPlugin } from '@boostercloud/framework-provider-aws-infrastructure/src/infrastructure-plugin'
 import { deploy, nuke } from './infrastructure'
-export { InfrastructurePlugin } from '@boostercloud/framework-provider-aws-infrastructure/src/infrastructure-plugin'
+export { InfrastructurePlugin } from './infrastructure-plugin'
 
 export const Infrastructure = (pluginDescriptors?: PluginDescriptor[]): ProviderInfrastructure => ({
   deploy: (config: BoosterConfig) => {
