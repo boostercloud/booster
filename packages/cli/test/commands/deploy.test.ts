fb9e6a23ab256909c9bbaa0d2bc739f2cc1db83d
sanskar-p <sanskarpatro2012@gmail.com>
2020-10-13 23:38:13 +0530

    added unit tests for pruneDependencies

--- packages/cli/test/commands/deploy.test.ts
+++ packages/cli/test/commands/deploy.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { expect } from '../expect'
 import { fancy } from 'fancy-test'
-import { restore, fake } from 'sinon'
+import { restore, fake, spy } from 'sinon'
 import { ProviderLibrary, Logger } from '@boostercloud/framework-types'
 import { test } from '@oclif/test'
 
@@ -38,6 +38,33 @@ describe('deploy', () => {
 
         await expect(runTasks('test-env', fakeLoader, fakeDeployer)).to.eventually.be.rejectedWith(msg)
         expect(fakeDeployer).not.to.have.been.called
+      })
+    })
+
+    context('when it prunes dev dependencies', () => {
+      fancy.stdout().it('calls pruneDependencies function', async (ctx) => {
+        fancy.stdout().it('pruned', async () => {
+          const fakeProvider = {} as ProviderLibrary
+
+        const fakeLoader = fake.resolves({
+          provider: fakeProvider,
+          appName: 'fake app',
+          region: 'tunte',
+          entities: {},
+        })
+
+        const fakeDeployer = fake((_config: unknown, logger: Logger) => {
+          logger.info('this is a progress update')
+        })
+
+        const pruneDependenciesSpy = spy(deploy, deploy.pruneDependencies)
+        await runTasks('test-env', fakeLoader, fakeDeployer)
+        
+        expect(pruneDependenciesSpy).to.have.been.calledOnce
+
+        expect(ctx.stdout).to.include('Deployment complete')
+        expect(fakeDeployer).to.have.been.calledOnce
+      })
       })
     })
 
