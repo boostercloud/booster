eaa4d1389959526936d88b757c32227920ef878d
Javier Toledo <javier@booster.cloud>
2020-12-01 10:38:14 +0000

    Make sure that dependencies are installed fefore running `lerna run clean` in the `check-all-the-things.*` scripts

--- scripts/check-all-the-things.ps1
+++ scripts/check-all-the-things.ps1
@@ -3,8 +3,8 @@
 # with Powershell <7.0 (what most Windows users have preinstalled)
 # we write it in the old-school style
 lerna clean --yes
-lerna run clean --stream
 if ($?) { lerna bootstrap }
+lerna run clean --stream
 if ($?) { lerna run compile --stream }
 if ($?) { lerna run lint:fix --stream }
 if ($?) { lerna run lint:check --stream }
--- scripts/check-all-the-things.sh
+++ scripts/check-all-the-things.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 lerna clean --yes \
-&& lerna run clean --stream \
 && lerna bootstrap \
+&& lerna run clean --stream \
 && lerna run compile --stream \
 && lerna run lint:fix --stream \
 && lerna run lint:check --stream \
