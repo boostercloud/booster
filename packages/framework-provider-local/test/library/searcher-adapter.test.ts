9b222911545772acc98653addf50f1aaecc0429a
Nikita Tchayka <nick@booster.cloud>
2020-12-02 17:15:24 +0000

    Test != operator

--- packages/framework-provider-local/test/library/searcher-adapter.test.ts
+++ packages/framework-provider-local/test/library/searcher-adapter.test.ts
@@ -4,9 +4,13 @@ import { queryRecordFor } from '../../src/library/searcher-adapter'
 describe('searcher-adapter', () => {
   const typeName = 'SomeReadModel'
   describe('filter conversion for keys to NeDB queries', () => {
-    it('converts the "eq" operator', () => {
+    it('converts the "=" operator', () => {
       const result = queryRecordFor(typeName, { field: { operation: '=', values: ['one'] } })
       expect(result).to.deep.equal({ 'value.field': 'one', typeName })
+    })
+    it('converts the "!=" operator', () => {
+      const result = queryRecordFor(typeName, { field: { operation: '!=', values: ['one'] } })
+      expect(result).to.deep.equal({ 'value.field': { $ne: 'one' }, typeName })
     })
   })
 })
