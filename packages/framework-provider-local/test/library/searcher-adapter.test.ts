056f4e23c1a048e49cf91d948ded0955e7e8a85e
Nikita Tchayka <nick@booster.cloud>
2020-12-02 17:29:50 +0000

    Test the rest of the simple operators

--- packages/framework-provider-local/test/library/searcher-adapter.test.ts
+++ packages/framework-provider-local/test/library/searcher-adapter.test.ts
@@ -8,9 +8,40 @@ describe('searcher-adapter', () => {
       const result = queryRecordFor(typeName, { field: { operation: '=', values: ['one'] } })
       expect(result).to.deep.equal({ 'value.field': 'one', typeName })
     })
+
     it('converts the "!=" operator', () => {
       const result = queryRecordFor(typeName, { field: { operation: '!=', values: ['one'] } })
       expect(result).to.deep.equal({ 'value.field': { $ne: 'one' }, typeName })
+    })
+
+    it('converts the "<" operator', () => {
+      const result = queryRecordFor(typeName, { field: { operation: '<', values: ['one'] } })
+      expect(result).to.deep.equal({ 'value.field': { $lt: 'one' }, typeName })
+    })
+
+    it('converts the ">" operator', () => {
+      const result = queryRecordFor(typeName, { field: { operation: '>', values: ['one'] } })
+      expect(result).to.deep.equal({ 'value.field': { $gt: 'one' }, typeName })
+    })
+
+    it('converts the "<=" operator', () => {
+      const result = queryRecordFor(typeName, { field: { operation: '<=', values: ['one'] } })
+      expect(result).to.deep.equal({ 'value.field': { $lte: 'one' }, typeName })
+    })
+
+    it('converts the ">=" operator', () => {
+      const result = queryRecordFor(typeName, { field: { operation: '>=', values: ['one'] } })
+      expect(result).to.deep.equal({ 'value.field': { $gte: 'one' }, typeName })
+    })
+
+    it('converts the "in" operator', () => {
+      const result = queryRecordFor(typeName, { field: { operation: 'in', values: ['one'] } })
+      expect(result).to.deep.equal({ 'value.field': { $in: ['one'] }, typeName })
+    })
+
+    it('converts the "between" operator', () => {
+      const result = queryRecordFor(typeName, { field: { operation: 'between', values: ['one', 'two'] } })
+      expect(result).to.deep.equal({ 'value.field': { $gt: 'one', $lte: 'two' }, typeName })
     })
   })
 })
