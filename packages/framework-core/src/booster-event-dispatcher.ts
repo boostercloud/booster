0ba74d251b8b4fcb9776c9ceac0c087685aa3a39
adriangmweb <adriangonzalezmesa@gmail.com>
2020-12-18 15:25:45 +0000

    Use promise.allSettled and log rejected promises

--- packages/framework-core/src/booster-event-dispatcher.ts
+++ packages/framework-core/src/booster-event-dispatcher.ts
@@ -37,7 +37,7 @@ export class BoosterEventDispatcher {
         case 'event':
           logger.debug('[BoosterEventDispatcher#eventProcessor]: Started processing workflow for event:', eventEnvelope)
           // TODO: Separate into two independent processes the snapshotting/read-model generation process from the event handling process
-          await Promise.allSettled([
+          await Promise.all([
             BoosterEventDispatcher.snapshotAndUpdateReadModels(eventEnvelope, eventStore, readModelStore, logger),
             BoosterEventDispatcher.handleEvent(eventEnvelope, config, logger),
           ])
@@ -87,6 +87,6 @@ export class BoosterEventDispatcher {
     )
     results
       .filter((result) => result.status === 'rejected')
-      .forEach((result) => logger.debug(`Event handler execution failed: ${(result as PromiseRejectedResult).reason}`))
+      .forEach((result) => logger.debug(`Event handlere execution failed: ${(result as PromiseRejectedResult).reason}`))
   }
 }
