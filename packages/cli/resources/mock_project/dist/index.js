05b84556c925c2e7ace7f8cf45ac3a84d2d8bd46
David Verdú <davidverdu@theagilemonkeys.com>
2021-01-13 07:38:16 +0000

    cli: add mock project ignored files to repository

---
+++ packages/cli/resources/mock_project/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const framework_core_1 = require("@boostercloud/framework-core");
+var framework_core_2 = require("@boostercloud/framework-core");
+Object.defineProperty(exports, "Booster", { enumerable: true, get: function () { return framework_core_2.Booster; } });
+Object.defineProperty(exports, "boosterEventDispatcher", { enumerable: true, get: function () { return framework_core_2.boosterEventDispatcher; } });
+Object.defineProperty(exports, "boosterPreSignUpChecker", { enumerable: true, get: function () { return framework_core_2.boosterPreSignUpChecker; } });
+Object.defineProperty(exports, "boosterServeGraphQL", { enumerable: true, get: function () { return framework_core_2.boosterServeGraphQL; } });
+Object.defineProperty(exports, "boosterRequestAuthorizer", { enumerable: true, get: function () { return framework_core_2.boosterRequestAuthorizer; } });
+Object.defineProperty(exports, "boosterNotifySubscribers", { enumerable: true, get: function () { return framework_core_2.boosterNotifySubscribers; } });
+framework_core_1.Booster.start();
