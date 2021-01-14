aa90e7b888eaa680295d7938f2111c8b6494a0ab
charlietfe <carlos@theagilemonkeys.com>
2021-01-14 21:13:22 +0000

    refactor

--- packages/rocket-auth-aws-infrastructure/src/auth-stack.ts
+++ packages/rocket-auth-aws-infrastructure/src/auth-stack.ts
@@ -8,7 +8,7 @@ import {
   UserPoolTriggers,
 } from '@aws-cdk/aws-cognito'
 import { Effect, PolicyStatement } from '@aws-cdk/aws-iam'
-import { Cors, CorsOptions, LambdaIntegration, MethodOptions, RestApi } from '@aws-cdk/aws-apigateway'
+import { Cors, CorsOptions, LambdaIntegration, MethodOptions, Resource, RestApi } from '@aws-cdk/aws-apigateway'
 import { createLamba } from './utils'
 
 export interface AWSAuthRocketParams {
@@ -131,6 +131,127 @@ export class AuthStack {
 
     const authResource = rootAuthAPI.root.addResource('auth')
 
+    const defaultCorsPreflightOptions: CorsOptions = {
+      allowHeaders: ['*'],
+      allowOrigins: Cors.ALL_ORIGINS,
+      allowMethods: ['POST', 'OPTIONS'],
+    }
+
+    // sign-in
+    const signInResource = authResource.addResource('sign-in', { defaultCorsPreflightOptions })
+    AuthStack.addIntegration('sign-in', stack, params, userPool, userPoolClientId, signInResource, 'sign-in.handler', [
+      'cognito-idp:InitiateAuth',
+    ])
+
+    // sign-up
+    // sign-up/confirm
+    // sign-up/resend-code
+
+    const signUpResource = authResource.addResource('sign-up', { defaultCorsPreflightOptions })
+    AuthStack.addIntegration('sign-up', stack, params, userPool, userPoolClientId, signUpResource, 'sign-up.handler', [
+      'cognito-idp:SignUp',
+    ])
+
+    let resource = signUpResource.addResource('confirm', { defaultCorsPreflightOptions })
+    AuthStack.addIntegration(
+      'sign-up-confirm',
+      stack,
+      params,
+      userPool,
+      userPoolClientId,
+      resource,
+      'sign-up-confirm.handler',
+      ['cognito-idp:ConfirmSignUp']
+    )
+
+    resource = signUpResource.addResource('resend-code', { defaultCorsPreflightOptions })
+    AuthStack.addIntegration(
+      'sign-up-resend-code',
+      stack,
+      params,
+      userPool,
+      userPoolClientId,
+      resource,
+      'resend-confirmation-code.handler',
+      ['cognito-idp:ResendConfirmationCode']
+    )
+
+    // token
+    // token/refresh
+    // token/revoke
+    const tokenResource = authResource.addResource('token', { defaultCorsPreflightOptions })
+    // In passwordless mode we'll have an integration to get a valid token responding to a challenge
+    if (params.mode === 'Passwordless') {
+      AuthStack.addIntegration(
+        'token',
+        stack,
+        params,
+        userPool,
+        userPoolClientId,
+        tokenResource,
+        'challenge-answer.handler',
+        ['cognito-idp:InitiateAuth', 'cognito-idp:RespondToAuthChallenge']
+      )
+    }
+
+    resource = tokenResource.addResource('refresh', { defaultCorsPreflightOptions })
+    AuthStack.addIntegration(
+      'refresh-token',
+      stack,
+      params,
+      userPool,
+      userPoolClientId,
+      resource,
+      'refresh-token.handler',
+      ['cognito-idp:InitiateAuth']
+    )
+
+    resource = tokenResource.addResource('revoke', { defaultCorsPreflightOptions })
+    AuthStack.addIntegration('revoke-token', stack, params, userPool, userPoolClientId, resource, 'sign-out.handler', [
+      'cognito-idp:GlobalSignOut',
+    ])
+
+    // password/forgot
+    // password/change
+    if (params.mode === 'UserPassword') {
+      const passwordResource = authResource.addResource('password', { defaultCorsPreflightOptions })
+      resource = passwordResource.addResource('forgot', { defaultCorsPreflightOptions })
+      AuthStack.addIntegration(
+        'forgot-password',
+        stack,
+        params,
+        userPool,
+        userPoolClientId,
+        resource,
+        'forgot-password.handler',
+        ['cognito-idp:ForgotPassword']
+      )
+
+      resource = passwordResource.addResource('change', { defaultCorsPreflightOptions })
+      AuthStack.addIntegration(
+        'change-password',
+        stack,
+        params,
+        userPool,
+        userPoolClientId,
+        resource,
+        'confirm-forgot-password.handler',
+        ['cognito-idp:ConfirmForgotPassword']
+      )
+    }
+    return rootAuthAPI
+  }
+
+  private static addIntegration(
+    name: string,
+    stack: Stack,
+    params: AWSAuthRocketParams,
+    userPool: UserPool,
+    userPoolClientId: string,
+    resource: Resource,
+    handler: string,
+    actions: string[]
+  ): void {
     const allowedOriginHeaderForCors = {
       'method.response.header.Access-Control-Allow-Origin': true,
     }
@@ -150,107 +271,22 @@ export class AuthStack {
         },
       ],
     }
-    const defaultCorsPreflightOptions: CorsOptions = {
-      allowHeaders: ['*'],
-      allowOrigins: Cors.ALL_ORIGINS,
-      allowMethods: ['POST', 'OPTIONS'],
-    }
 
-    const endpointIntegrations = [
-      {
-        endpoint: 'sign-in',
-        handler: 'sign-in.handler',
-        actions: ['cognito-idp:InitiateAuth'],
-        included: true,
-      },
-      {
-        endpoint: 'sign-up',
-        handler: 'sign-up.handler',
-        actions: ['cognito-idp:SignUp'],
-        included: true,
-      },
-      {
-        endpoint: 'confirm',
-        handler: 'sign-up-confirm.handler',
-        actions: ['cognito-idp:ConfirmSignUp'],
-        included: true,
-        parent: 'sign-up',
-      },
-      {
-        endpoint: 'resend-code',
-        handler: 'resend-confirmation-code.handler',
-        actions: ['cognito-idp:ResendConfirmationCode'],
-        included: true,
-        parent: 'sign-up',
-      },
-      {
-        endpoint: 'token',
-        handler: 'challenge-answer.handler',
-        actions: ['cognito-idp:InitiateAuth', 'cognito-idp:RespondToAuthChallenge'],
-        included: params.mode === 'Passwordless',
-      },
-      {
-        endpoint: 'refresh',
-        handler: 'refresh-token.handler',
-        actions: ['cognito-idp:InitiateAuth'],
-        included: true,
-        parent: 'token',
-      },
-      {
-        endpoint: 'token/revoke',
-        handler: 'sign-out.handler',
-        actions: ['cognito-idp:GlobalSignOut'],
-        included: true,
-        parent: 'token',
-      },
-      {
-        endpoint: 'forgot',
-        handler: 'forgot-password.handler',
-        actions: ['cognito-idp:ForgotPassword'],
-        included: params.mode === 'UserPassword',
-        parent: 'password',
-      },
-      {
-        endpoint: 'confirm',
-        handler: 'confirm-forgot-password.handler',
-        actions: ['cognito-idp:ConfirmForgotPassword'],
-        included: params.mode === 'UserPassword',
-        parent: 'password',
-      },
-    ]
+    const authLambda = createLamba(stack, `${AuthStack.rocketArtifactsPrefix(params)}-${name}`, handler, {
+      userPoolId: userPool.userPoolId,
+      userPoolClientId: userPoolClientId,
+      mode: params.mode,
+    })
 
-    endpointIntegrations
-      .filter((item) => item.included)
-      .map((item) => {
-        const authLambda = createLamba(
-          stack,
-          `${AuthStack.rocketArtifactsPrefix(params)}-${item.endpoint}`,
-          item.handler,
-          {
-            userPoolId: userPool.userPoolId,
-            userPoolClientId: userPoolClientId,
-            mode: params.mode,
-          }
-        )
+    resource.addMethod('POST', new LambdaIntegration(authLambda), methodOptions)
 
-        let resource = authResource
-        if (item.parent) {
-          resource = authResource.resourceForPath(item.parent)
-        }
-        resource
-          .addResource(item.endpoint, { defaultCorsPreflightOptions })
-          .addMethod('POST', new LambdaIntegration(authLambda), methodOptions)
-
-        authLambda.addToRolePolicy(
-          new PolicyStatement({
-            effect: Effect.ALLOW,
-            actions: item.actions,
-            resources: [userPool.userPoolArn],
-          })
-        )
+    authLambda.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: actions,
+        resources: [userPool.userPoolArn],
       })
-
-    return rootAuthAPI
+    )
   }
 
   private static printOutput(stack: Stack, userPoolId: string, authApi: RestApi): void {
