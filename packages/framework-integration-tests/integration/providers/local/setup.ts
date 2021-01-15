7a9e7fce439c740265244c49cb4b5172a63a0ffe
alvaroloes <alvaro@booster.cloud>
2021-01-15 15:09:52 +0000

    Fix: now when deploying using the integration test application, the local dependencies are used instead of the published ones

--- packages/framework-integration-tests/integration/providers/local/setup.ts
+++ packages/framework-integration-tests/integration/providers/local/setup.ts
@@ -2,7 +2,7 @@ import { start } from './utils'
 import { sleep } from '../../helper/sleep'
 import { ChildProcess } from 'child_process'
 import { createSandboxProject, removeFolders } from '../../helper/fileHelper'
-import { symLinkBoosterDependencies } from '../../helper/depsHelper'
+import { overrideWithBoosterLocalDependencies } from '../../helper/depsHelper'
 import { sandboxName } from './constants'
 import { runCommand } from '../../helper/runCommand'
 
@@ -17,7 +17,7 @@ before(async () => {
   await runCommand(sandboxPath, 'npm install')
 
   console.log('symlinking booster dependencies...')
-  await symLinkBoosterDependencies(sandboxPath)
+  await overrideWithBoosterLocalDependencies(sandboxPath)
 
   console.log(`starting local server in ${sandboxPath}...`)
   serverProcess = start('local', sandboxPath)
