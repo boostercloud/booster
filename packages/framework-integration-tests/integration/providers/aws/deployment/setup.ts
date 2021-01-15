ae599b9731d27d22626ec8cf79459f0b3cc408d0
Javier Toledo <javier@booster.cloud>
2021-01-15 21:18:05 +0000

    Rebuild projects before packing

--- packages/framework-integration-tests/integration/providers/aws/deployment/setup.ts
+++ packages/framework-integration-tests/integration/providers/aws/deployment/setup.ts
@@ -2,12 +2,15 @@ import { deploy } from '../deploy'
 import { sleep } from '../../../helper/sleep'
 import { setEnv, checkConfigAnd } from '../utils'
 import { createSandboxProject } from '../../../helper/fileHelper'
-import { overrideWithBoosterLocalDependencies } from '../../../helper/depsHelper'
+import { forceLernaRebuild, overrideWithBoosterLocalDependencies } from '../../../helper/depsHelper'
 
 before(async () => {
   await setEnv()
   const sandboxedProject = createSandboxProject('deploy')
+
+  await forceLernaRebuild()
   await overrideWithBoosterLocalDependencies(sandboxedProject)
+
   await checkConfigAnd(deploy.bind(null, sandboxedProject))
   console.log('Waiting 30 seconds after deployment to let the stack finish its initialization...')
   await sleep(30000)
--- packages/framework-integration-tests/integration/providers/aws/nuke/setup.ts
+++ packages/framework-integration-tests/integration/providers/aws/nuke/setup.ts
@@ -1,3 +1,4 @@
+import { forceLernaRebuild, overrideWithBoosterLocalDependencies } from '../../../helper/depsHelper'
 import { createSandboxProject, removeSandboxProject } from '../../../helper/fileHelper'
 import { nuke } from '../deploy'
 import { setEnv, checkConfigAnd } from '../utils'
@@ -5,6 +6,10 @@ import { setEnv, checkConfigAnd } from '../utils'
 before(async () => {
   await setEnv()
   const sandboxedProject = createSandboxProject('deploy')
+
+  await forceLernaRebuild()
+  await overrideWithBoosterLocalDependencies(sandboxedProject)
+
   await checkConfigAnd(nuke.bind(null, sandboxedProject))
   removeSandboxProject('deploy')
 })
