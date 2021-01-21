36b4763538e2d3b57982bfb5c75df7a9029ea9d1
adriangmweb <adriangonzalezmesa@gmail.com>
2021-01-21 14:17:26 +0000

    fix linting in searcher function

--- packages/framework-types/src/searcher.ts
+++ packages/framework-types/src/searcher.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/indent */
 import { UUID } from './concepts'
 import { Class } from './typelevel'
 
