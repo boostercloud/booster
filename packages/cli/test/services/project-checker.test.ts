764858da2bfc8236fab82a5922694e1eca0c2b85
David Verdú <davidverdu@theagilemonkeys.com>
2021-01-27 14:55:32 +0000

    cli test: fix project-checker test

--- packages/cli/test/services/project-checker.test.ts
+++ packages/cli/test/services/project-checker.test.ts
@@ -1,5 +1,5 @@
 import * as path from 'path'
-import { checkItIsABoosterProject } from '../../src/services/project-checker'
+import { checkItIsABoosterProject, checkCurrentDirIsABoosterProject } from '../../src/services/project-checker'
 import { restore, replace, fake } from 'sinon'
 import { expect } from '../expect'
 
@@ -9,24 +9,49 @@ describe('project checker', (): void => {
         restore()
     })
 
-    it('is a Booster project', async () => {
-        replace(process,'cwd', fake.returns(path.join(process.cwd(),'resources', 'mock_project')))
-        let exceptionThrown = false
-        await checkItIsABoosterProject().catch(() => exceptionThrown = true)
-        expect(exceptionThrown).to.be.equal(false)
+    describe('checkCurrentDirIsABoosterProject', () => {
+        it('is a Booster project', async () => {
+            replace(process,'cwd', fake.returns(path.join(process.cwd(),'resources', 'mock_project')))
+            let exceptionThrown = false
+            await checkCurrentDirIsABoosterProject().catch(() => exceptionThrown = true)
+            expect(exceptionThrown).to.be.equal(false)
+        })
+    
+        it('is a Booster project with bad index.ts', async () => {
+            replace(process,'cwd', fake.returns(path.join(process.cwd(),'resources', 'mock_project_bad_index')))
+            let exceptionThrown = false
+            await checkCurrentDirIsABoosterProject().catch(() => exceptionThrown = true)
+            expect(exceptionThrown).to.be.equal(true)
+        })
+    
+        it('is not a Booster project', async () => {
+            replace(process,'cwd', fake.returns(path.join(process.cwd(),'resources')))
+            let exceptionThrown = false
+            await checkCurrentDirIsABoosterProject().catch(() => exceptionThrown = true)
+            expect(exceptionThrown).to.be.equal(true)
+        })
     })
 
-    it('is a Booster project with bad index.ts', async () => {
-        replace(process,'cwd', fake.returns(path.join(process.cwd(),'resources', 'mock_project_bad_index')))
-        let exceptionThrown = false
-        await checkItIsABoosterProject().catch(() => exceptionThrown = true)
-        expect(exceptionThrown).to.be.equal(true)
-    })
-
-    it('is not a Booster project', async () => {
-        replace(process,'cwd', fake.returns(path.join(process.cwd(),'resources')))
-        let exceptionThrown = false
-        await checkItIsABoosterProject().catch(() => exceptionThrown = true)
-        expect(exceptionThrown).to.be.equal(true)
+    describe('checkItIsABoosterProject', (): void => {
+        it('is a Booster project', async () => {
+            const projectPath = path.join(process.cwd(),'resources', 'mock_project')
+            let exceptionThrown = false
+            await checkItIsABoosterProject(projectPath).catch(() => exceptionThrown = true)
+            expect(exceptionThrown).to.be.equal(false)
+        })
+    
+        it('is a Booster project with bad index.ts', async () => {
+            const projectPath = path.join(process.cwd(),'resources', 'mock_project_bad_index')
+            let exceptionThrown = false
+            await checkItIsABoosterProject(projectPath).catch(() => exceptionThrown = true)
+            expect(exceptionThrown).to.be.equal(true)
+        })
+    
+        it('is not a Booster project', async () => {
+            const projectPath = path.join(process.cwd(),'resources')
+            let exceptionThrown = false
+            await checkItIsABoosterProject(projectPath).catch(() => exceptionThrown = true)
+            expect(exceptionThrown).to.be.equal(true)
+        })
     })
 })
