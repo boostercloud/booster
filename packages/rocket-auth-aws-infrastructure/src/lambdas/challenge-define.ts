acd7e313de162277120d955e9cd10e304eeab650
charlietfe <carlos@theagilemonkeys.com>
2021-01-28 12:58:29 +0000

    refactored auth challenge

--- packages/rocket-auth-aws-infrastructure/src/lambdas/challenge-define.ts
+++ packages/rocket-auth-aws-infrastructure/src/lambdas/challenge-define.ts
@@ -1,32 +1,66 @@
+/* eslint-disable @typescript-eslint/indent */
 import { CognitoUserPoolEvent } from 'aws-lambda'
 
-export const handler = async (event: CognitoUserPoolEvent): Promise<CognitoUserPoolEvent> => {
-  if (event.request.session && event.request.session.find((attempt) => attempt.challengeName !== 'CUSTOM_CHALLENGE')) {
-    // We only accept custom challenges; fail auth
-    event.response.issueTokens = false
-    event.response.failAuthentication = true
-  } else if (
-    event.request.session &&
-    event.request.session.length >= 3 &&
-    event.request.session.slice(-1)[0].challengeResult === false
-  ) {
-    // The user provided a wrong answer 3 times; fail auth
-    event.response.issueTokens = false
-    event.response.failAuthentication = true
-  } else if (
-    event.request.session &&
-    event.request.session.length &&
-    event.request.session.slice(-1)[0].challengeName === 'CUSTOM_CHALLENGE' &&
-    event.request.session.slice(-1)[0].challengeResult === true
-  ) {
-    // The user provided the right answer; succeed auth
-    event.response.issueTokens = true
-    event.response.failAuthentication = false
-  } else {
-    // The user did not provide a correct answer yet; present challenge
-    event.response.issueTokens = false
-    event.response.failAuthentication = false
-    event.response.challengeName = 'CUSTOM_CHALLENGE'
-  }
+type Session = Array<{
+  challengeName:
+    | 'CUSTOM_CHALLENGE'
+    | 'PASSWORD_VERIFIER'
+    | 'SMS_MFA'
+    | 'DEVICE_SRP_AUTH'
+    | 'DEVICE_PASSWORD_VERIFIER'
+    | 'ADMIN_NO_SRP_AUTH'
+  challengeResult: boolean
+  challengeMetadata?: string
+}>
+
+// User answer helpers
+const onlyAcceptingCustomChallenges = (session: Session): boolean => {
+  return session.find((attempt) => attempt.challengeName !== 'CUSTOM_CHALLENGE') !== undefined
+}
+
+const userProvidedRightAnswer = (session: Session): boolean => {
+  return (
+    session.length > 0 &&
+    session.slice(-1)[0].challengeName === 'CUSTOM_CHALLENGE' &&
+    session.slice(-1)[0].challengeResult === true
+  )
+}
+
+const userProvidedWrongAnswerThreeTimes = (session: Session): boolean => {
+  return session.length >= 3 && session.slice(-1)[0].challengeResult === false
+}
+
+// Response helpers
+const presentChallenge = (event: CognitoUserPoolEvent): CognitoUserPoolEvent => {
+  event.response.issueTokens = false
+  event.response.failAuthentication = false
+  event.response.challengeName = 'CUSTOM_CHALLENGE'
   return event
+}
+
+const succeedAuth = (event: CognitoUserPoolEvent): CognitoUserPoolEvent => {
+  event.response.issueTokens = true
+  event.response.failAuthentication = false
+  return event
+}
+
+const failAuth = (event: CognitoUserPoolEvent): CognitoUserPoolEvent => {
+  event.response.issueTokens = false
+  event.response.failAuthentication = true
+  return event
+}
+
+export const handler = async (event: CognitoUserPoolEvent): Promise<CognitoUserPoolEvent> => {
+  if (!event.request.session) {
+    return event
+  }
+  const currentSession = event.request.session
+
+  if (onlyAcceptingCustomChallenges(currentSession) || userProvidedWrongAnswerThreeTimes(currentSession)) {
+    return failAuth(event)
+  } else if (userProvidedRightAnswer(currentSession)) {
+    return succeedAuth(event)
+  } else {
+    return presentChallenge(event)
+  }
 }
--- packages/rocket-auth-aws-infrastructure/src/lambdas/challenge-verify.ts
+++ packages/rocket-auth-aws-infrastructure/src/lambdas/challenge-verify.ts
@@ -2,10 +2,6 @@ import { CognitoUserPoolEvent } from 'aws-lambda'
 
 export const handler = async (event: CognitoUserPoolEvent): Promise<CognitoUserPoolEvent> => {
   const expectedAnswer = event.request.privateChallengeParameters?.secretLoginCode ?? ''
-  if (event.request.challengeAnswer === expectedAnswer) {
-    event.response.answerCorrect = true
-  } else {
-    event.response.answerCorrect = false
-  }
+  event.response.answerCorrect = event.request.challengeAnswer === expectedAnswer
   return event
 }
