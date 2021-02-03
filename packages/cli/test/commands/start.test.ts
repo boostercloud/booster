38c794b8cff5a5be905b7905c57a755ce7a714a6
David Verdú <davidverdu@theagilemonkeys.com>
2021-02-03 15:45:55 +0000

    fix start unit tests

--- packages/cli/test/commands/start.test.ts
+++ packages/cli/test/commands/start.test.ts
@@ -1,15 +1,14 @@
 import { expect } from '../expect'
 import { restore, fake, replace } from 'sinon'
 import rewire = require('rewire')
-import { ProviderLibrary } from '@boostercloud/framework-types'
+import { ProviderLibrary, BoosterConfig } from '@boostercloud/framework-types'
 import * as Start from '../../src/commands/start'
 import * as providerService from '../../src/services/provider-service'
 import { oraLogger } from '../../src/services/logger'
 import { IConfig } from '@oclif/config'
-import * as childProcessPromise from 'child-process-promise'
-import * as path from 'path'
 import { test } from '@oclif/test'
 import * as environment from '../../src/services/environment'
+import * as configService from '../../src/services/config-service'
 
 const start = rewire('../../src/commands/start')
 const runTasks = start.__get__('runTasks')
@@ -53,9 +52,10 @@ describe('start', () => {
     })
   })
 
-  describe('deploy class', () => {
+  describe('start class', () => {
     beforeEach(() => {
-      replace(childProcessPromise, 'exec', fake.resolves({}))
+      const config = new BoosterConfig('fake_environment')
+      replace(configService,'compileProjectAndLoadConfig', fake.resolves(config))
       replace(providerService,'startProvider', fake.resolves({}))
       replace(oraLogger,'fail', fake.resolves({}))
       replace(oraLogger, 'info', fake.resolves({}))
@@ -66,9 +66,9 @@ describe('start', () => {
     it('without flags', async () => {
       await new Start.default([], {} as IConfig).run()
 
-      expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm run clean && npm run compile')
+      expect(configService.compileProjectAndLoadConfig).to.have.not.been.called
       expect(providerService.startProvider).to.have.not.been.called
-      expect(oraLogger.fail).to.have.been.calledWithMatch('Error: No environment set. Use the flag `-e` or set the environment variable BOOSTER_ENV to set it before running this command. Example usage: `boost deploy -e <environment>`.')
+      expect(oraLogger.fail).to.have.been.calledWithMatch(/No environment set/)
     })
 
     it('with -e flag incomplete', async () => {
@@ -82,7 +82,7 @@ describe('start', () => {
       }
       expect(exceptionThrown).to.be.equal(true)
       expect(exceptionMessage).to.be.equal('Flag --environment expects a value')
-      expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm run clean && npm run compile')
+      expect(configService.compileProjectAndLoadConfig).to.have.not.been.called
       expect(providerService.startProvider).to.have.not.been.called
     })
 
@@ -97,36 +97,16 @@ describe('start', () => {
       }
       expect(exceptionThrown).to.be.equal(true)
       expect(exceptionMessage).to.be.equal('Flag --environment expects a value')
-      expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm run clean && npm run compile')
-      expect(providerService.startProvider).to.have.not.been.called
-    })
-
-    it('outside a booster project', async () => {
-      let exceptionThrown = false
-      let exceptionMessage = ''
-      try {
-        await new Start.default(['--environment','fake_environment'], {} as IConfig).run()
-      } catch (e) {
-        exceptionThrown = true
-        exceptionMessage = e.message
-      }
-      expect(exceptionThrown).to.be.equal(true)
-      expect(exceptionMessage).to.contain('Error: There was an error when recognizing the application. Make sure you are in the root path of a Booster project')
-      expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm run clean && npm run compile')
+      expect(configService.compileProjectAndLoadConfig).to.have.not.been.called
       expect(providerService.startProvider).to.have.not.been.called
     })
 
     describe('inside a booster project', () => {
-
-      beforeEach(() => {
-        replace(process,'cwd', fake.returns(path.join(process.cwd(),'test','fixtures', 'mock_project')))
-      })
-
+    
       it('entering correct environment', async () => {
         await new Start.default(['-e','fake_environment'], {} as IConfig).run()
   
-        expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm install --production --no-bin-links')
-        expect(childProcessPromise.exec).to.have.been.calledWithMatch('npm run clean && npm run compile')
+        expect(configService.compileProjectAndLoadConfig).to.have.been.called
         expect(providerService.startProvider).to.have.been.called
         expect(oraLogger.start).to.have.been.calledWithMatch('Starting debug server on port')
       })
@@ -134,8 +114,7 @@ describe('start', () => {
       it('entering correct environment and --port flag', async () => {
         await new Start.default(['-e','fake_environment','--port','5000'], {} as IConfig).run()
   
-        expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm install --production --no-bin-links')
-        expect(childProcessPromise.exec).to.have.been.calledWithMatch('npm run clean && npm run compile')
+        expect(configService.compileProjectAndLoadConfig).to.have.been.called
         expect(providerService.startProvider).to.have.been.called
         expect(oraLogger.start).to.have.been.calledWithMatch('Starting debug server on port 5000')
       })
@@ -143,8 +122,7 @@ describe('start', () => {
       it('entering correct environment and -p flag', async () => {
         await new Start.default(['-e','fake_environment','-p','5000'], {} as IConfig).run()
   
-        expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm install --production --no-bin-links')
-        expect(childProcessPromise.exec).to.have.been.calledWithMatch('npm run clean && npm run compile')
+        expect(configService.compileProjectAndLoadConfig).to.have.been.called
         expect(providerService.startProvider).to.have.been.called
         expect(oraLogger.start).to.have.been.calledWithMatch('Starting debug server on port 5000')
       })
@@ -160,8 +138,7 @@ describe('start', () => {
         }
         expect(exceptionThrown).to.be.equal(true)
         expect(exceptionMessage).to.contain('Unexpected argument: --nonexistingoption')
-        expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm install --production --no-bin-links')
-        expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm run clean && npm run compile')
+        expect(configService.compileProjectAndLoadConfig).to.have.not.been.called
         expect(providerService.startProvider).to.have.not.been.called
         expect(oraLogger.start).to.have.not.been.calledWithMatch('Starting debug server on port')
       })
@@ -177,7 +154,7 @@ describe('start', () => {
         }
         expect(exceptionThrown).to.be.equal(true)
         expect(exceptionMessage).to.contain('Flag --port expects a value')
-        expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm run clean && npm run compile')
+        expect(configService.compileProjectAndLoadConfig).to.have.not.been.called
         expect(providerService.startProvider).to.have.not.been.called
         expect(oraLogger.start).to.have.not.been.calledWithMatch('Starting debug server on port')
       })
@@ -193,57 +170,17 @@ describe('start', () => {
         }
         expect(exceptionThrown).to.be.equal(true)
         expect(exceptionMessage).to.contain('Flag --port expects a value')
-        expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm run clean && npm run compile')
+        expect(configService.compileProjectAndLoadConfig).to.have.not.been.called
         expect(providerService.startProvider).to.have.not.been.called
         expect(oraLogger.start).to.have.not.been.calledWithMatch('Starting debug server on port')
-      })
-
-      it('entering nonexisting environment', async () => {
-        let exceptionThrown = false
-        let exceptionMessage = ''
-        try {
-          await new Start.default(['-e','nonexisting_environment'], {} as IConfig).run()
-        } catch(e) {
-          exceptionThrown = true
-          exceptionMessage = e.message
-        }
-        expect(exceptionThrown).to.be.equal(true)
-        expect(exceptionMessage).to.contain('The environment \'nonexisting_environment\' does not match any of the environments you used to configure your Booster project')
-        expect(childProcessPromise.exec).to.have.been.calledWithMatch('npm run clean && npm run compile')
-        expect(providerService.startProvider).to.have.not.been.called
-        expect(oraLogger.start).to.have.not.been.calledWithMatch('Starting debug server on port')
-      })
-
-      it('entering nonexisting environment and -p flag', async () => {
-        let exceptionThrown = false
-        let exceptionMessage = ''
-        try {
-          await new Start.default(['-e','nonexisting_environment','-p','5000'], {} as IConfig).run()
-        } catch(e) {
-          exceptionThrown = true
-          exceptionMessage = e.message
-        }
-        expect(exceptionThrown).to.be.equal(true)
-        expect(exceptionMessage).to.contain('The environment \'nonexisting_environment\' does not match any of the environments you used to configure your Booster project')
-        expect(childProcessPromise.exec).to.have.been.calledWithMatch('npm run clean && npm run compile')
-        expect(providerService.startProvider).to.have.not.been.called
-        expect(oraLogger.start).to.have.not.been.calledWithMatch('Starting debug server on port')
-      })
-
-      it('without defining environment', async () => {
-        await new Start.default([], {} as IConfig).run()
-  
-        expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm run clean && npm run compile')
-        expect(providerService.startProvider).to.have.not.been.called
-        expect(oraLogger.fail).to.have.been.calledWithMatch('Error: No environment set. Use the flag `-e` or set the environment variable BOOSTER_ENV to set it before running this command. Example usage: `boost deploy -e <environment>`.')
       })
 
       it('without defining environment and -p', async () => {
         await new Start.default(['-p','5000'], {} as IConfig).run()
   
-        expect(childProcessPromise.exec).to.have.not.been.calledWithMatch('npm run clean && npm run compile')
+        expect(configService.compileProjectAndLoadConfig).to.have.not.been.called
         expect(providerService.startProvider).to.have.not.been.called
-        expect(oraLogger.fail).to.have.been.calledWithMatch('Error: No environment set. Use the flag `-e` or set the environment variable BOOSTER_ENV to set it before running this command. Example usage: `boost deploy -e <environment>`.')
+        expect(oraLogger.fail).to.have.been.calledWithMatch(/No environment set/)
       })
     })
   })
