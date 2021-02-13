1739082bd77b6330439d26791073402053edc7b2
Ricards Tagils <tagils@mail.ru>
2021-02-13 21:42:01 +0200

    test(cli): add checkResourceExists method unit tests

--- packages/cli/test/services/project-checker.test.ts
+++ packages/cli/test/services/project-checker.test.ts
@@ -1,8 +1,9 @@
 import * as path from 'path'
 import {
-  checkItIsABoosterProject,
-  checkCurrentDirIsABoosterProject,
-  checkProjectAlreadyExists,
+    checkItIsABoosterProject,
+    checkCurrentDirIsABoosterProject,
+    checkProjectAlreadyExists,
+    checkResourceExists,
 } from '../../src/services/project-checker'
 import { restore, replace, fake, spy, stub } from 'sinon'
 import { expect } from '../expect'
@@ -27,14 +28,14 @@ describe('project checker', (): void => {
             await checkCurrentDirIsABoosterProject().catch(() => exceptionThrown = true)
             expect(exceptionThrown).to.be.equal(false)
         })
-    
+
         it('is a Booster project with bad index.ts', async () => {
             replace(process,'cwd', fake.returns(path.join(process.cwd(),'test', 'fixtures', 'mock_project_bad_index')))
             let exceptionThrown = false
             await checkCurrentDirIsABoosterProject().catch(() => exceptionThrown = true)
             expect(exceptionThrown).to.be.equal(true)
         })
-    
+
         it('is not a Booster project', async () => {
             replace(process,'cwd', fake.returns(path.join(process.cwd(),'test', 'fixtures')))
             let exceptionThrown = false
@@ -50,14 +51,14 @@ describe('project checker', (): void => {
             await checkItIsABoosterProject(projectPath).catch(() => exceptionThrown = true)
             expect(exceptionThrown).to.be.equal(false)
         })
-    
+
         it('is a Booster project with bad index.ts', async () => {
             const projectPath = path.join(process.cwd(),'test', 'fixtures', 'mock_project_bad_index')
             let exceptionThrown = false
             await checkItIsABoosterProject(projectPath).catch(() => exceptionThrown = true)
             expect(exceptionThrown).to.be.equal(true)
         })
-    
+
         it('is not a Booster project', async () => {
             const projectPath = path.join(process.cwd(),'test', 'fixtures')
             let exceptionThrown = false
@@ -67,7 +68,6 @@ describe('project checker', (): void => {
     })
 
     describe('checkProjectAlreadyExists', (): void => {
-
         it('should do nothing if project with given name does not exist', async () => {
             const existsSyncStub = stub(fs, 'existsSync')
             existsSyncStub.returns(false)
@@ -118,6 +118,56 @@ describe('project checker', (): void => {
             await checkProjectAlreadyExists(projectName)
 
             expect(fs.removeSync).to.have.been.calledWithMatch(projectPath)
+        })
+    })
+
+    describe('checkResourceExists', (): void => {
+        it('should print info message and do nothing if resource doesn\'t exist', async () => {
+            const resourcePath = path.join('test', 'fixtures', 'mock_project', 'src', 'entities')
+            const existsSyncStub = stub(fs, 'existsSync')
+            existsSyncStub.returns(false)
+            spy(Prompter, 'confirmPrompt')
+
+            await checkResourceExists('TestResource', resourcePath, '.ts')
+
+            expect(oraLogger.info).to.have.been.calledWithMatch('Checking if resource already exists...')
+            expect(fs.existsSync).to.have.been.calledWithMatch(resourcePath)
+            expect(Prompter.confirmPrompt).not.to.have.been.called
+        })
+
+        it('should throw error when resource exists and user refuses to overwrite it', async () => {
+            const resourcePath = path.join('test', 'fixtures', 'mock_project', 'src', 'entities')
+            const fakePrompter = fake.resolves(false)
+            const existsSyncStub = stub(fs, 'existsSync')
+            let exceptionThrown = false
+            let exceptionMessage = ''
+
+            existsSyncStub.returns(true)
+            replace(Prompter, 'confirmPrompt', fakePrompter)
+
+            await checkResourceExists('TestResource', resourcePath, '.ts').catch((e) => {
+                exceptionThrown = true
+                exceptionMessage = e.message
+            })
+
+            expect(fs.existsSync).to.have.been.calledWithMatch(resourcePath)
+            expect(Prompter.confirmPrompt).to.have.been.called
+            expect(exceptionThrown).to.be.equal(true)
+            expect(exceptionMessage).to.be.equal( 'The \'entities\' resource "test-resource.ts" already exists. Please use another resource name')
+        })
+
+        it('should remove resource when it exists and user agreed to overwrite it', async () => {
+            const resourcePath = path.join('test', 'fixtures', 'mock_project', 'src', 'entities')
+            const fakePrompter = fake.resolves(true)
+            const existsSyncStub = stub(fs, 'existsSync')
+
+            existsSyncStub.returns(true)
+            replace(fs, 'removeSync', fake.resolves({}))
+            replace(Prompter, 'confirmPrompt', fakePrompter)
+
+            await checkResourceExists('TestResource', resourcePath, '.ts')
+
+            expect(fs.removeSync).to.have.been.calledWithMatch(resourcePath)
         })
     })
 })
