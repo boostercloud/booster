0155a52534c7897e4b26fbea22069a5bc6271d7c
alvaroloes <alvaro@booster.cloud>
2021-02-18 17:48:52 +0000

    Fix unit tests

--- packages/framework-core/test/services/graphql/graphql-query-generator.test.ts
+++ packages/framework-core/test/services/graphql/graphql-query-generator.test.ts
@@ -140,7 +140,11 @@ describe('GraphQLQueryGenerator', () => {
               properties: [
                 {
                   name: mockPropertyName,
-                  type: mockPropertyType,
+                  typeInfo: {
+                    name: mockPropertyType.name,
+                    type: mockPropertyType,
+                    parameters: [],
+                  },
                 },
               ],
             }
