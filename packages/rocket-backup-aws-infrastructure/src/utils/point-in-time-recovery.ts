a4861f39feb3f792d96a02330b3adf4ef1994f93
Carlos Dominguez Padron <carlos@theagilemonkeys.com>
2021-03-01 17:01:33 +0000

    removed backup rocket (#641)

--- packages/rocket-backup-aws-infrastructure/src/utils/point-in-time-recovery.ts
+++
@@ -1,9 +0,0 @@
-import { Table } from '@aws-cdk/aws-dynamodb'
-
-export const applyPointInTimeRecoveryBackup = (tables: Array<Table>): void => {
-  tables.map((table: Table) => {
-    table.node['host'].table.pointInTimeRecoverySpecification = {
-      pointInTimeRecoveryEnabled: true,
-    }
-  })
-}
