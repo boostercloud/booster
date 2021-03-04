57bc89e6ed1ddce6a9a9656df6ffc419fa975918
charlietfe <carlos@theagilemonkeys.com>
2021-03-04 10:45:04 +0000

    removed file processing rockets

--- packages/rocket-batch-file-process-aws-infrastructure/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { InfrastructureRocket } from '@boostercloud/framework-provider-aws-infrastructure'
-import { BatchFileProcessingStack, AWSBatchProcessingFilesParams } from './batch-file-processing'
-
-const AWSBatchProcessingFiles = (params: AWSBatchProcessingFilesParams): InfrastructureRocket => ({
-  mountStack: BatchFileProcessingStack.mountStack.bind(null, params),
-  unmountStack: BatchFileProcessingStack.unmountStack.bind(null, params),
-})
-
-export default AWSBatchProcessingFiles
--- packages/rocket-fileuploader-aws-infrastructure/test/expect.ts
+++
@@ -1,6 +0,0 @@
-import * as chai from 'chai'
-
-chai.use(require('sinon-chai'))
-chai.use(require('chai-as-promised'))
-
-export const expect = chai.expect
