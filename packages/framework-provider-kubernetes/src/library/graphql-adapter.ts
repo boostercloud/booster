e1c0da0e8d824acfef409bc0a3884f0b74a64afc
Nikita Tchayka <nick@booster.cloud>
2021-03-17 12:02:33 +0000

    Split into many files

---
+++ packages/framework-provider-kubernetes/src/library/graphql-adapter.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Logger,
+  BoosterConfig,
+  GraphQLRequestEnvelope,
+  GraphQLRequestEnvelopeError,
+  GraphQLClientMessage,
+  GraphQLOperation,
+} from '@boostercloud/framework-types'
+
+export const rawToEnvelope = async (
+  request: { body?: GraphQLOperation | GraphQLClientMessage },
+  logger: Logger,
+  _config: BoosterConfig
+): Promise<GraphQLRequestEnvelope | GraphQLRequestEnvelopeError> => {
+  logger.debug('Received GraphQL request: ', request)
+  return {
+    requestID: '1',
+    eventType: 'MESSAGE',
+    currentUser: JSON.parse('{}'),
+    value: request.body,
+  }
+}
+
+export const handleResult = async (
+  result?: unknown,
+  headers?: Record<string, string> | undefined
+): Promise<unknown> => ({
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    ...headers,
+  },
+  statusCode: 200,
+  body: result ? JSON.stringify(result) : '',
+})
