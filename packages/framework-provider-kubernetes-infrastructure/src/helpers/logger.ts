4f2c4780c9ef490a1ef36f9211d9933d7cedf76c
Nikita Tchayka <nick@booster.cloud>
2021-03-18 17:53:42 +0000

    Fix some stuff related to logging

--- packages/framework-provider-kubernetes-infrastructure/src/helpers/logger.ts
+++ packages/framework-provider-kubernetes-infrastructure/src/helpers/logger.ts
@@ -1,9 +1,9 @@
 import { Logger } from '@boostercloud/framework-types'
 
-export const scopeLogger = (scope: string, logger: Logger): Logger =>
-  Object.keys(logger).reduce((prev, key) => {
-    const k = key as keyof Logger
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    prev[k] = (...args: any[]): void => prev[k](`${scope}#`, ...args)
-    return prev
-  }, logger)
+export const scopeLogger = (scope: string, logger: Logger): Logger => {
+  const newLogger: Logger = { ...logger }
+  newLogger.debug = (...args: any[]) => logger.debug([`[${scope}]`, ...args].join(' '))
+  newLogger.info = (...args: any[]) => logger.info([`[${scope}]`, ...args].join(' '))
+  newLogger.error = (...args: any[]) => logger.error([`[${scope}]`, ...args].join(' '))
+  return newLogger
+}
