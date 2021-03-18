1ea98ef3e392b9f57c2abf84401bd2b6d06b38eb
David Verdú <davidverdu@theagilemonkeys.com>
2021-03-18 10:17:39 +0000

    generate a redis key for events and snapshots (#703)
    * geenrate a redis key for events and snapshots
    
    * use uuid in place of a randon number

--- packages/framework-provider-kubernetes/src/services/event-registry.ts
+++ packages/framework-provider-kubernetes/src/services/event-registry.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { EventEnvelope, Logger } from 'framework-types/dist'
+import { v4 as uuidv4 } from 'uuid';
 import fetch from 'node-fetch'
 
 export class EventRegistry {
@@ -8,7 +9,7 @@ export class EventRegistry {
   public async store(event: EventEnvelope, logger: Logger): Promise<void> {
     const stateUrl = `${this.url}/v1.0/state/statestore`
     logger.debug('About to post', event)
-    const data = [{ key: event.entityID, value: event }]
+    const data = [{ key: this.eventKey(event), value: event }]
     const response = await fetch(stateUrl, {
       method: 'POST',
       body: JSON.stringify(data),
@@ -31,5 +32,19 @@ export class EventRegistry {
   public async queryLatest(_query: object): Promise<EventEnvelope> {
     // TODO: Implement accordingly to Dapr
     throw new Error('EventRegistry#queryLatest: Not implemented yet')
+  }
+
+  private eventKey(event: EventEnvelope): string {
+    //ee_Post_95ddb544-4a60-439f-a0e4-c57e806f2f6e_event_PostCreated_2021-03-17T16:49:29.143Z_j9qn8kd8
+    const keyParts = [
+      'ee',                 // event envelope marker
+      event.entityTypeName, // 'Post' entity name
+      event.entityID,       // entityId
+      event.kind,           // 'event' | 'snapshot'
+      event.typeName,       // 'PostCreated' event name
+      event.createdAt,      // timespan
+      uuidv4()              // hash to make key unique
+    ]
+    return keyParts.join('_')
   }
 }
