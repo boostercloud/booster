02768ccda3ffccc4a349e2ad5a61d43ca13d99e5
adriangmweb <adriangonzalezmesa@gmail.com>
2021-03-18 09:18:29 +0000

    add support for complex filters

--- packages/framework-provider-azure/src/library/searcher-adapter.ts
+++ packages/framework-provider-azure/src/library/searcher-adapter.ts
@@ -8,8 +8,9 @@ export async function searchReadModel(
   readModelName: string,
   filters: FilterFor<unknown>
 ): Promise<Array<any>> {
+  const filterExpression = buildFilterExpression(filters)
   const querySpec: SqlQuerySpec = {
-    query: `SELECT * FROM c ${buildFilterExpression(filters)}`,
+    query: `SELECT * FROM c ${filterExpression !== '' ? `WHERE ${filterExpression}` : filterExpression}`,
     parameters: buildExpressionAttributeValues(filters),
   }
   logger.debug('Running search with the following params: \n', querySpec)
@@ -25,18 +26,26 @@ export async function searchReadModel(
   return resources ?? []
 }
 
-function buildFilterExpression(filters: FilterFor<any>): string {
-  const filterExpression = Object.entries(filters)
-    .map(([propName, filter]) => buildOperation(propName, filter))
+function buildFilterExpression(filters: FilterFor<any>, usedPlaceholders: Array<string> = []): string {
+  return Object.entries(filters)
+    .map(([propName, filter]) => {
+      switch (propName) {
+        case 'not':
+          return `NOT (${buildFilterExpression(filter as FilterFor<any>, usedPlaceholders)})`
+        case 'and':
+        case 'or':
+          return `(${(filter as Array<FilterFor<any>>)
+            .map((arrayFilter) => buildFilterExpression(arrayFilter, usedPlaceholders))
+            .join(` ${propName} `)})`
+        default:
+          return buildOperation(propName, filter, usedPlaceholders)
+      }
+    })
     .join(' AND ')
-  if (filterExpression !== '') {
-    return `WHERE ${filterExpression}`
-  }
-  return filterExpression
 }
 
-function buildOperation(propName: string, filter: Operation<any> = {}): string {
-  const holder = placeholderBuilderFor(propName)
+function buildOperation(propName: string, filter: Operation<any> = {}, usedPlaceholders: Array<string>): string {
+  const holder = placeholderBuilderFor(propName, usedPlaceholders)
   return Object.entries(filter)
     .map(([operation, value], index) => {
       switch (operation) {
@@ -58,31 +67,85 @@ function buildOperation(propName: string, filter: Operation<any> = {}): string {
             .join(',')})`
         case 'contains':
           return `CONTAINS(c["${propName}"], ${holder(index)})`
-        case 'begins-with':
+        case 'beginsWith':
           return `STARTSWITH(c["${propName}"], ${holder(index)})`
+        case 'includes':
+          return `CONTAINS(c["${propName}"], ${holder(index)})`
         default:
+          if (typeof value === 'object') {
+            return `c["${propName}"].${buildOperation(operation, value, usedPlaceholders)}`
+          }
           throw new InvalidParameterError(`Operator "${operation}" is not supported`)
       }
     })
     .join(' AND ')
 }
 
-function placeholderBuilderFor(propName: string): (valueIndex: number, valueSubIndex?: number) => string {
-  return (valueIndex: number, valueSubIndex?: number) =>
-    `@${propName}_${valueIndex}` + (typeof valueSubIndex === 'number' ? `_${valueSubIndex}` : '')
+function placeholderBuilderFor(
+  propName: string,
+  usedPlaceholders: Array<string>
+): (valueIndex: number, valueSubIndex?: number) => string {
+  return (valueIndex: number, valueSubIndex?: number) => {
+    const placeholder = `@${propName}_${valueIndex}` + (typeof valueSubIndex === 'number' ? `_${valueSubIndex}` : '')
+    if (usedPlaceholders.includes(placeholder)) return placeholderBuilderFor(propName, usedPlaceholders)(valueIndex + 1)
+    usedPlaceholders.push(placeholder)
+    return placeholder
+  }
 }
 
-function buildExpressionAttributeValues(filters: FilterFor<any>): Array<SqlParameter> {
-  const attributeValues: Array<SqlParameter> = []
+function buildExpressionAttributeValues(
+  filters: FilterFor<any>,
+  usedPlaceholders: Array<string> = []
+): Array<SqlParameter> {
+  let attributeValues: Array<SqlParameter> = []
   Object.entries(filters).forEach(([propName]) => {
-    const filter = filters[propName]
-    const holder = placeholderBuilderFor(propName)
-    Object.values(filter as any).forEach((value, index) => {
+    switch (propName) {
+      case 'not':
+        attributeValues = [
+          ...attributeValues,
+          ...buildExpressionAttributeValues(filters[propName] as FilterFor<any>, usedPlaceholders),
+        ]
+        break
+      case 'and':
+      case 'or':
+        for (const filter of filters[propName] as Array<FilterFor<any>>) {
+          attributeValues = [
+            ...attributeValues,
+            ...buildExpressionAttributeValues(filter as FilterFor<any>, usedPlaceholders),
+          ]
+        }
+        break
+      default:
+        attributeValues = [...attributeValues, ...buildAttributeValue(propName, filters[propName], usedPlaceholders)]
+        break
+    }
+  })
+  return attributeValues
+}
+
+function buildAttributeValue(
+  propName: string,
+  filter: Operation<any> = {},
+  usedPlaceholders: Array<string>
+): Array<SqlParameter> {
+  let attributeValues: Array<SqlParameter> = []
+  const holder = placeholderBuilderFor(propName, usedPlaceholders)
+  Object.entries(filter).forEach(([key, value], index) => {
+    if (Array.isArray(value)) {
+      value.forEach((element, subIndex) => {
+        attributeValues.push({
+          name: holder(index, subIndex),
+          value: element,
+        })
+      })
+    } else if (typeof value === 'object' && key !== 'includes') {
+      attributeValues = [...attributeValues, ...buildExpressionAttributeValues({ [key]: value }, usedPlaceholders)]
+    } else {
       attributeValues.push({
         name: holder(index),
         value: value as {},
       })
-    })
+    }
   })
   return attributeValues
 }
