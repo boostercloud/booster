9199bc98d7598bca5c02bed0e588d799dc00ae77
Roberto Diaz <roberto@theagilemonkeys.com>
2021-03-24 09:59:31 +0000

    added configuration for deploy in multiple clusters from the configuration file (#744)

--- packages/framework-provider-kubernetes-infrastructure/src/index.ts
+++ packages/framework-provider-kubernetes-infrastructure/src/index.ts
@@ -1,5 +1,5 @@
-import { deploy, nuke } from './infrastructure'
-
+import { deploy, nuke, BoosterK8sConfiguration } from './infrastructure'
+export { BoosterK8sConfiguration }
 export const Infrastructure = {
   deploy,
   nuke,
