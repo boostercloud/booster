dc646bfb922d87876155a8eae91e4e82b7c6c3a2
Roberto Diaz <roberto@theagilemonkeys.com>
2021-04-11 21:20:51 +0100

    modify the logger to set default level to info in kubernetes provider

--- packages/framework-provider-kubernetes-infrastructure/src/helpers/logger.ts
+++ packages/framework-provider-kubernetes-infrastructure/src/helpers/logger.ts
@@ -1,9 +1,21 @@
-import { Logger } from '@boostercloud/framework-types'
+import { Level, Logger } from '@boostercloud/framework-types'
 
-export const scopeLogger = (scope: string, logger: Logger): Logger => {
+export const scopeLogger = (scope: string, logger: Logger, level = Level.info): Logger => {
   const newLogger: Logger = { ...logger }
-  newLogger.debug = (...args: any[]) => logger.debug([`[${scope}]`, ...args].join(' '))
-  newLogger.info = (...args: any[]) => logger.info([`[${scope}]`, ...args].join(' '))
-  newLogger.error = (...args: any[]) => logger.error([`[${scope}]`, ...args].join(' '))
+  newLogger.debug = (...args: any[]) => {
+    if (level <= Level.debug) {
+      logger.debug([`[${scope}]`, ...args].join(' '))
+    }
+  }
+  newLogger.info = (...args: any[]) => {
+    if (level <= Level.info) {
+      logger.info([`[${scope}]`, ...args].join(' '))
+    }
+  }
+  newLogger.error = (...args: any[]) => {
+    if (level <= Level.error) {
+      logger.error([`[${scope}]`, ...args].join(' '))
+    }
+  }
   return newLogger
 }
