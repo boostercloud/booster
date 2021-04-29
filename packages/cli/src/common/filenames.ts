51be306bbd92f3da2a26754c3fd8f758aac53f34
Ricards Tagils <tagils@mail.ru>
2021-04-29 18:38:38 +0300

    feat: PascalCase resource name generation check

--- packages/cli/src/common/filenames.ts
+++ packages/cli/src/common/filenames.ts
@@ -1,9 +1,38 @@
 import * as inflected from 'inflected'
+import { logger } from '../services/logger'
+import Brand from './brand'
 
 export function classNameToFileName(name: string): string {
   return inflected.dasherize(inflected.underscore(name))
 }
 
-export function resourceNameToClassName(name: string): string {
-  return inflected.classify(inflected.underscore(inflected.parameterize(inflected.tableize(name))))
+export async function checkResourceNameIsValid(name: string): Promise<void> {
+  logger.info(Brand.mellancholize('Checking if resource has valid name...'))
+
+  if (!hasValidResourceName(name))
+    throw new Error(`'${name}' is not valid resource name. Please use PascalCase name with valid characters.`)
 }
+
+function hasValidResourceName(name: string): boolean {
+  const resourceName = formatResourceName(name)
+  return resourceName === name
+}
+
+function formatResourceName(name: string): null | string {
+  const resourceName: string = name
+    .replace(/^[\d-]|[#$-/:-?{-~!"^_`[\]]/g, ' ')
+    .replace(/\s+/g, ' ')
+    .trim()
+
+  if (resourceName === '') return null
+
+  if (resourceName.length === 1) return resourceName.toLocaleUpperCase()
+
+  const match = resourceName.match(/[a-zA-Z\d]+/g)
+
+  if (match) return match.map(titleCaseString).join('')
+
+  return resourceName
+}
+
+const titleCaseString = (value: string): string => value[0].toLocaleUpperCase() + value.slice(1)
--- packages/cli/test/common/filenames.test.ts
+++ packages/cli/test/common/filenames.test.ts
@@ -1,27 +1,101 @@
-import { classNameToFileName, resourceNameToClassName } from '../../src/common/filenames'
+import { checkResourceNameIsValid, classNameToFileName } from '../../src/common/filenames'
 import { expect } from '../expect'
+import { fake, replace, restore } from 'sinon'
+import { logger } from '../../src/services/logger'
+
+const rewire = require('rewire')
+const filenames = rewire('../../src/common/filenames')
+const formatResourceName = filenames.__get__('formatResourceName')
+const titleCaseString = filenames.__get__('titleCaseString')
 
 describe('filenames', () => {
-  describe('resourceNameToClassName', () => {
-    it('creates correct class name', () => {
-      const generatedClassName = resourceNameToClassName('testResource')
-      expect(generatedClassName).to.equal('TestResource')
+  afterEach(() => {
+    restore()
+  })
+
+  describe('checkResourceNameIsValid', () => {
+    beforeEach(() => {
+      replace(logger, 'info', fake.resolves({}))
     })
 
-    describe('generates correct class name when passed with invalid characters', () => {
-      it('resource name with space characters', () => {
-        const generatedClassName = resourceNameToClassName('test resource name')
-        expect(generatedClassName).to.equal('TestResourceName')
+    it('should print info message', async () => {
+      await checkResourceNameIsValid('TestResource')
+      expect(logger.info).to.have.been.calledWithMatch('Checking if resource has valid name...')
+    })
+
+    it('should do nothing if resource name is valid PascalCase', async () => {
+      let exceptionThrown = false
+
+      await checkResourceNameIsValid('TestResource').then(() => {
+        exceptionThrown = false
       })
 
-      it('resource name with underline character', () => {
-        const generatedClassName = resourceNameToClassName('test_resource_name')
-        expect(generatedClassName).to.equal('TestResourceName')
+      expect(exceptionThrown).to.be.equal(false)
+    })
+
+    it('should throw error if resource name is not valid PascalCase ', async () => {
+      const resourceName = 'test resource'
+      let exceptionThrown = false
+      let exceptionMessage = ''
+
+      await checkResourceNameIsValid(resourceName).catch((e) => {
+        exceptionThrown = true
+        exceptionMessage = e.message
       })
 
-      it('resource name with dash character', () => {
-        const generatedClassName = resourceNameToClassName('test-resource-name')
-        expect(generatedClassName).to.equal('TestResourceName')
+      expect(exceptionThrown).to.be.equal(true)
+      expect(exceptionMessage).to.be.equal(
+        `'${resourceName}' is not valid resource name. Please use PascalCase name with valid characters.`
+      )
+    })
+  })
+
+  describe('formatResourceName', () => {
+    describe('valid PascalCase', () => {
+      it('from string with spaces', () => {
+        expect(formatResourceName('test case')).to.be.equal('TestCase')
+      })
+
+      it('from single character string', () => {
+        expect(formatResourceName('t')).to.be.equal('T')
+      })
+
+      it('from string with spaces and invalid characters ', () => {
+        expect(formatResourceName('_ =& test resource^name*With 21   =Fields')).to.be.equal(
+          'TestResourceNameWith21Fields'
+        )
+      })
+
+      it('from string with underline characters', () => {
+        expect(formatResourceName('test_resource_name')).to.be.equal('TestResourceName')
+      })
+    })
+
+    describe('should return null when', () => {
+      it('string is empty', () => {
+        expect(formatResourceName('')).to.be.null
+      })
+
+      it('string is empty after removing invalid characters', () => {
+        expect(formatResourceName('###!##-==-_-')).to.be.null
+      })
+    })
+  })
+
+  describe('titleCaseString', () => {
+    it('should transform string to title case', () => {
+      expect(titleCaseString('test')).to.be.equal('Test')
+    })
+
+    it('should transform string[] to expected title case', () => {
+      const stringsMap: string[][] = [
+        ['first', 'First'],
+        ['second', 'Second'],
+        ['third', 'Third'],
+      ]
+
+      stringsMap.forEach((item) => {
+        expect(titleCaseString(item[0])).to.be.equal(item[1])
       })
     })
   })
