015d709cddc4fd2de5fc8a4c9dfaa2f1c5054042
juanjoman <j2rgez@gmail.com>
2021-04-29 16:15:59 +0200

    Include node_modules on sandbox project

--- packages/framework-integration-tests/integration/providers/kubernetes/deployment/setup.ts
+++ packages/framework-integration-tests/integration/providers/kubernetes/deployment/setup.ts
@@ -4,6 +4,7 @@ import { sandboxProjectName } from '../constants'
 import { overrideWithBoosterLocalDependencies } from '../../../helper/deps-helper'
 import { sleep } from '../../../helper/sleep'
 import { deploy } from '../deploy'
+import { exec } from 'child-process-promise'
 
 before(async () => {
   console.log('preparing sandboxed project...')
@@ -12,6 +13,9 @@ before(async () => {
 
   console.log('overriding Booster dependencies...')
   await overrideWithBoosterLocalDependencies(sandboxPath)
+
+  // So the docker image can find the kubernetes runtime (inside node_modules). Only needed for integration tests.
+  await exec(`cp -r node_modules ${sandboxPath}`)
 
   console.log(`starting kubernetes server in ${sandboxPath}...`)
   // start kubernetes
