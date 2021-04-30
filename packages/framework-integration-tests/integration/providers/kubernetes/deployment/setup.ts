6fc667084824a91f95d5e7620db9bd83f8e6d24b
juanjoman <j2rgez@gmail.com>
2021-04-30 13:13:11 +0200

    Run npm install on sandboxed project

--- packages/framework-integration-tests/integration/providers/kubernetes/deployment/setup.ts
+++ packages/framework-integration-tests/integration/providers/kubernetes/deployment/setup.ts
@@ -14,9 +14,9 @@ before(async () => {
   console.log('overriding Booster dependencies...')
   await overrideWithBoosterLocalDependencies(sandboxPath)
 
-  // So the docker image can find the kubernetes runtime (inside node_modules). Only needed for integration tests.
-  await exec(`cp -r node_modules ${sandboxPath}`)
-
+  // This command is also ran during the deployment, but this provider takes dependencies from the root sandboxed directory.
+  // This is a bug that only happens on integration-tests, and this is a quick workaround to avoid editing the provider or the cli package.
+  await exec('npm install --production --no-bin-links', { cwd: sandboxPath })
   console.log(`starting kubernetes server in ${sandboxPath}...`)
   // start kubernetes
   await deploy(sandboxPath)
