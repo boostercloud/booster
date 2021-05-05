e47184961a3bbf4d31973955287250be42eef89f
alvaroloes <alvaro@booster.cloud>
2021-05-05 17:58:55 +0100

    Make end-to-end tests completely provider unaware

--- packages/application-tester/src/provider-test-helper.ts
+++ packages/application-tester/src/provider-test-helper.ts
@@ -3,6 +3,20 @@ export interface ApplicationOutputs {
   websocketURL: string
 }
 
+export interface Counters {
+  subscriptions(): Promise<number>
+  connections(): Promise<number>
+  readModels(readModelName: string): Promise<number>
+  events(): Promise<number>
+}
+
+export interface Queries {
+  events(primaryKey: string): Promise<unknown>
+  readModels(primaryKey: string, readModelName: string): Promise<unknown>
+}
+
 export interface ProviderTestHelper {
   outputs: ApplicationOutputs
+  counters: Counters
+  queries: Queries
 }
