cd5907f4a2a814d6101cf17ed7c11448dc49b17a
alvaroloes <alvaro@booster.cloud>
2021-05-06 13:25:09 +0100

    Refactored integration tests structure to leave clear which ones are provider unaware

--- packages/framework-integration-tests/integration/end-to-end/setup.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationTester } from '@boostercloud/application-tester'
-import { getProviderTestHelper, setEnv } from '../helper/app-helper'
-
-export let applicationUnderTest: ApplicationTester
-
-before(async () => {
-  await setEnv()
-  applicationUnderTest = new ApplicationTester(await getProviderTestHelper())
-})
---
+++ packages/framework-integration-tests/integration/provider-unaware/functionality/scheduled-commands.integration.ts
@@ -0,0 +1,35 @@
+import gql from 'graphql-tag'
+import { expect } from 'chai'
+import { waitForIt } from '../../helper/sleep'
+import { applicationUnderTest } from './setup'
+
+describe('Scheduled commands', () => {
+  it('scheduled command ran and created a product', async () => {
+    const client = await applicationUnderTest.graphql.client()
+    const checkedCartSku = 'the-checked-cart'
+
+    // Check that scheduled command created the new product
+    const cartData = await waitForIt(
+      () => {
+        return client.query({
+          query: gql`
+            query {
+              CartReadModel(id: "${checkedCartSku}") {
+                id
+                checks
+              }
+            }
+          `,
+        })
+      },
+      (result) => !!result?.data?.CartReadModel,
+      10000,
+      90000 // CheckCartCount is run every minute, we need to give this test enough time to make sure AWS does the first call
+    )
+
+    const cartReadModel = cartData?.data?.CartReadModel
+    expect(cartReadModel).not.to.be.null
+    expect(cartReadModel.id).to.equal(checkedCartSku)
+    expect(cartReadModel.checks).to.be.greaterThan(0)
+  })
+})
