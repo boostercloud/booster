2c7d87c4a6fade6ab7c585c64ed5f6e7aa09877e
alvaroloes <alvaro@booster.cloud>
2021-05-06 17:32:41 +0100

    Remove a comment

--- packages/framework-core/test/booster-token-verifier.test.ts
+++ packages/framework-core/test/booster-token-verifier.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/camelcase */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-magic-numbers */
