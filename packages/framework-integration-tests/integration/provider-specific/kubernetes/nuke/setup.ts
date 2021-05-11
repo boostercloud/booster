90f231de45e4a63854816f51318645bc8495be46
juanjoman <j2rgez@gmail.com>
2021-05-11 14:43:17 +0200

    Create components file from config.assets

--- packages/framework-integration-tests/integration/provider-specific/kubernetes/nuke/setup.ts
+++ packages/framework-integration-tests/integration/provider-specific/kubernetes/nuke/setup.ts
@@ -1,23 +1,17 @@
 import { overrideWithBoosterLocalDependencies } from '../../../helper/deps-helper'
 import { nuke } from '../deploy'
-import { createSandboxProject, removeSandboxProject } from '../../../../../cli/src/common/sandbox'
+import { removeSandboxProject } from '../../../../../cli/src/common/sandbox'
 import { sandboxPathFor } from '../../../helper/file-helper'
 import { sandboxProjectName } from '../constants'
 import { exec } from 'child-process-promise'
-import { createDaprComponentFile } from '../util'
 
 before(async () => {
   const sandboxPath = sandboxPathFor(sandboxProjectName)
-  const configuredAssets = ['assets', 'assetFile.txt']
-  const sandboxedProject = createSandboxProject(sandboxPath, configuredAssets)
 
   await overrideWithBoosterLocalDependencies(sandboxPath)
   // Only the deploy command creates the production dependencies
   await exec('npm install --production --no-bin-links', { cwd: sandboxPath })
 
-  // boost deploy removes the dapr components folder, so we need to create it again.
-  await createDaprComponentFile(sandboxPath)
-
-  await nuke(sandboxedProject)
+  await nuke(sandboxPath)
   removeSandboxProject(sandboxPath)
 })
