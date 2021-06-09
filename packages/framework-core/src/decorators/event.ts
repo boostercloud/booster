105432fc71743b0de6eda72462979b1cfa2dc275
Nikita Tchayka <nick@booster.cloud>
2021-06-09 20:08:41 +0100

    ESLint fixes

--- packages/framework-core/src/decorators/event.ts
+++ packages/framework-core/src/decorators/event.ts
@@ -1,5 +1,5 @@
-import {Class, EventInterface} from '@boostercloud/framework-types'
-import {Booster} from '../booster'
+import { Class, EventInterface } from '@boostercloud/framework-types'
+import { Booster } from '../booster'
 
 /**
  * Annotation to tell Booster which classes are your Events
@@ -9,13 +9,13 @@ import {Booster} from '../booster'
 // Disabling unused vars here, because it won't allow us to call the decorator without parens
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function Event<TEvent extends EventInterface>(eventClass: Class<TEvent>): void {
-    Booster.configureCurrentEnv((config): void => {
-      if (config.events[eventClass.name]) {
-        throw new Error(`A event called ${eventClass.name} is already registered.
+  Booster.configureCurrentEnv((config): void => {
+    if (config.events[eventClass.name]) {
+      throw new Error(`A event called ${eventClass.name} is already registered.
         If you think that this is an error, try performing a clean build.`)
-      }
-      config.events[eventClass.name] = {
-        class: eventClass,
-      }
-    })
+    }
+    config.events[eventClass.name] = {
+      class: eventClass,
+    }
+  })
 }
