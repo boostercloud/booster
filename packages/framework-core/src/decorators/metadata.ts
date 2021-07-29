a590e7e0207acaea160f207d3816dfe5f0f04004
Nikita Tchayka <nick@booster.cloud>
2021-07-29 14:23:20 +0100

    Remove unused functions

--- packages/framework-core/src/decorators/metadata.ts
+++ packages/framework-core/src/decorators/metadata.ts
@@ -3,27 +3,9 @@ import { ClassMetadata, PropertyMetadata } from 'metadata-booster'
 import 'reflect-metadata'
 
 export function getPropertiesMetadata(classType: AnyClass): Array<PropertyMetadata> {
-  try {
-    return getMetadata(classType).fields
-  } catch (e) {
-    console.log(e.message)
-    return []
-  }
-}
-
-export function getMethodsMetadata(classType: AnyClass): Array<PropertyMetadata> {
-  try {
-    return getMetadata(classType).methods
-  } catch (e) {
-    console.log(e.message)
-    return []
-  }
-}
-
-export function getMetadata(classType: AnyClass): ClassMetadata {
   const meta: ClassMetadata = Reflect.getMetadata('booster:typeinfo', classType)
   if (!meta) {
     throw new Error(`Could not get proper metadata information of ${classType.name}`)
   }
-  return meta
+  return meta.fields
 }
