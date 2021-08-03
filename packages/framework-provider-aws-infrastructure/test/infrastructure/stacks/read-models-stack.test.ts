b623e507243cee81cdd69896fe0579ee345787bd
Javier Toledo <javier@booster.cloud>
2021-08-03 23:15:11 +0100

    Added support for read model sort keys for the AWS provider

--- packages/framework-provider-aws-infrastructure/test/infrastructure/stacks/read-models-stack.test.ts
+++ packages/framework-provider-aws-infrastructure/test/infrastructure/stacks/read-models-stack.test.ts
@@ -1,35 +1,74 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { expect } from '../../expect'
 import { describe } from 'mocha'
-import { Stack } from '@aws-cdk/core'
+import { Stack, App } from '@aws-cdk/core'
 import { BoosterConfig, UUID } from '@boostercloud/framework-types'
-import { App } from '@aws-cdk/core'
+
 import { ReadModelsStack } from '../../../src/infrastructure/stacks/read-models-stack'
 import { Table } from '@aws-cdk/aws-dynamodb'
 
 describe('ReadModelsStack', () => {
   describe('the `build` method', () => {
-    class SomeReadModel {
-      public constructor(readonly id: UUID) {}
-    }
-    const config = new BoosterConfig('test')
-    config.userProjectRootPath = '.'
-    config.readModels['SomeReadModel'] = {
-      class: SomeReadModel,
-      authorizedRoles: 'all',
-      properties: [],
-      before: [],
-    }
+    context('When no sort key has been defined', () => {
+      class SomeReadModel {
+        public constructor(readonly id: UUID) {}
+      }
+      const config = new BoosterConfig('test')
+      config.userProjectRootPath = '.'
+      config.readModels['SomeReadModel'] = {
+        class: SomeReadModel,
+        authorizedRoles: 'all',
+        properties: [],
+        before: [],
+      }
 
-    const stack = new Stack(new App(), 'some-app')
+      const stack = new Stack(new App(), 'some-app')
 
-    it('generates a DynamoDB table', () => {
-      const readModelsStack = new ReadModelsStack(config, stack)
+      it('generates a DynamoDB table with the field `id` as the partitionKey and no `sortKey`', () => {
+        const readModelsStack = new ReadModelsStack(config, stack)
 
-      const tables = readModelsStack.build()
+        const tables = readModelsStack.build()
 
-      expect(tables.length).to.be.equal(1)
-      expect(tables[0]).to.be.instanceOf(Table)
+        expect(tables.length).to.be.equal(1)
+        expect(tables[0]).to.be.instanceOf(Table)
+
+        const someReadModelTable = tables[0] as any
+        expect(someReadModelTable?.tablePartitionKey['name']).to.equal('id')
+        expect(someReadModelTable?.tablePartitionKey['type']).to.equal('S')
+        expect(someReadModelTable.tableSortKey).to.be.undefined
+      })
+    })
+
+    context('When a sort key has been defined', () => {
+      class SomeReadModel {
+        public constructor(readonly id: UUID) {}
+      }
+      const config = new BoosterConfig('test')
+      config.userProjectRootPath = '.'
+      config.readModels['SomeReadModel'] = {
+        class: SomeReadModel,
+        authorizedRoles: 'all',
+        properties: [],
+        before: [],
+      }
+      config.readModelSortKeys['SomeReadModel'] = 'timestamp'
+
+      const stack = new Stack(new App(), 'some-app')
+
+      it('generates a DynamoDB table with the field `id` as the partitionKey and the defined field as the `sortKey`', () => {
+        const readModelsStack = new ReadModelsStack(config, stack)
+
+        const tables = readModelsStack.build()
+
+        expect(tables.length).to.be.equal(1)
+        expect(tables[0]).to.be.instanceOf(Table)
+
+        const someReadModelTable = tables[0] as any
+        expect(someReadModelTable?.tablePartitionKey?.['name']).to.equal('id')
+        expect(someReadModelTable?.tablePartitionKey?.['type']).to.equal('S')
+        expect(someReadModelTable?.tableSortKey?.['name']).to.equal('timestamp')
+        expect(someReadModelTable?.tableSortKey?.['type']).to.equal('S')
+      })
     })
   })
 })
