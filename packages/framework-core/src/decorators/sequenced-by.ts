797e5bfb4514eff3d67e72e4b4c9b478914f5a2d
Javier Toledo <javier@booster.cloud>
2021-08-06 21:55:26 +0100

    Renamed the decorator as `@sequencedBy`

--- packages/framework-core/src/decorators/sequenced-by.ts
+++ packages/framework-core/src/decorators/sequenced-by.ts
@@ -2,20 +2,20 @@ import { Booster } from '../booster'
 import { Class, ReadModelInterface } from '@boostercloud/framework-types'
 import { getFunctionArguments } from './metadata'
 
-export function sortBy(klass: Class<ReadModelInterface>, _functionName: string, parameterIndex: number): void {
+export function sequencedBy(klass: Class<ReadModelInterface>, _functionName: string, parameterIndex: number): void {
   const args = getFunctionArguments(klass)
   const propertyName = args[parameterIndex]
   Booster.configureCurrentEnv((config): void => {
-    if (config.readModelSortKeys[klass.name] && config.readModelSortKeys[klass.name] !== propertyName) {
+    if (config.readModelSequenceKeys[klass.name] && config.readModelSequenceKeys[klass.name] !== propertyName) {
       throw new Error(
         `Error trying to register a sort key named \`${propertyName}\` for class \`${
           klass.name
         }\`. It already had the sort key \`${
-          config.readModelSortKeys[klass.name]
+          config.readModelSequenceKeys[klass.name]
         }\` defined and only one sort key is allowed for each read model.`
       )
     } else {
-      config.readModelSortKeys[klass.name] = propertyName
+      config.readModelSequenceKeys[klass.name] = propertyName
     }
   })
 }
