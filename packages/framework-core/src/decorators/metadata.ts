8085b10d07c367ec16b9a3418d2c2345b7f65b96
Javier Toledo <javier@theagilemonkeys.com>
2021-08-10 12:27:02 +0100

    Added a `sortBy` decorator to generate metadata about which read model field should be used as the sort key
    Also renamed the file `packages/framework-core/src/decorators/projection.ts` to match the actual decorator name.

--- packages/framework-core/src/decorators/metadata.ts
+++ packages/framework-core/src/decorators/metadata.ts
@@ -10,3 +10,33 @@ export function getPropertiesMetadata(classType: AnyClass): Array<PropertyMetada
   }
   return meta.fields
 }
+
+/**
+ * Get the argument names from a given function.
+ *
+ * This implementation is a TypeScript adaptation of a JavaScript implementation
+ * borrowed from the promisify-node code and can be found in the following link:
+ * https://github.com/nodegit/promisify-node/blob/02fc47cfc00146a533193bc4740e2e3e3be81c6f/utils/args.js
+ *
+ * @param {Function} func - The function to parse.
+ * @returns {Array} arg - List of arguments in the function.
+ */
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function getFunctionArguments(func: Function): Array<string> {
+  // First match everything inside the function argument parens.
+  const args = func.toString().match(/([^(])*\(([^)]*)\)/)?.[2]
+
+  if (!args) return []
+
+  // Split the arguments string into an array comma delimited.
+  return args
+    .split(', ')
+    .map(function (arg) {
+      // Ensure no inline comments are parsed and trim the whitespace.
+      return arg.replace(/\/\*.*\*\//, '').trim()
+    })
+    .filter(function (arg) {
+      // Ensure no undefineds are added.
+      return arg
+    })
+}
