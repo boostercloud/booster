f931a1461c844a5460680ee9996abfceffb0d881
Javier Toledo <javier@theagilemonkeys.com>
2021-08-14 12:36:04 +0100

    Added basic property tests for the searcher class

--- packages/framework-types/test/searcher.test.ts
+++ packages/framework-types/test/searcher.test.ts
@@ -7,30 +7,68 @@ describe('the `Searcher` class', () => {
 
   const searcherFunction = fake()
   const finderByKeyFunction = fake()
+  let searcher: Searcher<SomeModel>
+
+  beforeEach(() => {
+    searcher = new Searcher(SomeModel, searcherFunction, finderByKeyFunction)
+  })
 
   context('with a valid searcher class', () => {
-    const searcher = new Searcher(SomeModel, searcherFunction, finderByKeyFunction)
-
     describe('the constructor', () => {
       it('creates a valid searcher instance', () => {
-        expect(searcher).to.be.a('Searcher')
+        // The searcher instance won't respond with the right class
+        // when using a conventional typeOf call because the Searcher class 
+        // is a generic. The instance keeps a reference to the 
+        // constructor that we can use though.
+        expect(searcher.constructor.name).to.be.equal('Searcher')
       })
     })
 
     describe('the method `filter`', () => {
-      it('...') // TODO
+      it('adds an array of filters to the searcher and returns the searcher', () => {
+        const fakeFilters = [{name: 'a'},{name: 'b'}]
+
+        const newSearcher = searcher.filter(fakeFilters)
+
+        expect(newSearcher).to.be.deep.equal(searcher)
+        expect(newSearcher as any).to.have.deep.property('filters', fakeFilters)
+      })
     })
 
     describe('the method `limit`', () => {
-      it('...') // TODO
+      it('sets a limit in the searcher and returns the searcher', () => {
+        const limit = 42
+
+        const newSearcher = searcher.limit(limit)
+
+        expect(newSearcher).to.be.deep.equal(searcher)
+        expect(newSearcher as any).to.have.property('_limit', limit)
+      })
     })
 
     describe('the method `afterCursor`', () => {
-      it('...') // TODO
+      it('sets the afterCursor in the searcher and returns the searcher', () => {
+        const afterCursor = 42
+
+        const newSearcher = searcher.afterCursor(afterCursor)
+
+        expect(newSearcher).to.be.deep.equal(searcher)
+        expect(newSearcher as any).to.have.property('_afterCursor', afterCursor)
+      })
     })
 
     describe('the method `paginatedVersion`', () => {
-      it('...') // TODO
+      it('sets the paginatedVersion in the searcher and returns the searcher', () => {
+        // Check that the default is false
+        expect(searcher as any).to.have.property('_paginatedVersion', false)
+
+        const paginatedVersion = true
+
+        const newSearcher = searcher.paginatedVersion(paginatedVersion)
+
+        expect(newSearcher).to.be.deep.equal(searcher)
+        expect(newSearcher as any).to.have.property('_paginatedVersion', paginatedVersion)
+      })
     })
 
     describe('the method `findById`', () => {
