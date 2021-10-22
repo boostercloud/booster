2ad574bd78a15c5b286f95fc037f537d61e10b9f
Ricards Tagils <tagils@mail.ru>
2021-10-22 14:29:36 +0300

    feat(cli): New add method commands

---
+++ packages/cli/test/commands/add/projection.test.ts
@@ -0,0 +1,221 @@
+import { fake, replace, restore, stub, spy } from 'sinon'
+import { IConfig } from '@oclif/config'
+import { Project, MethodDeclarationStructure, SourceFile, ClassDeclaration } from 'ts-morph'
+import * as ProjectChecker from '../../../src/services/project-checker'
+import { expect } from '../../expect'
+import Projection from '../../../src/commands/add/projection'
+import * as MethodGenerator from '../../../src/services/method-generator'
+import * as Filenames from '../../../src/common/filenames'
+import { parseProjectionField } from '../../../src/services/generator/target'
+
+describe('add', async () => {
+  describe('projection', async () => {
+    const readModelName = 'PostReadModel'
+    const projectionName = 'Post:id'
+    const sourceFileText = `
+      import { ReadModel } from '@boostercloud/framework-core'
+      import { UUID } from '@boostercloud/framework-types'
+      
+      @ReadModel({
+        authorize: 'all',
+      })
+      export class PostReadModel {
+        public constructor(public id: UUID, readonly title: string, readonly author: string) {}
+      }
+    `
+
+    beforeEach(() => {
+      stub(ProjectChecker, 'checkCurrentDirIsABoosterProject').returnsThis()
+      replace(ProjectChecker, 'checkCurrentDirBoosterVersion', fake.resolves({}))
+      replace(Filenames, 'fileNameWithExtension', fake.returns('post-read-model.ts'))
+    })
+
+    afterEach(() => {
+      restore()
+    })
+
+    it('init calls checkCurrentDirBoosterVersion', async () => {
+      await new Projection([], {} as IConfig).init()
+      expect(ProjectChecker.checkCurrentDirBoosterVersion).to.have.been.called
+    })
+
+    it('generates projection correctly', async () => {
+      const { projection } = await parseProjectionField(projectionName)
+
+      const project = new Project()
+      const fakeSourceFile = project.createSourceFile('post.ts', sourceFileText)
+
+      stub(Project.prototype, 'getSourceFileOrThrow').returns(fakeSourceFile)
+      stub(MethodGenerator, 'generateProjection').returns({} as MethodDeclarationStructure)
+      stub(SourceFile.prototype, 'getClassOrThrow').returns({
+        addMethod: stub(),
+      } as unknown as ClassDeclaration)
+
+      stub(SourceFile.prototype, 'fixMissingImports').returnsThis()
+      stub(SourceFile.prototype, 'save').resolves()
+
+      await new Projection(['--read-model', readModelName, '--entity', projectionName], {} as IConfig).run()
+
+      expect(Filenames.fileNameWithExtension).to.have.been.calledWith(readModelName)
+      expect(Project.prototype.getSourceFileOrThrow).to.have.been.calledOnceWith('post-read-model.ts')
+      expect(MethodGenerator.generateProjection).to.have.been.calledOnceWith(readModelName, projection)
+      expect(SourceFile.prototype.getClassOrThrow).to.have.been.calledOnceWith(readModelName)
+      expect(SourceFile.prototype.fixMissingImports).to.have.been.calledOnce
+      expect(SourceFile.prototype.save).to.have.been.calledOnce
+    })
+
+    describe('displays an error', () => {
+      it('with flags missing', async () => {
+        let exceptionThrown = false
+        let exceptionMessage = null
+
+        try {
+          await new Projection([], {} as IConfig).run()
+        } catch (e) {
+          exceptionThrown = true
+          exceptionMessage = e.message
+        }
+
+        expect(exceptionThrown).to.be.true
+        expect(exceptionMessage).to.contain('Missing required flag')
+        expect(exceptionMessage).to.contain('--read-model READ-MODEL')
+      })
+
+      it('with empty --read-model', async () => {
+        let exceptionThrown = false
+        let exceptionMessage = null
+
+        try {
+          await new Projection(['--entity', projectionName, '--read-model'], {} as IConfig).run()
+        } catch (e) {
+          exceptionThrown = true
+          exceptionMessage = e.message
+        }
+
+        expect(exceptionThrown).to.be.true
+        expect(exceptionMessage).to.contain('--read-model expects a value')
+      })
+
+      it('with empty --entity', async () => {
+        let exceptionThrown = false
+        let exceptionMessage = null
+
+        try {
+          await new Projection(['--read-model', readModelName, '--entity'], {} as IConfig).run()
+        } catch (e) {
+          exceptionThrown = true
+          exceptionMessage = e.message
+        }
+
+        expect(exceptionThrown).to.be.true
+        expect(exceptionMessage).to.contain('--entity expects a value')
+      })
+
+      it("when projection doesn't have entity id", async () => {
+        let exceptionThrown = false
+        let exceptionMessage = null
+
+        try {
+          await new Projection(['--read-model', readModelName, '--entity', 'Post'], {} as IConfig).run()
+        } catch (e) {
+          exceptionThrown = true
+          exceptionMessage = e.message
+        }
+
+        expect(exceptionThrown).to.be.true
+        expect(exceptionMessage).to.contain(
+          'Error parsing projection Post. Projections must be in the form of <entity name>:<entity id>'
+        )
+      })
+
+      it('when projection has empty entity id', async () => {
+        let exceptionThrown = false
+        let exceptionMessage = null
+
+        try {
+          await new Projection(['--read-model', readModelName, '--entity', 'Post:'], {} as IConfig).run()
+        } catch (e) {
+          exceptionThrown = true
+          exceptionMessage = e.message
+        }
+
+        expect(exceptionThrown).to.be.true
+        expect(exceptionMessage).to.contain(
+          'Error parsing projection Post:. Projections must be in the form of <entity name>:<entity id>'
+        )
+      })
+
+      it('when projection has empty entity name', async () => {
+        let exceptionThrown = false
+        let exceptionMessage = null
+
+        try {
+          await new Projection(['--read-model', readModelName, '--entity', ':id'], {} as IConfig).run()
+        } catch (e) {
+          exceptionThrown = true
+          exceptionMessage = e.message
+        }
+
+        expect(exceptionThrown).to.be.true
+        expect(exceptionMessage).to.contain(
+          'Error parsing projection :id. Projections must be in the form of <entity name>:<entity id>'
+        )
+      })
+
+      it("when source file doesn't exist", async () => {
+        stub(Project.prototype, 'getSourceFileOrThrow').throws()
+        const sourceFileSpy = spy(SourceFile.prototype)
+        const methodGeneratorSpy = spy(MethodGenerator.generateProjection)
+        const classDeclarationSpy = spy(ClassDeclaration.prototype)
+
+        let exceptionThrown = false
+
+        try {
+          await new Projection(['--read-model', readModelName, '--entity', projectionName], {} as IConfig).run()
+        } catch (e) {
+          exceptionThrown = true
+        }
+
+        expect(exceptionThrown).to.be.true
+        expect(Filenames.fileNameWithExtension).to.have.been.calledWith(readModelName)
+        expect(Project.prototype.getSourceFileOrThrow).to.have.been.calledOnceWith('post-read-model.ts')
+        expect(classDeclarationSpy.addMethod).to.have.not.been.calledOnce
+        expect(sourceFileSpy.getClassOrThrow).to.have.not.been.called
+        expect(sourceFileSpy.fixMissingImports).to.have.not.been.called
+        expect(sourceFileSpy.save).to.have.not.been.called
+        expect(methodGeneratorSpy).to.have.not.been.called
+      })
+
+      it("when class doesn't exist in source file", async () => {
+        const project = new Project()
+        const fakeSourceFile = project.createSourceFile('post.ts', sourceFileText)
+
+        stub(Project.prototype, 'getSourceFileOrThrow').returns(fakeSourceFile)
+        replace(SourceFile.prototype, 'getClassOrThrow', fake.throws(new Error()))
+        replace(SourceFile.prototype, 'fixMissingImports', spy())
+        replace(SourceFile.prototype, 'save', spy())
+
+        const methodGeneratorSpy = spy(MethodGenerator.generateProjection)
+        const classDeclarationSpy = spy(ClassDeclaration.prototype)
+
+        let exceptionThrown = false
+
+        try {
+          await new Projection(['--read-model', readModelName, '--entity', projectionName], {} as IConfig).run()
+        } catch (e) {
+          exceptionThrown = true
+        }
+
+        expect(exceptionThrown).to.be.true
+        expect(Filenames.fileNameWithExtension).to.have.been.calledWith(readModelName)
+        expect(Project.prototype.getSourceFileOrThrow).to.have.been.calledOnceWith('post-read-model.ts')
+        expect(SourceFile.prototype.getClassOrThrow).to.throw
+        expect(SourceFile.prototype.fixMissingImports).to.have.not.been.called
+        expect(SourceFile.prototype.save).to.have.not.been.called
+        expect(methodGeneratorSpy).to.have.not.been.called
+        expect(methodGeneratorSpy).to.have.not.been.called
+        expect(classDeclarationSpy.addMethod).to.have.not.been.calledOnce
+      })
+    })
+  })
+})
---
+++ packages/cli/test/services/method-generator.test.ts
@@ -0,0 +1,251 @@
+import { HasName, HasProjection, HasReaction } from '../../src/services/generator/target'
+import { expect } from '../expect'
+import { generateProjection, generateReducers, getResourceSourceFile } from '../../src/services/method-generator'
+import { replace, stub, fake, restore } from 'sinon'
+import { fileNameWithExtension } from '../../src/common/filenames'
+import * as Filenames from '../../src/common/filenames'
+import { Project } from 'ts-morph'
+
+describe('method generator', (): void => {
+  it('generates projection', async () => {
+    type ProjectionInfo = HasName & HasProjection
+
+    const info: ProjectionInfo = {
+      name: 'PostReadModel',
+      projection: { entityName: 'Post', entityId: 'id' },
+    }
+
+    const projectionMethodParams = await generateProjection(info.name, info.projection)
+
+    expect(projectionMethodParams)
+      .to.be.an('object')
+      .that.has.all.keys(['decorators', 'scope', 'isStatic', 'name', 'parameters', 'returnType', 'statements'])
+
+    expect(projectionMethodParams).to.be.deep.equal({
+      decorators: [
+        {
+          name: 'Projects',
+          arguments: [info.projection.entityName, `'${info.projection.entityId}'`],
+        },
+      ],
+      scope: 'public',
+      isStatic: true,
+      name: `project${info.projection.entityName}`,
+      parameters: [
+        {
+          name: 'entity',
+          type: info.projection.entityName,
+        },
+        {
+          name: `current${info.name}`,
+          type: info.name,
+          hasQuestionToken: true,
+        },
+      ],
+      returnType: `ProjectionResult<${info.name}>`,
+      statements: [`return /* NEW ${info.name} HERE */`],
+    })
+  })
+
+  it('generates reducers', async () => {
+    type ReducerInfo = HasName & HasReaction
+
+    it('generates single reducer', async () => {
+      const info: ReducerInfo = {
+        name: 'Post',
+        events: [{ eventName: 'PostCreated' }],
+      }
+
+      const reducerMethodParams = await generateReducers(info.name, info.events)
+
+      expect(reducerMethodParams).to.have.lengthOf(1)
+      expect(reducerMethodParams[0]).to.deep.include({ name: `reduce${info.events[0].eventName}` })
+      expect(reducerMethodParams[0])
+        .to.be.an('object')
+        .that.has.all.keys(['decorators', 'scope', 'isStatic', 'name', 'parameters', 'returnType', 'statements'])
+
+      expect(reducerMethodParams).to.have.deep.members([
+        {
+          decorators: [
+            {
+              name: 'Reduces',
+              arguments: [info.events[0].eventName],
+            },
+          ],
+          scope: 'public',
+          isStatic: true,
+          name: `reduce${info.events[0].eventName}`,
+          returnType: info.name,
+          parameters: [
+            {
+              name: 'event',
+              type: info.events[0].eventName,
+            },
+            {
+              name: `current${info.name}`,
+              type: info.name,
+              hasQuestionToken: true,
+            },
+          ],
+          statements: [`return /* NEW ${info.name} HERE */`],
+        },
+      ])
+    })
+
+    it('generates multiple reducers', async () => {
+      const info: ReducerInfo = {
+        name: 'Post',
+        events: [{ eventName: 'PostCreated' }, { eventName: 'PostUpdated' }],
+      }
+
+      const reducerMethodParams = await generateReducers(info.name, info.events)
+
+      expect(reducerMethodParams).to.have.lengthOf(2)
+      reducerMethodParams.forEach((member, index) => {
+        expect(member.name).to.be.equal(`reduce${info.events[index].eventName}`)
+
+        expect(member)
+          .to.be.an('object')
+          .that.has.all.keys(['decorators', 'scope', 'isStatic', 'name', 'parameters', 'returnType', 'statements'])
+
+        expect(member).to.deep.include({
+          scope: 'public',
+          isStatic: true,
+          returnType: info.name,
+          statements: [`return /* NEW ${info.name} HERE */`],
+          decorators: [
+            {
+              name: 'Reduces',
+              arguments: [info.events[index].eventName],
+            },
+          ],
+          name: `reduce${info.events[index].eventName}`,
+          parameters: [
+            {
+              name: 'event',
+              type: info.events[index].eventName,
+            },
+            {
+              name: `current${info.name}`,
+              type: info.name,
+              hasQuestionToken: true,
+            },
+          ],
+        })
+      })
+
+      expect(reducerMethodParams).to.have.deep.members([
+        {
+          scope: 'public',
+          isStatic: true,
+          returnType: info.name,
+          statements: [`return /* NEW ${info.name} HERE */`],
+          decorators: [
+            {
+              name: 'Reduces',
+              arguments: [info.events[0].eventName],
+            },
+          ],
+          name: `reduce${info.events[0].eventName}`,
+          parameters: [
+            {
+              name: 'event',
+              type: info.events[0].eventName,
+            },
+            {
+              name: `current${info.name}`,
+              type: info.name,
+              hasQuestionToken: true,
+            },
+          ],
+        },
+        {
+          scope: 'public',
+          isStatic: true,
+          returnType: info.name,
+          statements: [`return /* NEW ${info.name} HERE */`],
+          decorators: [
+            {
+              name: 'Reduces',
+              arguments: [info.events[1].eventName],
+            },
+          ],
+          name: `reduce${info.events[1].eventName}`,
+          parameters: [
+            {
+              name: 'event',
+              type: info.events[1].eventName,
+            },
+            {
+              name: `current${info.name}`,
+              type: info.name,
+              hasQuestionToken: true,
+            },
+          ],
+        },
+      ])
+    })
+  })
+
+  describe('getResourceSourceFile', () => {
+    afterEach(() => {
+      restore()
+    })
+
+    it('should return source file', () => {
+      const resourceName = 'Post'
+      replace(Filenames, 'fileNameWithExtension', fake.returns('post.ts'))
+
+      const project = new Project()
+      const fileText = `
+      import { Entity } from '@boostercloud/framework-core'
+      import { UUID } from '@boostercloud/framework-types'
+      
+      @Entity
+      export class Post {
+        public constructor(public id: UUID, readonly title: string, readonly content: string, readonly author: string) {}
+      }`
+
+      const fakeSourceFile = project.createSourceFile('src/entities/post.ts', fileText)
+      stub(Project.prototype, 'getSourceFileOrThrow').returns(fakeSourceFile)
+
+      const sourceFile = getResourceSourceFile(resourceName)
+      const sourceFileClasses = sourceFile.getClasses().map((className) => className.getName())
+
+      expect(Project.prototype.getSourceFileOrThrow).to.have.been.called
+      expect(Project.prototype.getSourceFileOrThrow).to.have.been.calledOnce
+      expect(Project.prototype.getSourceFileOrThrow).to.not.have.been.calledTwice
+      expect(Project.prototype.getSourceFileOrThrow).to.have.been.calledWith(fileNameWithExtension(resourceName))
+
+      expect(sourceFile).to.be.equal(fakeSourceFile)
+      expect(fileNameWithExtension).to.have.been.calledWith(resourceName)
+      expect(sourceFileClasses).to.contain(resourceName)
+      expect(sourceFile.getFilePath()).to.match(/src\/entities\/post.ts/)
+    })
+
+    it("should throw error if source file doesn't exist", () => {
+      replace(Filenames, 'fileNameWithExtension', fake.returns('fake-post.ts'))
+      stub(Project.prototype, 'getSourceFileOrThrow').throws(
+        new Error('Could not find source file in project with the provided file name: fake-post.ts')
+      )
+
+      const resourceName = 'FakePost'
+      let exceptionThrown = false
+      let exceptionMessage = ''
+
+      try {
+        getResourceSourceFile(resourceName)
+      } catch (e) {
+        exceptionThrown = true
+        exceptionMessage = e.message
+      }
+
+      expect(fileNameWithExtension).to.have.been.calledWith(resourceName)
+      expect(getResourceSourceFile).to.throw()
+      expect(exceptionThrown).to.be.true
+      expect(exceptionMessage).to.be.equal(
+        'Could not find source file in project with the provided file name: fake-post.ts'
+      )
+    })
+  })
+})
