7ebeed1fb511608786d9372cb5d9a0bf31ccc5b2
gonzalogarcia <gonzalo@theagilemonkeys.com>
2021-11-04 13:24:43 +0000

    feature(azure): GitHub action Azure integration tests

--- packages/framework-provider-azure-infrastructure/src/infrastructure/helper/az-cli-helper.ts
+++ packages/framework-provider-azure-infrastructure/src/infrastructure/helper/az-cli-helper.ts
@@ -5,7 +5,7 @@ const azCommand = 'az'
 
 export async function getResourceGroup(appName: string): Promise<ResourceGroup> {
   const resourceGroupName = `resource-group-${appName}-azure`
-  console.log(`Get resource group ${appName}`)
+  console.log(`Get resource group ${resourceGroupName}`)
   const command = await runCommand('.', `${azCommand} group show --name ${resourceGroupName}`, true)
   if (command?.stdout.includes('could not be found')) {
     return Promise.reject(`Resource Group for application ${appName} does not exist`)
