fb79a008979bfad107b9fa3b58c002091324b408
gonzalogarcia <gonzalo@theagilemonkeys.com>
2021-11-11 14:10:17 +0000

    feat(azure): Fix class dependency

--- packages/framework-provider-azure-infrastructure/src/index.ts
+++ packages/framework-provider-azure-infrastructure/src/index.ts
@@ -1,6 +1,8 @@
 import { deploy, nuke } from './infrastructure'
 import { BoosterConfig, Logger, ProviderInfrastructure, RocketDescriptor } from '@boostercloud/framework-types'
 
+export * from './test-helper/azure-test-helper'
+
 export const Infrastructure = (rocketDescriptors?: RocketDescriptor[]): ProviderInfrastructure => {
   return {
     deploy: async (config: BoosterConfig, logger: Logger) => await deploy(config, logger),
