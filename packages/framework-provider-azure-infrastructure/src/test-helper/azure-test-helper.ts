95efa881e492cf2e228fe67df350348434063282
Roberto Diaz <roberto@theagilemonkeys.com>
2021-11-12 09:33:09 +0000

    starting working on unaware tests

--- packages/framework-provider-azure-infrastructure/src/test-helper/azure-test-helper.ts
+++ packages/framework-provider-azure-infrastructure/src/test-helper/azure-test-helper.ts
@@ -1,8 +1,21 @@
 import { configuration } from '../infrastructure/params'
 import { getResourceGroup } from '../infrastructure/helper/az-cli-helper'
 import { ResourceGroup } from '../infrastructure/types/resource-group'
+import { AzureCounters } from './azure-counters'
+import { AzureQueries } from './azure-queries'
+
+interface ApplicationOutputs {
+  graphqlURL: string
+  websocketURL: string
+}
 
 export class AzureTestHelper {
+  private constructor(
+    readonly outputs: ApplicationOutputs,
+    readonly counters: AzureCounters,
+    readonly queries: AzureQueries
+  ) {}
+
   public static async checkResourceGroup(applicationName: string): Promise<ResourceGroup> {
     console.log('Check resource group')
     return getResourceGroup(applicationName)
@@ -10,7 +23,14 @@ export class AzureTestHelper {
 
   public static async build(): Promise<AzureTestHelper> {
     this.ensureAzureConfiguration()
-    return new AzureTestHelper()
+    return new AzureTestHelper(
+      {
+        graphqlURL: await this.graphqlURL(),
+        websocketURL: await this.websocketURL(),
+      },
+      new AzureCounters(db, config),
+      new AzureQueries()
+    )
   }
 
   public static ensureAzureConfiguration(): void {
@@ -24,5 +44,29 @@ export class AzureTestHelper {
     } else {
       console.log('Azure Region set to ' + configuration.region)
     }
+  }
+
+  // todo
+  private static async graphqlURL(): Promise<string> {
+    const url = stack.Outputs?.find((output) => {
+      return output.OutputKey === 'graphqlURL'
+    })?.OutputValue
+
+    if (!url) {
+      throw 'Unable to get the Base HTTP URL from the current stack'
+    }
+    return url
+  }
+
+  // todo
+  private static async websocketURL(): Promise<string> {
+    const url = stack.Outputs?.find((output) => {
+      return output.OutputKey === 'websocketURL'
+    })?.OutputValue
+
+    if (!url) {
+      throw 'Unable to get the Base Websocket URL from the current stack'
+    }
+    return url
   }
 }
