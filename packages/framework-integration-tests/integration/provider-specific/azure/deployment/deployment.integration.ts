2ccedc44ba3ccd4c7143808263b9cfdfbf4d417d
Gonzalo Garcia Jaubert <gjaubert@gmail.com>
2021-12-28 10:12:47 +0000

    feat(core): Rockets 2.0. Fail if env variable is not set on Azure integration tests (#1006)

--- packages/framework-integration-tests/integration/provider-specific/azure/deployment/deployment.integration.ts
+++ packages/framework-integration-tests/integration/provider-specific/azure/deployment/deployment.integration.ts
@@ -1,6 +1,6 @@
 import { AzureTestHelper } from '@boostercloud/framework-provider-azure-infrastructure'
 import { expect } from '../../../helper/expect'
-import { applicationName } from '../../../helper/app-helper'
+import { applicationName, checkAndGetCurrentEnv } from '../../../helper/app-helper'
 
 describe('After deployment', () => {
   describe('the ARM template', () => {
@@ -8,7 +8,7 @@ describe('After deployment', () => {
       // The project must have been deployed by the cliHelper hook in setup.ts
       // that scripts uses the cli to do the deployment, so we just check here
       // that the resource group exists
-      const environmentName = process.env.BOOSTER_ENV ?? 'azure'
+      const environmentName = checkAndGetCurrentEnv()
       await expect(AzureTestHelper.checkResourceGroup(applicationName(), environmentName)).to.be.eventually.fulfilled
     })
   })
