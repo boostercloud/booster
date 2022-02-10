e4b2d4eb2b9378583dd363a9665f10334f31665f
charlietfe <carlos@theagilemonkeys.com>
2022-02-10 15:48:03 +0000

    Allow empty commands

--- packages/framework-core/src/services/graphql/graphql-mutation-generator.ts
+++ packages/framework-core/src/services/graphql/graphql-mutation-generator.ts
@@ -1,4 +1,4 @@
-import { ResolverBuilder, TargetTypesMap } from './common'
+import { ResolverBuilder, TargetTypeMetadata, TargetTypesMap } from './common'
 import { GraphQLTypeInformer } from './graphql-type-informer'
 import { GraphQLFieldConfigMap, GraphQLObjectType, GraphQLNonNull } from 'graphql'
 
@@ -24,16 +24,24 @@ export class GraphQLMutationGenerator {
     const mutations: GraphQLFieldConfigMap<any, any> = {}
     for (const name in this.targetTypes) {
       const type = this.targetTypes[name]
+      const input = this.generateInputForType(type)
       mutations[name] = {
         type: this.typeInformer.getGraphQLTypeFor(type.returnClass ?? Boolean),
         args: {
-          input: {
-            type: new GraphQLNonNull(this.typeInformer.getGraphQLInputTypeFor(type.class)),
-          },
+          ...input,
         },
         resolve: this.mutationResolver(type.class),
       }
     }
     return mutations
+  }
+
+  private generateInputForType(type: TargetTypeMetadata): any {
+    if (type.properties.length === 0) return {}
+    return {
+      input: {
+        type: new GraphQLNonNull(this.typeInformer.getGraphQLInputTypeFor(type.class)),
+      },
+    }
   }
 }
