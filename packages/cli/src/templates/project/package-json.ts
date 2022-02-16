c11c8f28122f5f3d55cbbea9aaf6f16746707694
gonzalogarcia <gonzalo@theagilemonkeys.com>
2022-02-16 18:40:04 +0000

    update package.json 's'

--- packages/cli/src/templates/project/package-json.ts
+++ packages/cli/src/templates/project/package-json.ts
@@ -29,7 +29,7 @@ export const template = `{
     "ts-node": "9.1.1",
     "@types/node": "15.0.2",
     "ttypescript": "1.5.12",
-    "metadata-booster": "0.4.0"
+    "metadata-booster": "0.4.1"
   },
   "engines": {
     "node": ">=14.0.0"
