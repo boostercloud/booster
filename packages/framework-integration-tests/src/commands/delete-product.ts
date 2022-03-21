e032fe9efa7ed1dabc96304a52e851961cce62a8
gonzalogarcia <gonzalo@theagilemonkeys.com>
2022-03-21 12:21:14 +0000

    After ReadModel

--- packages/framework-integration-tests/src/commands/delete-product.ts
+++ packages/framework-integration-tests/src/commands/delete-product.ts
@@ -1,21 +1,15 @@
 import { Command } from '@boostercloud/framework-core'
-import { CommandInput, Register, UUID } from '@boostercloud/framework-types'
+import { Register, UUID } from '@boostercloud/framework-types'
 import { ProductDeleted } from '../events/product-deleted'
-import { ProductAvailabilityChanged } from '../events/product-availability-changed'
+import { Admin } from '../roles'
 
 @Command({
-  authorize: 'all',
-  after: [DeleteProduct.afterFn],
+  authorize: [Admin],
 })
 export class DeleteProduct {
-  public constructor(readonly productId: UUID, readonly deleteReason: string) {}
+  public constructor(readonly productId: UUID) {}
 
   public static async handle(command: DeleteProduct, register: Register): Promise<void> {
     register.events(new ProductDeleted(command.productId))
-  }
-
-  public static async afterFn(result: unknown, command: CommandInput, register: Register): Promise<void> {
-    const productId = (command as DeleteProduct).productId
-    register.events(new ProductAvailabilityChanged(productId, 0))
   }
 }
