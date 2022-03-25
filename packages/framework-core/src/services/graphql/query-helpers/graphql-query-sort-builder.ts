b8f0fa5ab30af6bb9eefa6f9a799f6f0f1ddd00f
gonzalogarcia <gonzalo@theagilemonkeys.com>
2022-03-25 16:54:15 +0000

    SortBy enum graphql

--- packages/framework-core/src/services/graphql/query-helpers/graphql-query-sort-builder.ts
+++ packages/framework-core/src/services/graphql/query-helpers/graphql-query-sort-builder.ts
@@ -1,17 +1,15 @@
 import { AnyClass } from '@boostercloud/framework-types'
 import {
+  GraphQLEnumType,
   GraphQLFieldConfigArgumentMap,
   GraphQLInputFieldConfigMap,
   GraphQLInputObjectType,
-  GraphQLNonNull,
-  GraphQLScalarType,
   Thunk,
 } from 'graphql'
 import { PropertyMetadata } from 'metadata-booster'
 import { buildGraphqlSimpleEnumFor, isExternalType } from '../common'
 import { GraphQLTypeInformer } from '../graphql-type-informer'
 import { getClassMetadata } from '../../../decorators/metadata'
-import { GraphQLJSONObject } from 'graphql-type-json'
 
 export class GraphqlQuerySortBuilder {
   private generatedSortByByTypeName: Record<string, GraphQLInputObjectType> = {}
@@ -30,41 +28,34 @@ export class GraphqlQuerySortBuilder {
     return args
   }
 
-  private generateSortFor(prop: PropertyMetadata): GraphQLInputObjectType | GraphQLScalarType {
+  private generateSortFor(prop: PropertyMetadata): GraphQLInputObjectType | GraphQLEnumType {
     let sortByName = `${prop.typeInfo.name}PropertySortBy`
     sortByName = sortByName.charAt(0).toUpperCase() + sortByName.substr(1).replace(/\[]/g, '')
 
     if (this.generatedSortByByTypeName[sortByName]) return this.generatedSortByByTypeName[sortByName]
     let fields: Thunk<GraphQLInputFieldConfigMap> = {}
-    //     checks: {
-    //       checks: ASC
-    //     },
-    // TODO debe ser checks: ASC solamente
 
-    if (prop.typeInfo.name === 'UUID' || prop.typeInfo.name === 'Date' || prop.typeInfo.typeGroup === 'Array') {
-      fields = { [prop.name]: { type: this.orderType } }
-    } else if (prop.typeInfo.type && prop.typeInfo.typeGroup === 'Class') {
-      if (isExternalType(prop.typeInfo)) return GraphQLJSONObject // TODO ?
+    // TODO if Array??
+
+    if (prop.typeInfo.type && prop.typeInfo.typeGroup === 'Class') {
+      if (isExternalType(prop.typeInfo)) return this.orderType
       let nestedProperties: GraphQLInputFieldConfigMap = {}
       const metadata = getClassMetadata(prop.typeInfo.type)
-      if (metadata.fields.length === 0) return GraphQLJSONObject
+      if (metadata.fields.length === 0) return this.orderType
 
       this.typeInformer.generateGraphQLTypeForClass(prop.typeInfo.type, true)
 
       for (const prop of metadata.fields) {
-        const property = { [prop.name]: { type: this.orderType } }
+        const property = { [prop.name]: { type: this.generateSortFor(prop) } }
         nestedProperties = { ...nestedProperties, ...property }
       }
       fields = () => ({
         ...nestedProperties,
       })
-    } else if (prop.typeInfo.type && prop.typeInfo.type.name !== 'Object') {
-      fields = { [prop.name]: { type: this.orderType } }
-    } else {
-      return GraphQLJSONObject
+      this.generatedSortByByTypeName[sortByName] = new GraphQLInputObjectType({ name: sortByName, fields })
+      return this.generatedSortByByTypeName[sortByName]
     }
 
-    this.generatedSortByByTypeName[sortByName] = new GraphQLInputObjectType({ name: sortByName, fields })
-    return this.generatedSortByByTypeName[sortByName]
+    return this.orderType
   }
 }
