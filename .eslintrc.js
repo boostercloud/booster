6453b14037879a16e7bb3ba7e9970c56341bbdcf
Nikita Tchayka <nick@booster.cloud>
2022-06-16 13:21:42 +0100

    Disable extra-parens-rule
    It is already handled by prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,6 +36,7 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 0,
     '@typescript-eslint/ban-ts-ignore': 0,
     '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/no-extra-parens': 0,
     '@typescript-eslint/explicit-function-return-type': [
       'warn',
       {
