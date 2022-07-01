a993ba03dcd405ff1fc298707ec248e898474880
Nick Tchayka <nick@booster.cloud>
2022-07-01 18:22:04 +0100

    chore: Move metadata-booster to main repo

---
+++ packages/metadata-booster/src/metadata-types.ts
@@ -0,0 +1,40 @@
+export type ClassType = { new (...args: unknown[]): unknown }
+
+// type instead of enum to be able to install this package as a devDependency and not a production dependency
+export type TypeGroup =
+  | 'String'
+  | 'Number'
+  | 'Boolean'
+  | 'Enum'
+  | 'Union'
+  | 'Intersection'
+  | 'Function'
+  | 'Class'
+  | 'Interface'
+  | 'Type'
+  | 'Array'
+  | 'Object'
+  | 'ReadonlyArray'
+  | 'Other'
+
+export interface TypeMetadata {
+  name: string
+  typeGroup: TypeGroup
+  parameters: Array<TypeMetadata>
+  isNullable: boolean
+  typeName?: string
+  importPath?: string
+  type?: ClassType
+}
+
+export interface PropertyMetadata {
+  name: string
+  typeInfo: TypeMetadata
+}
+
+export interface ClassMetadata {
+  name: string
+  type: ClassType
+  fields: Array<PropertyMetadata>
+  methods: Array<PropertyMetadata>
+}
