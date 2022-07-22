8be198018dd8f3e233902045870031755365bb00
Nick Tchayka <nick@booster.cloud>
2022-07-22 12:30:52 +0100

    fix: Integration tests after metadata-booster (#1143)
    * Fix format issue
    
    * And another one
    
    * Add newline
    
    * Fix format
    
    * Make package public

--- packages/cli/src/templates/project/tsconfig-json.ts
+++ packages/cli/src/templates/project/tsconfig-json.ts
@@ -14,11 +14,8 @@ export const template = `{
     "noFallthroughCasesInSwitch": true,
     "forceConsistentCasingInFileNames": true,
     "experimentalDecorators": true,
-    "plugins": [
-      { "transform": "@boostercloud/metadata-booster" }
-    ]
+    "plugins": [{ "transform": "@boostercloud/metadata-booster" }]
   },
-  "include": [
-    "src/**/*"
-  ]
-}`
+  "include": ["src/**/*"]
+}
+`
