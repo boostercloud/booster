df632ced599e0ef7acc89a67cadb2351fdab0126
Nick Tchayka <nick@booster.cloud>
2022-08-22 16:33:43 +0100

    Attempt to fix boost path

--- packages/framework-integration-tests/integration/helper/cli-helper.ts
+++ packages/framework-integration-tests/integration/helper/cli-helper.ts
@@ -3,7 +3,7 @@ import { ChildProcess } from 'child_process'
 import { runCommand } from '@boostercloud/framework-common-helpers'
 
 // Path to the CLI binary compiled for this project
-const cliBinaryPath = path.join('..', '..', 'cli', 'bin', 'run')
+const cliBinaryPath = path.join(__dirname, '..', '..', 'node_modules', '.bin', 'boost')
 
 export async function deploy(projectPath: string, environmentName = 'production'): Promise<void> {
   // Production dependencies are installed by the cliHelper command
