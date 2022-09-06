b4adfb87de2e6fa1dcd270489ecfdf75e0a03c07
rodsoaresTD <rodrigo.soares@talkdesk.com>
2022-09-06 23:07:57 +0100

    Test a LOT of cron expressions with fast-check property testing

--- packages/framework-provider-local-infrastructure/test/scheduler.test.ts
+++ packages/framework-provider-local-infrastructure/test/scheduler.test.ts
@@ -4,12 +4,13 @@ import { fake, replace, restore } from 'sinon'
 import { configureScheduler } from '../src/scheduler'
 import { expect } from '../test/expect'
 import * as scheduler from 'node-schedule'
+import * as fc from 'fast-check'
 
 describe('scheduler', () => {
   let mockConfig: any
 
   const userProject = {
-    boosterTriggerScheduledCommand: () => {}
+    boosterTriggerScheduledCommand: () => {},
   }
 
   describe('configureScheduler', () => {
@@ -42,8 +43,7 @@ describe('scheduler', () => {
               hour: '4',
               day: '3',
               month: '*',
-              weekDay: '*',
-              year: '*',
+              weekDay: '*'
             },
           },
           cmdWithoutScheduledOn: {
@@ -60,7 +60,7 @@ describe('scheduler', () => {
           cmdWithNullScheduledOn: {
             class: fake(),
             scheduledOn: null,
-          }
+          },
         },
       }
 
@@ -68,8 +68,9 @@ describe('scheduler', () => {
 
       expect(scheduler.scheduleJob).to.have.been.calledWithMatch('cmd1', '5 4 * * *')
       expect(scheduler.scheduleJob).to.have.been.calledWithMatch('cmd2', '* 4 3 * *')
-      
-      expect(scheduler.scheduleJob).to.not.have.been.calledWithMatch('cmdWithEmptyScheduledOn')
+      expect(scheduler.scheduleJob).to.have.been.calledWithMatch('cmdWithEmptyScheduledOn', {})
+
+      // expect(scheduler.scheduleJob).to.not.have.been.calledWithMatch('cmdWithEmptyScheduledOn')
       expect(scheduler.scheduleJob).to.not.have.been.calledWithMatch('cmdWithoutScheduledOn')
       expect(scheduler.scheduleJob).to.not.have.been.calledWithMatch('cmdWithUndefinedScheduledOn')
       expect(scheduler.scheduleJob).to.not.have.been.calledWithMatch('cmdWithNullScheduledOn')
@@ -83,6 +84,51 @@ describe('scheduler', () => {
       configureScheduler(mockConfig as unknown as BoosterConfig, userProject)
 
       expect(scheduler.scheduleJob).to.not.have.been.called
+    })
+
+
+    it('should accept all kinds of cron expressions', () => {
+      const minuteRegex = /(\*|(?:\*|(?:[0-9]|(?:[1-5][0-9])))\/(?:[0-9]|(?:[1-5][0-9]))|(?:[0-9]|(?:[1-5][0-9]))(?:(?:\-[0-9]|\-(?:[1-5][0-9]))?|(?:\,(?:[0-9]|(?:[1-5][0-9])))*))/
+      const hourRegex = /(\*|(?:[0-9]|1[0-9]|2[0-3])(?:(?:\-(?:[0-9]|1[0-9]|2[0-3]))?|(?:\,(?:[0-9]|1[0-9]|2[0-3]))*))/
+      const dayRegex = /(\*|(?:[1-9]|(?:[12][0-9])|3[01])(?:(?:\-(?:[1-9]|(?:[12][0-9])|3[01]))?|(?:\,(?:[1-9]|(?:[12][0-9])|3[01]))*))/
+      const monthRegex = /(\*|(?:[1-9]|1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)(?:(?:\-(?:[1-9]|1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC))?|(?:\,(?:[1-9]|1[012]|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC))*))/
+      const weekdayRegex = /(\*|(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT)(?:(?:\-(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT))?|(?:\,(?:[0-6]|SUN|MON|TUE|WED|THU|FRI|SAT))*))/
+
+      const scheduledOnArb = fc.record(
+        {
+          minute: fc.string({maxLength: 9}).filter((randomString) => minuteRegex.test(randomString)),
+          hour: fc.string({maxLength: 9}).filter((randomString) => hourRegex.test(randomString)),
+          day: fc.string({maxLength: 9}).filter((randomString) => dayRegex.test(randomString)),
+          month: fc.string({maxLength: 9}).filter((randomString) => monthRegex.test(randomString)),
+          weekDay: fc.string({maxLength: 9}).filter((randomString) => weekdayRegex.test(randomString))
+        }
+      )
+
+      fc.assert(
+        fc.property(
+          scheduledOnArb, (scheduledOn) => {
+            mockConfig = {
+              scheduledCommandHandlers: {
+                cmd: {
+                  class: fake(),
+                  scheduledOn: {
+                    minute: scheduledOn.minute,
+                    hour: scheduledOn.hour,
+                    day: scheduledOn.day,
+                    month: scheduledOn.month,
+                    weekDay: scheduledOn.weekDay
+                  },
+                }
+              },
+            }
+            configureScheduler(mockConfig as unknown as BoosterConfig, userProject)
+            expect(scheduler.scheduleJob).to.have.been.calledWithMatch(
+              'cmd',
+              `${scheduledOn.minute} ${scheduledOn.hour} ${scheduledOn.day} ${scheduledOn.month} ${scheduledOn.weekDay}`
+            )
+          }
+        )
+      )
     })
   })
 })
