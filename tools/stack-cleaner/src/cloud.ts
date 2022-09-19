cbaafdebf0d2778cfaca9f5b29b2aa1de143ee20
Nick Tchayka <nick@booster.cloud>
2022-09-19 17:45:41 +0100

    Start stack-cleaner

---
+++ tools/stack-cleaner/src/cloud.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { buildModule } from './common'
+
+// Define operations for the module SDK
+export interface CloudSDK {
+  getDeployedStacks: () => Promise<Array<string>>
+  deleteStack: (stackName: string) => Promise<void>
+  listStorages: () => Promise<Array<string>>
+  listObjectsInStorage: (storageName: string) => Promise<Array<string>>
+  deleteObject: (storageName: string, objectName: string) => Promise<void>
+  deleteStorage: (storageName: string) => Promise<void>
+}
+
+// Error definition and handling
+export type CloudError = { _tag: 'StackOperationError'; error: Error } | { _tag: 'StorageError'; error: Error }
+
+const toStackOperationError = (error: unknown): CloudError => ({
+  _tag: 'StackOperationError',
+  error: error instanceof Error ? error : new Error(String(error)),
+})
+
+const toStorageOperationError = (error: unknown): CloudError => ({
+  _tag: 'StorageError',
+  error: error instanceof Error ? error : new Error(String(error)),
+})
+
+// Define the module, here we combine them
+const StackModule = buildModule<CloudSDK, CloudError>(['getDeployedStacks', 'deleteStack'], toStackOperationError)
+
+const StorageModule = buildModule<CloudSDK, CloudError>(
+  ['listStorages', 'listObjectsInStorage', 'deleteObject', 'deleteStorage'],
+  toStorageOperationError
+)
+
+export const CloudModule = {
+  ...StackModule,
+  ...StorageModule,
+}
---
+++ tools/stack-cleaner/src/main.ts
@@ -0,0 +1,103 @@
+import * as Arr from 'fp-ts/ReadonlyArray'
+import * as Str from 'fp-ts/string'
+import * as Either from 'fp-ts/Either'
+import { flow, pipe } from 'fp-ts/function'
+import * as RTE from 'fp-ts/ReaderTaskEither'
+import { CloudError, CloudModule as Cloud } from './cloud'
+import { AwsImplementation } from './aws'
+import { CloudFormationClient } from '@aws-sdk/client-cloudformation'
+import { S3Client } from '@aws-sdk/client-s3'
+
+const filterStoreNames = Arr.filter(Str.startsWith('my-store-'))
+const filterToolkitNames = Arr.filter(Str.includes('toolkit'))
+const filterNames = flow(filterStoreNames, filterToolkitNames)
+const partitionByType = Arr.partition(Str.includes('toolkit'))
+
+const trace =
+  <A>(message: string) =>
+  (a: A): RTE.ReaderTaskEither<unknown, never, A> =>
+    RTE.fromIO(() => {
+      console.log(message, a)
+      return a
+    })
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const listObjectsAlongStorage = (storageName: string) =>
+  pipe(
+    Cloud.listObjectsInStorage(storageName),
+    RTE.chainW(trace('Got objects ' + storageName)),
+    RTE.map(Arr.map((objectName: string) => ({ storageName, objectName })))
+  )
+
+const inParallel = Arr.traverse(RTE.ApplicativeSeq)
+
+const cleanBuckets = pipe(
+  RTE.Do,
+  RTE.bind('buckets', () => pipe(Cloud.listStorages(), RTE.map(filterNames))),
+  RTE.chainFirstW(({ buckets }) => trace('Found buckets')(buckets)),
+  RTE.bindW('objects', ({ buckets }) =>
+    pipe(buckets, RTE.traverseArray(listObjectsAlongStorage), RTE.map(Arr.flatten))
+  ),
+  RTE.chainFirstW(({ objects }) => trace('Found objects')(objects)),
+  RTE.bindW('deletedObjects', ({ objects }) =>
+    pipe(
+      objects,
+      RTE.traverseSeqArray(({ storageName, objectName }) => Cloud.deleteObject(storageName, objectName))
+    )
+  ),
+  RTE.bindW('deletedBuckets', ({ buckets }) => pipe(buckets, RTE.traverseSeqArray(Cloud.deleteStorage))),
+  RTE.chain(() => RTE.right(undefined))
+  // RTE.bind('objectDeleteTasks', ({ objects }) =>
+  //   pipe(
+  //     objects,
+  //     Arr.flatten,
+  //     Arr.reduce(
+  //       RTE.fromIO(() => {}),
+  //       (current, { storageName, objectName }) =>
+  //         pipe(
+  //           current,
+  //           RTE.chain(() => Cloud.deleteObject(storageName, objectName))
+  //         )
+  //     )
+  //   )
+  // ),
+  // RTE.bind('bucketDeleteTasks', ({ buckets }) => inParallel(Cloud.deleteStorage)(buckets))
+)
+
+const parallelDeleteStacks = inParallel(Cloud.deleteStack)
+
+const cleanStacks = pipe(
+  RTE.Do,
+  RTE.bindW('stacks', Cloud.getDeployedStacks),
+  RTE.bindW('partitionedStacks', ({ stacks }) => RTE.right(partitionByType(filterNames(stacks)))),
+  RTE.chainFirstW(({ partitionedStacks }) => parallelDeleteStacks(partitionedStacks.left)),
+  RTE.chainW(({ partitionedStacks }) => parallelDeleteStacks(partitionedStacks.right))
+)
+
+const cleanAll = pipe(
+  cleanBuckets,
+  RTE.chain(() => cleanStacks)
+)
+
+const handleError = (cloudError: CloudError): void => {
+  switch (cloudError._tag) {
+    case 'StackOperationError':
+      console.error('Stack operation error', cloudError.error)
+      break
+    case 'StorageError':
+      console.error('Storage operation error', cloudError.error)
+      break
+  }
+}
+
+const handleSuccess = (): void => {
+  console.log('Cleaned up successfully')
+}
+
+export const main = async (): Promise<void> => {
+  const region = 'us-east-1'
+  const sdk = AwsImplementation(new CloudFormationClient({ region }), new S3Client({ region }))
+  pipe(cleanAll)
+  const result = await cleanAll(sdk)()
+  return pipe(result, Either.fold(handleError, handleSuccess))
+}
