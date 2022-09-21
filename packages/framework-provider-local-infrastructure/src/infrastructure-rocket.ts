b7ae658eb99a362a2bdc052000700cfd7158c1fe
gonzalojaubert <gonzalo_marcos@optum.com>
2022-09-21 14:38:05 +0100

    add port to mountStack for local rockets

--- packages/framework-provider-local-infrastructure/src/infrastructure-rocket.ts
+++ packages/framework-provider-local-infrastructure/src/infrastructure-rocket.ts
@@ -2,6 +2,6 @@ import { BoosterConfig } from '@boostercloud/framework-types'
 import { Router } from 'express'
 
 export interface InfrastructureRocket {
-  mountStack: (config: BoosterConfig, router: Router) => void
+  mountStack: (config: BoosterConfig, router: Router, port: number) => void
   unmountStack?: () => void
 }
