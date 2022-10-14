eca26b225860bbfb04cb7dfa91cefdfadf05f83d
gonzalojaubert <gonzalo_marcos@optum.com>
2022-10-14 11:35:34 +0100

    Add InfrastructureRocketMetadata

--- packages/framework-provider-local-infrastructure/src/infrastructure-rocket.ts
+++ packages/framework-provider-local-infrastructure/src/infrastructure-rocket.ts
@@ -1,7 +1,15 @@
 import { BoosterConfig } from '@boostercloud/framework-types'
 import { Router } from 'express'
 
+export interface InfrastructureRocketMetadata {
+  port: number
+}
+
 export interface InfrastructureRocket {
-  mountStack: (config: BoosterConfig, router: Router, port: number) => void
+  mountStack: (
+    config: BoosterConfig,
+    router: Router,
+    infrastructureRocketMetadata?: InfrastructureRocketMetadata
+  ) => void
   unmountStack?: () => void
 }
