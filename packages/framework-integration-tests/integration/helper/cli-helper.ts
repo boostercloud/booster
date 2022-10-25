c40110ef9c6b50325f559e26a5bc68d724d8b323
Nick Tchayka <nick@booster.cloud>
2022-10-25 12:07:27 +0100

    Make integration commands verbose

--- packages/framework-integration-tests/integration/helper/cli-helper.ts
+++ packages/framework-integration-tests/integration/helper/cli-helper.ts
@@ -7,15 +7,15 @@ const cliBinaryPath = path.join(__dirname, '..', '..', 'node_modules', '.bin', '
 
 export async function deploy(projectPath: string, environmentName = 'production'): Promise<void> {
   // Production dependencies are installed by the cliHelper command
-  await runCommand(projectPath, `${cliBinaryPath} deploy -e ${environmentName}`)
+  await runCommand(projectPath, `${cliBinaryPath} deploy --verbose -e ${environmentName}`)
 }
 
 export async function nuke(projectPath: string, environmentName = 'production'): Promise<void> {
   // Dependencies should be installed before running the nuke command
   await runCommand(projectPath, 'npm install --production --no-bin-links --no-optional')
-  await runCommand(projectPath, `${cliBinaryPath} nuke -e ${environmentName} --force`)
+  await runCommand(projectPath, `${cliBinaryPath} nuke --verbose -e ${environmentName} --force`)
 }
 
 export function start(path: string, environmentName = 'local'): ChildProcess {
-  return runCommand(path, `${cliBinaryPath} start -e ${environmentName}`).childProcess
+  return runCommand(path, `${cliBinaryPath} start --verbose -e ${environmentName}`).childProcess
 }
