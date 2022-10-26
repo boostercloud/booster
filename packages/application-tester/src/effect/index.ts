da245dd1b81962c757c9c97bbd648e8dab2d589f
Nick Tchayka <nick@booster.cloud>
2022-10-26 14:29:47 +0100

    Update docs

--- packages/application-tester/src/effect/index.ts
+++ packages/application-tester/src/effect/index.ts
@@ -25,22 +25,80 @@ import { SinonSpy } from 'sinon'
  * tests, the fakes record that you passed, and a `reset` function that will reset the history
  * of all the fakes, so you can use them in multiple tests.
  *
+ * A useful pattern is to create a `makeTestService` function that will create a new instance of
+ * the service for each test, passing the overrides you need for that test. This way, you can
+ * use the same `makeTestService` function in multiple tests, and you don't need to be passing all
+ * the fakes in every test.
+ *
+ * NOTE: This has a limitation where the service cannot define constants or values that are effects,
+ * as a workaround, you can define them as functions that return the value.
+ *
+ * E.g.:
+ * `() => Effect<...>`  instead of `Effect<...>`
+ *
+ * and
+ *
+ * `() => Effect<..., T>` instead of `T`
+ *
+ * @example
+ * Here's the typical usage of this function (you can also check the tests in the @boostercloud/cli package):
+ * ```typescript
+ * import { fakeService, FakeOverrides } from '@boostercloud/application-tester/src/effect'
+ * import { fake } from 'sinon'
+ * import { MyService } from '../../../src/services/my-service'
+ *
+ * const makeTestMyService = (overrides?: FakeOverrides<MyService>) =>
+ *   fakeService(MyService, {
+ *     voidMethodInService: fake(),
+ *     methodThatReturnsAValue: fake.returns(''),
+ *     ...overrides,
+ *   })
+ *
+ * // In your test:
+ * describe('my test', () => {
+ *   it('should do something', async () => {
+ *     const { layer, fakes, reset } = makeTestMyService()
+ *
+ *     await unsafeRunEffect(functionThatUsesMyService(), {
+ *       layer,
+ *       onError: orDie
+ *     })
+ *
+ *     expect(fakes.methodThatReturnsAValue).to.have.been.calledWith('some value')
+ *     reset()  // You can also reset the fakes in an `afterEach` hook
+ *   })
+ * })
+ * ```
+ *
  * @param tag - The tag of the service to fake. E.g. `ProcessService`.
  * @param fakes - A record of the methods to fake. E.g. `{ cwd: fake.returns(''), exec: fake.returns('') }`.
  * @return {FakeServiceUtils} - An object with the layer to use in the dependency graph, and the fakes to assert the service was called with the right parameters.
  */
 export const fakeService = <T extends ShapeFn<T>>(tag: Tag<T>, fakes: Fakes<T>): FakeServiceUtils<T> => {
-  // Assemble the fakes into a service that returns Effects in its functions
+  // Assemble the fakes into a service that returns Effects in its functions.
+  // We disable the `any` warning because at this point the record is empty and TS will complain
+  // while we assemble it. The alternative was to use a `reduce` method, but the code became much more
+  // contrived, while not getting too much more type safety, as all the objects were of type unknown and we
+  // had to cast them explicitly
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const fakeService = {} as any
+
+  // Object.entries doesn't type properly the keys an values, it returns always string and unknown (yay!)
   for (const [k, v] of Object.entries(fakes)) {
+    // We cast the value to a SinonSpy, as we know that's what we're passing in the `fakes` record.
+    // We don't really need to type the arguments or the return type, as that is typed by the return type
+    // of this function.
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const fakeFunction = v as SinonSpy<any[], any>
+
+    // We wrap the fake function in an Effect, so the layer can be used by the functions that require this
+    // service. We don't need to type the arguments or the return type, as that is typed by the return type.
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     fakeService[k] = (...args: any[]) => succeedWith(() => fakeFunction(...args))
   }
 
-  // Create a layer with that service as the only dependency
+  // Create a layer with that service as the only dependency, we can combine it with other layers using
+  // `Layer.all` in the tests.
   const layer = Layer.fromValue(tag)(fakeService)
 
   // Create a reset function to reset all the fakes
