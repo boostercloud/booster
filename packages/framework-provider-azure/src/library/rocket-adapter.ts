06248493fdcd27b27210f7126ac736a4425c16ad
gonzalojaubert <gonzalo_marcos@optum.com>
2022-11-11 09:07:59 +0000

    Use env-vars on local if request header is not found

--- packages/framework-provider-azure/src/library/rocket-adapter.ts
+++ packages/framework-provider-azure/src/library/rocket-adapter.ts
@@ -1,8 +1,7 @@
-import { BoosterConfig, RocketEnvelope, rocketFunctionIDEnvVar } from '@boostercloud/framework-types'
+import { BoosterConfig, RocketEnvelope, rocketFunctionIDEnvVar } from "@boostercloud/framework-types";
 
 export function rawRocketInputToEnvelope(config: BoosterConfig, request: unknown): RocketEnvelope {
-  const id = process.env[rocketFunctionIDEnvVar]
   return {
-    rocketId: id,
+    rocketId: process.env[rocketFunctionIDEnvVar],
   }
 }
--- packages/framework-provider-local/src/library/rocket-adapter.ts
+++ packages/framework-provider-local/src/library/rocket-adapter.ts
@@ -1,7 +1,8 @@
 import { BoosterConfig, RocketEnvelope, rocketFunctionIDEnvVar } from '@boostercloud/framework-types'
 
 export function rawRocketInputToEnvelope(config: BoosterConfig, request: unknown): RocketEnvelope {
-  const id = (request as any)[rocketFunctionIDEnvVar]
+  const idFromRequest = (request as any)[rocketFunctionIDEnvVar]
+  const id = idFromRequest ?? process.env[rocketFunctionIDEnvVar]
   return {
     rocketId: id,
   }
