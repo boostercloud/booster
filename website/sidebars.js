47d05f58ed62b90f403e20e6c098bf17c7f947ec
Borja Zarco <borjazarco@theagilemonkeys.com>
2022-12-02 13:37:30 +0000

    Adopt a standard documentation tool (Docusaurus) (#1253)
    * init docusaurus
    
    * cleanup docusaurus
    
    * add tailwind
    
    * add current docs
    
    * fix svgs
    
    * adapt footer
    
    * set sidebar manually
    
    * change favicon
    
    * split getting started section
    
    * finish docs migration
    
    * font and style changes
    
    * final fixes
    
    * rush update
    
    * fix changes check workflow

---
+++ website/sidebars.js
@@ -0,0 +1,42 @@
+// @ts-check
+
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  docs: [
+    'introduction',
+    {
+      type: 'category',
+      label: 'Getting Started',
+      link: {
+        type: 'generated-index',
+      },
+      items: ['getting-started/installation', 'getting-started/configuration', 'getting-started/coding'],
+    },
+    {
+      type: 'category',
+      label: 'Booster architecture',
+      link: {
+        type: 'doc',
+        id: 'architecture/event-driven',
+      },
+      items: [
+        'architecture/command',
+        'architecture/event',
+        'architecture/event-handler',
+        'architecture/entity',
+        'architecture/read-model',
+      ],
+    },
+    'authentication',
+    'logging',
+    'graphql',
+    'error-handling',
+    'data-migrations',
+    'testing',
+    'going-deeper',
+    'frequently-asked-questions',
+    'contributing',
+  ],
+}
+
+module.exports = sidebars
