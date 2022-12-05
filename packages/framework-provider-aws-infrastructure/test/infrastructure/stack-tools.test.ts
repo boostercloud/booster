1d526088e2fb45f7b31caeca2fd145d7f99e1f84
nicolatl <lawford.nicola@gmail.com>
2022-12-05 15:08:43 -0500

    fix timeout and iOS-style path assertion blocking test 13

--- packages/framework-provider-aws-infrastructure/test/infrastructure/stack-tools.test.ts
+++ packages/framework-provider-aws-infrastructure/test/infrastructure/stack-tools.test.ts
@@ -161,7 +161,7 @@ describe('the `stack-tools` module', () => {
         const fun: any = Object.values(stackResources).find((obj: any) => {
           return obj.Properties.FunctionName == 'testing-app-app-graphql-handler'
         })
-        expect(fun.Properties.Handler).to.equal('dist/index.boosterServeGraphQL')
+        expect(fun.Properties.Handler).to.be.oneOf('dist/index.boosterServeGraphQL', 'dist\\index.boosterServeGraphQL')
       })
     })
 
