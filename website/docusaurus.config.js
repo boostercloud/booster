dc8d87f4269f32d2d3103892ae25e5f1113c815b
BorjaZarco <borjazarco@theagilemonkeys.com>
2022-12-13 15:05:25 +0000

    show last update time and author

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -31,6 +31,8 @@ const config = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
+          showLastUpdateTime: true,
+          showLastUpdateAuthor: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/boostercloud/booster/tree/main/website/',
