342b9ccbecc9d2789636cf730234df2fcb4d883a
Nick Tchayka <nick@booster.cloud>
2023-02-17 17:45:42 +0000

    Fix tests

--- packages/framework-integration-tests/src/commands/flush-notifications.ts
+++ packages/framework-integration-tests/src/commands/flush-notifications.ts
@@ -10,9 +10,15 @@ export class FlushNotifications {
   public constructor(readonly cartId: UUID, readonly previousProducts: number, readonly afterProducts: number) {}
 
   public static async handle(command: FlushNotifications, register: Register): Promise<Array<Cart>> {
-    const previousEvents = Array(command.previousProducts).fill(new CartAbandoned(command.cartId.toString()))
+    const previousEvents = []
+    for (let i = 0; i < command.previousProducts; i++) {
+      previousEvents.push(new CartAbandoned(command.cartId.toString()))
+    }
 
-    const afterEvents = Array(command.afterProducts).fill(new CartAbandoned(command.cartId.toString()))
+    const afterEvents = []
+    for (let i = 0; i < command.afterProducts; i++) {
+      afterEvents.push(new CartAbandoned(command.cartId.toString()))
+    }
 
     console.log('REGISTERING PREVIOUS EVENTS', previousEvents)
     register.events(...previousEvents)
@@ -29,7 +35,8 @@ export class FlushNotifications {
     return [previousCart, afterCart]
   }
 
-  private static async getCart(cartId: UUID, maxRetries = 10): Promise<Cart> {
+  private static async getCart(cartId: UUID): Promise<Cart> {
+    let maxRetries = 10
     let cart: Cart | undefined = undefined
     do {
       cart = await new Promise((resolve) =>
