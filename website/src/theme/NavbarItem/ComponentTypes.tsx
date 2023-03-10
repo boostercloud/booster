0117c3bfd042012afb39da2b7f16fa1e2b1ee6ed
Nick Tchayka <nick@booster.cloud>
2023-03-10 12:27:07 +0000

    Reset changes and fix import

--- website/src/theme/NavbarItem/ComponentTypes.tsx
+++ website/src/theme/NavbarItem/ComponentTypes.tsx
@@ -1,7 +1,7 @@
-import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
-import CustomNavbarItem from '../../components/NavbarItems/CustomNavbarItem';
+import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes'
+const CustomNavbarItem = require(`${__dirname}/../../components/NavbarItems/CustomNavbarItem`).default
 
 export default {
   ...ComponentTypes,
   'custom-navbar-item': CustomNavbarItem,
-};
+}
