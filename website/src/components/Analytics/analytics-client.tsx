de752ea7e432c9d0b68b4c07afb1d7df74d1fe65
Juan Sagasti <juan@theagilemonkeys.com>
2023-03-14 18:39:40 +0000

    Revert "Test change for VPN compatibility"
    This reverts commit f35b9ad98e05f83124f0ccee12f4a594027f27b3.

--- website/src/components/Analytics/analytics-client.tsx
+++ website/src/components/Analytics/analytics-client.tsx
@@ -2,7 +2,7 @@ import * as Fathom from 'fathom-client'
 
 export class AnalyticsClient {
   static start() {
-    Fathom.load('LHRTIPFZ', { url: 'https://tl1.docs.boosterframework.com/script.js' })
+    Fathom.load('LHRTIPFZ', { url: 'https://tl1.boosterframework.com/script.js' })
   }
 
   static trackEvent(event: string) {
