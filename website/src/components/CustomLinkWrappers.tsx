f2132780a7451b0ce268e6b3c49cad84ebfa4c2a
Damien Vieira <damien@theagilemonkeys.com>
2023-03-21 17:04:17 +0200

    Test link

---
+++ website/src/components/CustomLinkWrappers.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { AnalyticsClient } from './Analytics/analytics-client'
+import CustomLink from './CustomLink'
+
+type CLStepByStepProps = {
+    children: React.ReactNode
+}
+  
+export const CLStepByStep: React.FC = ({ children }: CLStepByStepProps) => {
+  const href = 'https://github.com/boostercloud/booster/tree/main/examples'
+  const onClick = () => AnalyticsClient.trackEvent('GYHPPIBS')
+
+  return (<CustomLink href={href} onClick={onClick}>{children}</CustomLink>)
+}
+
+export const CLExampleApps: React.FC = ({ children }: CLStepByStepProps) => {
+  const href = 'https://github.com/boostercloud/examples'
+  const onClick = () => AnalyticsClient.trackEvent('YY7T3ZSZ')
+
+  return (<CustomLink href={href} onClick={onClick}>{children}</CustomLink>)
+}
