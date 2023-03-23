1e24b9793248f648c7da25593e8497391d060ed9
Damien Vieira <damien@theagilemonkeys.com>
2023-03-23 17:27:12 +0200

    Updated bash script

--- .github/actions/update-website/action.sh
+++ .github/actions/update-website/action.sh
@@ -10,7 +10,7 @@
 # ===================================================================================================================================================================================== #
 
 GRAPHQL_URL="$AI_BOOSTER_URL"
-REPO_URL="https://raw.githubusercontent.com/boostercloud/booster/main/"
+REPO_URL="https://raw.githubusercontent.com/boostercloud/booster/main/website/"
 
 # Auth0 credentials from environment variables
 CLIENT_ID="$AUTH0_CLIENT_ID"
@@ -18,8 +18,8 @@ CLIENT_SECRET="$AUTH0_CLIENT_SECRET"
 AUDIENCE="$AUTH0_PRIVATEGPT_AUDIENCE"
 AUTH0_DOMAIN="$AUTH0_DOMAIN"
 
-# Go up three directories to get to the parent's parent's parent
-cd ../../..
+# Go to the website folder
+cd ../../../website/
 
 # Get Auth0 token
 auth0_response=$(curl -s -X POST -H "Content-Type: application/json" \
@@ -27,6 +27,8 @@ auth0_response=$(curl -s -X POST -H "Content-Type: application/json" \
   $AUTH0_DOMAIN)
 
 access_token=$(echo $auth0_response | jq -r '.access_token')
+echo "access token: ${access_token}"
+
 
 # Define GraphQL query
 fetch_pages_query='{ListPageReadModels(filter: {}, limit: 1000, sortBy: {}) {items {id path title checksum} count cursor}}'
@@ -53,7 +55,6 @@ checksums=()
 
 
 echo "Remote Files ========================="
-
 # Loop over each item in the 'items' array
 for item in $(echo "${items}" | jq -r '.[] | @base64'); do
   # Decode the base64-encoded JSON for the current item
@@ -67,9 +68,9 @@ for item in $(echo "${items}" | jq -r '.[] | @base64'); do
   title=$(_jq '.title')
   checksum=$(_jq '.checksum')
 
-  echo "$path/$title:$checksum"
+  echo "$path:$checksum"
   remote_file_ids+=("$id")
-  remote_file_paths+=("$path/$title")
+  remote_file_paths+=("$path")
   remote_file_checksums+=("$checksum")
 done
 echo "Remote Files Count: ${#remote_file_ids[@]} ========================="
@@ -80,9 +81,9 @@ excluded_files=("00_ai-assistant.md" "README.md")
 # Define array of excluded directories
 excluded_dirs=("node_modules" "build" ".docusaurus" "static" "src")
 
-# Iterate through each .md and .mdx file in the website directory and its subdirectories
+# Iterate through each .md and .mdx file in the current directory and its subdirectories
 # Build the find command with excluded directories
-find_cmd="find website -type f \( -name \"*.md\" -o -name \"*.mdx\" \)"
+find_cmd="find . -type f \( -name \"*.md\" -o -name \"*.mdx\" \)"
 
 for dir in "${excluded_dirs[@]}"; do
   find_cmd+=" -not -path \"*/$dir/*\""
@@ -93,13 +94,13 @@ for file in "${excluded_files[@]}"; do
 done
 
 echo "Local Files ========================="
-
 # Use process substitution to avoid creating a subshell
 while read filepath; do
   checksum=$(openssl sha256 < "$filepath" | cut -d ' ' -f 2)
+  path="${filepath#./}"
 
-  echo "$REPO_URL$filepath:$checksum"
-  paths+=("$REPO_URL$filepath")
+  echo "$REPO_URL$path:$checksum"
+  paths+=("$REPO_URL$path")
   checksums+=("$checksum")
 done < <(eval "$find_cmd")
 echo "Local Files Count: ${#paths[@]} ========================="
@@ -109,7 +110,7 @@ removed_pages=()
 
 echo "Find missing files in local files ==============="
 # Find missing files in local files
-for (( i=0; i<${#remote_file_paths[@]}; i++ )); do
+for (( i=0; i<${#remote_file_ids[@]}; i++ )); do
   file="${remote_file_paths[$i]}"
   checksum="${remote_file_checksums[$i]}"
   
@@ -135,7 +136,6 @@ for (( i=0; i<${#paths[@]}; i++ )); do
   file="${paths[$i]}"
   checksum="${checksums[$i]}"
   
-
   # Check if the file exists in remote_file and has the same checksum
   j=-1
   for (( k=0; k<${#remote_file_checksums[@]}; k++ )); do
@@ -146,33 +146,52 @@ for (( i=0; i<${#paths[@]}; i++ )); do
   done
 
 
-  if (( j == -1 )); then
-    # The file does not exist in remote_file_checksums, so create a new page
-    stored_pages+=("${file}:${checksum}")
-
-    echo "Storing page with checksum: ${checksum}"
-    path=$(dirname "$file")
-    filename=$(basename "$file")
-
-    curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ${access_token}"\
-    -d "{\"query\": \"mutation { StorePage(input: { path: \\\"$path\\\", title: \\\"$filename\\\", checksum: \\\"$checksum\\\" }) }\"}" \
-    $GRAPHQL_URL
-
-  else
-    remote_file="${remote_file_paths[$j]}"
+  path=$(dirname "$file")
+  filename=$(basename "$file")    
     
-    if [[ "${file}" == "${remote_file}" ]]; then
-      # The file has the same checksum and path, so nothing to do here
-    :
+  # Remove the prefix until "website/" or "docs/"
+  last_folder=$(echo "$path" | sed -E 's/.*(website|docs)//')
+
+  # Check if the last folder is "docs" or "website"
+  if [ "$last_folder" == "" ]; then
+    path=$filename
+  else
+    path="${last_folder:1}/$filename"
+  fi
+
+  # Apply the first regex: remove prefixes and suffixes
+  result=$(echo "$path" | sed -E 's/^[\d_]+//;s/_([0-9]+\/)/\1/g')
+
+  # Apply the second regex: remove digits and underscores
+  result=$(echo "$result" | sed -E 's/[0-9]+_//g')
+
+  # Remove extension from file
+  path="${result%.*}"
+
+
+  if [ "$j" -ne -1 ] && [ "$path" == "${remote_file_paths[$j]}" ]; then
+    # The file has the same checksum and path so nothing to do here
+  :
+  else
+    title=$(basename "$path")
+
+    if (( j == -1 )); then
+      # The file does not exist in remote_file_checksums, so create a new page
+      stored_pages+=("${file}:${checksum}")
+
+      echo "Storing page with checksum: ${checksum}"
+      
+      curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ${access_token}"\
+      -d "{\"query\": \"mutation { StorePage(input: { url: \\\"$file\\\", path: \\\"$path\\\", title: \\\"$title\\\", checksum: \\\"$checksum\\\" }) }\"}" \
+      $GRAPHQL_URL
+
     else
       page_id="${remote_file_ids[$j]}"
-      path=$(dirname "$file")
-      filename=$(basename "$file")
 
       echo "Updating page with id: ${page_id} to path: ${path} name: ${filename} checksum: ${checksum}"
-
+      
       curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ${access_token}"\
-      -d "{\"query\": \"mutation { UpdatePage(input: { pageID: \\\"$page_id\\\", path: \\\"$path\\\", title: \\\"$filename\\\" }) }\"}" \
+      -d "{\"query\": \"mutation { UpdatePage(input: { pageID: \\\"$page_id\\\", path: \\\"$path\\\", title: \\\"$title\\\" }) }\"}" \
       $GRAPHQL_URL
     fi
   fi
