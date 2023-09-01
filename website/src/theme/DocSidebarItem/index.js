16e1faad3096e287cfeadae30da361a7a22a40dc
turnerran <turnerran87@gmail.com>
2023-09-01 21:40:18 +0200

    Removed some redundant code

--- website/src/theme/DocSidebarItem/index.js
+++ website/src/theme/DocSidebarItem/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import DocSidebarItem from '@theme-original/DocSidebarItem';
+import React from 'react'
+import DocSidebarItem from '@theme-original/DocSidebarItem'
 
 export default function DocSidebarItemWrapper(props) {
   const handleClick = (customProps) => {
-    if (customProps && customProps.trackingEvent) {
-      fathom.trackGoal(customProps.trackingEvent, 0);
+    if (customProps?.trackingEvent) {
+      fathom.trackGoal(customProps.trackingEvent, 0)
     }
-  };
+  }
 
   return (
     <div onClick={() => handleClick(props.item.customProps)}>
       <DocSidebarItem {...props} />
     </div>
-  );
+  )
 }
-
