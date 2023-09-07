1a7d8a89c2cc72fc1c694665a209211b718d5375
Javier Toledo <javier@theagilemonkeys.com>
2023-09-07 18:37:32 +0100

    Added arguments and options parsing with commander.js

--- packages/create/src/create.ts
+++ packages/create/src/create.ts
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
+import { OptionValues, program } from 'commander';
+const packageJSON = require('../package.json');
 
-console.log("hello world")
+// Parse the input using Commander.js
+//
+// Typical usage: npm create @boostercloud <app-name> --template githubuser/repo
+program
+  .version(packageJSON.version)
+  .argument('<project-name>')
+  .option('-t, --template <template>', 'Template to use for the project', 'boostercloud/base-template')
+  .description('Creates a new Booster project using the given name. If no template is provided it will use the default (boostercloud/base-template)')
+  .action((projectName: string, options: OptionValues) => {
+    console.log(`Creating a new Booster project ${projectName} using the template ${options.template}`)
+  })
+  .showHelpAfterError()
+  .parse()
