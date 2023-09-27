c79319915becbe5c0754dbb3f30220f2e1530cfd
Javier Toledo <javier@theagilemonkeys.com>
2023-09-27 16:20:25 +0100

    Consistent versions of rimraf dependency across packages

--- packages/cli/src/templates/project/package-json.ts
+++ packages/cli/src/templates/project/package-json.ts
@@ -18,7 +18,7 @@ export const template = `{
     "@boostercloud/framework-provider-local-infrastructure": "^${VERSION}",
     "@boostercloud/metadata-booster": "^${VERSION}",
     "{{{providerPackageName}}}-infrastructure": "^${VERSION}",
-    "rimraf": "^3.0.1",
+    "rimraf": "^5.0.0",
     "@typescript-eslint/eslint-plugin": "4.22.1",
     "@typescript-eslint/parser": "4.22.1",
     "eslint": "7.26.0",
@@ -47,7 +47,6 @@ export const template = `{
     "lint:fix": "eslint --quiet --fix --ext '.js,.ts' **/*.ts",
     "build": "ttsc -b tsconfig.json",
     "clean": "rimraf ./dist tsconfig.tsbuildinfo",
-    "test": "AWS_SDK_LOAD_CONFIG=true BOOSTER_ENV=test nyc --extension .ts mocha --forbid-only \\"test/**/*.test.ts\\""
   },
   "types": "lib/index.d.ts"
 }`
