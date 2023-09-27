2bf45dacd01f689c911b728575cfc1d8100a817d
gonzalojaubert <gonzalo_marcos@optum.com>
2023-09-27 09:17:33 +0100

    Add elapsed time to data migration entity

--- packages/framework-integration-tests/src/read-models/data-migrations-read-model.ts
+++ packages/framework-integration-tests/src/read-models/data-migrations-read-model.ts
@@ -1,17 +1,27 @@
 import { BoosterDataMigrationEntity, Projects, ReadModel } from '@boostercloud/framework-core'
-import { ProjectionResult } from '@boostercloud/framework-types'
+import { BoosterDataMigrationEntityDuration, ProjectionResult } from '@boostercloud/framework-types'
 
 @ReadModel({
   authorize: 'all',
 })
 export class DataMigrationsReadModel {
-  public constructor(readonly id: string, readonly status: string, readonly lastUpdated: string) {}
+  public constructor(
+    readonly id: string,
+    readonly status: string,
+    readonly lastUpdated: string,
+    readonly duration?: BoosterDataMigrationEntityDuration
+  ) {}
 
   @Projects(BoosterDataMigrationEntity, 'id')
   public static updated(
     migration: BoosterDataMigrationEntity,
     _oldMigration?: DataMigrationsReadModel
   ): ProjectionResult<DataMigrationsReadModel> {
-    return new DataMigrationsReadModel(migration.id, migration.status.toString(), migration.lastUpdated)
+    return new DataMigrationsReadModel(
+      migration.id,
+      migration.status.toString(),
+      migration.lastUpdated,
+      migration.duration
+    )
   }
 }
