fe4b7bcc455b71981e4ed12e292f12145895da81
Javier Toledo <javier@theagilemonkeys.com>
2023-10-25 22:18:23 +0100

    Removed deprecated packages related to graphql subscriptions

--- packages/application-tester/src/graphql-helper.ts
+++ packages/application-tester/src/graphql-helper.ts
@@ -1,16 +1,6 @@
-import {
-  ApolloClient,
-  ApolloClientOptions,
-  ApolloLink,
-  HttpLink,
-  InMemoryCache,
-  NormalizedCacheObject,
-  split,
-} from '@apollo/client'
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, NormalizedCacheObject, split } from '@apollo/client'
 import { WebSocketLink } from '@apollo/client/link/ws'
 import fetch from 'cross-fetch'
-import * as WebSocket from 'ws'
-import { SubscriptionClient } from 'subscriptions-transport-ws'
 import { ProviderTestHelper } from './provider-test-helper'
 import { getMainDefinition } from '@apollo/client/utilities'
 
@@ -31,23 +21,31 @@ export class GraphQLHelper {
     })
   }
 
-  /**
-   * IMPORTANT: After using this "DisconnectableApolloClient", you must call ".disconnect()" to close the socket. Otherwise
-   * it will keep waiting for messages forever
-   */
-  public async clientWithSubscriptions(authToken?: AuthToken): Promise<DisconnectableApolloClient> {
-    const subscriptionClient: SubscriptionClient = await this.subscriptionsClient(authToken)
+  public async clientWithSubscriptions(authToken?: AuthToken): Promise<ApolloClient<NormalizedCacheObject>> {
+    const wsLink = new WebSocketLink({
+      uri: this.providerTestHelper.outputs.websocketURL,
+      options: {
+        reconnect: true,
+        connectionParams: () => {
+          if (authToken) {
+            const token = typeof authToken == 'function' ? authToken() : authToken
+            return { Authorization: 'Bearer ' + token }
+          }
+          return {}
+        },
+      },
+    })
 
     const link = split(
       ({ query }) => {
         const definition = getMainDefinition(query)
         return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
       },
-      new WebSocketLink(subscriptionClient),
+      wsLink,
       this.getAuthLink(authToken).concat(this.getApolloHTTPLink())
     )
 
-    return new DisconnectableApolloClient(subscriptionClient, {
+    return new ApolloClient({
       cache: new InMemoryCache(),
       link: link,
       defaultOptions: {
@@ -73,78 +71,5 @@ export class GraphQLHelper {
       }
       return forward(operation)
     })
-  }
-
-  private async subscriptionsClient(authToken?: AuthToken): Promise<SubscriptionClient> {
-    return new Promise((resolve, reject) => {
-      const subscriptionClient = new SubscriptionClient(
-        this.providerTestHelper.outputs.websocketURL,
-        {
-          reconnect: true,
-          connectionParams: () => {
-            if (authToken) {
-              const token = typeof authToken == 'function' ? authToken() : authToken
-              return {
-                Authorization: 'Bearer ' + token,
-              }
-            }
-            return {}
-          },
-          connectionCallback: (err?: any) => {
-            if (err) {
-              reject(err)
-              return
-            }
-            resolve(subscriptionClient)
-          },
-        },
-        class MyWebSocket extends WebSocket {
-          public constructor(url: string, protocols?: string | string[]) {
-            super(url, protocols)
-
-            this.addListener('open', (): void => {
-              console.debug('[GraphQL socket] on open')
-            })
-            this.addListener('ping', (): void => {
-              console.debug('[GraphQL socket] on "ping"')
-            })
-            this.addListener('pong', (): void => {
-              console.debug('[GraphQL socket] on "pong"')
-            })
-            this.addListener('message', (data: WebSocket.Data): void => {
-              console.debug('[GraphQL socket] on message: ', data)
-            })
-            this.addListener('close', (code: number, message: string): void => {
-              console.debug('[GraphQL socket] on close: ', code, message)
-            })
-            this.addListener('error', (err: Error): void => {
-              console.debug('[GraphQL socket] on error: ', err.message)
-            })
-          }
-        }
-      )
-    })
-  }
-}
-
-export class DisconnectableApolloClient extends ApolloClient<NormalizedCacheObject> {
-  constructor(
-    private readonly subscriptionClient: SubscriptionClient,
-    options: ApolloClientOptions<NormalizedCacheObject>
-  ) {
-    super(options)
-  }
-
-  public reconnect(): Promise<void> {
-    const reconnectPromise = new Promise<void>((resolve) => {
-      this.subscriptionClient.onReconnected(resolve)
-    })
-    this.subscriptionClient.close(false)
-    return reconnectPromise
-  }
-
-  public disconnect(): void {
-    this.subscriptionClient.close()
-    this.stop()
   }
 }
