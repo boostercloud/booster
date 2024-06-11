b1eb4fea3acfa244d126c6800ab5c1787b872f24
Castro, Mario <mariocs@optum.com>
2024-06-11 11:49:11 -0300

    Remove unnecessary comment

--- packages/framework-core/src/decorators/global-event-handler.ts
+++ packages/framework-core/src/decorators/global-event-handler.ts
@@ -6,6 +6,5 @@ export const BOOSTER_GLOBAL_EVENT_HANDLERS = 'BOOSTER_GLOBAL_EVENT_HANDLERS'
 export function GlobalEventHandler<TEventHandler extends EventHandlerInterface>(
   eventHandlerClass: TEventHandler
 ): void {
-  // We would need to do the function `registerEventHandler` exported
   registerEventHandler(BOOSTER_GLOBAL_EVENT_HANDLERS, eventHandlerClass)
 }
