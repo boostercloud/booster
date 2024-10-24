4deb4374fc62d5dca7eab364ad924b9ef9ad98f8
Nicolas Scheers <nicolas@lemon.be>
2024-10-24 11:33:22 +0200

    fix: simplify query

--- packages/framework-integration-tests/src/queries/search-media.ts
+++ packages/framework-integration-tests/src/queries/search-media.ts
@@ -3,32 +3,22 @@ import { QueryInfo } from '@boostercloud/framework-types'
 import { BookReadModel } from '../read-models/book-read-model'
 import { MovieReadModel } from '../read-models/movie-read-model'
 
-export abstract class BaseQuery<M extends BaseQuery<M>> {
-  constructor(obj: M) {
-    Object.assign(this, obj)
+export class BookMedia {
+  title!: string
+  pages!: number
+  constructor(book: BookMedia) {
+    ;(this.title = book.title), (this.pages = book.pages)
   }
 }
 
-export class BookMedia extends BaseQuery<BookMedia> {
+export class MovieMedia {
   title!: string
-  pages!: number
-}
-
-export class MovieMedia extends BaseQuery<MovieMedia> {
-  title!: string
-}
-
-export class Media extends BaseQuery<Media> {
-  mediaType!: MediaType
+  constructor(movie: MovieMedia) {
+    this.title = movie.title
+  }
 }
 
 export type MediaValue = BookMedia | MovieMedia
-
-export enum MediaType {
-  BookMedia = 'BookMedia',
-  MovieMedia = 'MovieMedia',
-}
-
 class SearchResult {
   readonly results!: MediaValue[]
   constructor(results: MediaValue[]) {
