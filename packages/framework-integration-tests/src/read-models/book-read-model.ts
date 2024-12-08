182bd621da0c25b85f1d48d9e0ff139092c36c5a
Nicolas Scheers <nicolas@lemon.be>
2024-12-08 22:25:17 +0100

    commit: remove unnecessary getId calls

--- packages/framework-integration-tests/src/read-models/book-read-model.ts
+++ packages/framework-integration-tests/src/read-models/book-read-model.ts
@@ -9,10 +9,7 @@ export class BookReadModel {
   public constructor(readonly id: UUID, readonly title: string, readonly pages: number) {}
 
   @Projects(Book, 'id')
-  public static updateBook(book: Book, oldBook?: BookReadModel): ProjectionResult<BookReadModel> {
-    // This method calls are here to ensure they work. More info: https://github.com/boostercloud/booster/issues/797
-    book.getId()
-
+  public static updateBook(book: Book): ProjectionResult<BookReadModel> {
     return new BookReadModel(book.id, book.title, book.pages)
   }
 }
--- packages/framework-integration-tests/src/read-models/movie-read-model.ts
+++ packages/framework-integration-tests/src/read-models/movie-read-model.ts
@@ -9,10 +9,7 @@ export class MovieReadModel {
   public constructor(readonly id: UUID, readonly title: string) {}
 
   @Projects(Movie, 'id')
-  public static updateMovie(movie: Movie, oldMovie?: MovieReadModel): ProjectionResult<MovieReadModel> {
-    // This method calls are here to ensure they work. More info: https://github.com/boostercloud/booster/issues/797
-    movie.getId()
-
+  public static updateMovie(movie: Movie): ProjectionResult<MovieReadModel> {
     return new MovieReadModel(movie.id, movie.title)
   }
 }
